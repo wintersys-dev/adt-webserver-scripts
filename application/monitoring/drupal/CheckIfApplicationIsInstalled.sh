44710e11678083dae17581cab103c22d422f7198
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-13 21:15:32 +0100

    Update CheckIfApplicationIsInstalled.sh

--- application/monitoring/drupal/CheckIfApplicationIsInstalled.sh
+++ application/monitoring/drupal/CheckIfApplicationIsInstalled.sh
@@ -20,6 +20,8 @@
 #######################################################################################################
 #set -x 
 
+webroot_directory="`/bin/grep "^WEBROOT_DIRECTORY:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
+
 installed="1"
 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh APPLICATION:drupal`" = "1" ] )
 then	
