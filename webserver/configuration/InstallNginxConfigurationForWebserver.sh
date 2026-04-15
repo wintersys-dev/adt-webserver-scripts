475b6348f877515cc1a2a4ae9fc9e7297c0bbc81
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-15 17:08:56 +0100

    Update InstallNginxConfigurationForWebserver.sh

--- webserver/configuration/InstallNginxConfigurationForWebserver.sh
+++ webserver/configuration/InstallNginxConfigurationForWebserver.sh
@@ -55,6 +55,25 @@ fi
 /bin/sed -i "s/XXXXPORTXXXX/${port}/" ${HOME}/webserver/configuration/application/nginx/site-available.conf.${APPLICATION}
 /bin/sed -i "s;XXXXWEBROOT_DIRECTORYXXXX;${webroot_directory};" ${HOME}/webserver/configuration/application/nginx/site-available.conf.${APPLICATION}
 
+if ( [ ! -d /var/lib/php/session ] )
+then
+	/bin/mkdir -p /var/lib/php/sessions
+	/bin/chown -R www-data:www-data /var/lib/php
+fi
+
+php_ini="/etc/php/${PHP_VERSION}/fpm/php.ini"
+www_conf="/etc/php/${PHP_VERSION}/fpm/pool.d/www.conf"
+/bin/sed -i "s/^;env/env/g" ${www_conf}
+
+if ( [ "`/bin/echo ${port} | /bin/grep -o "^[0-9]*$"`" != "" ] )
+then
+	/bin/sed -i "s/^listen =.*/listen = 127.0.0.1:${port}/g" ${www_conf}
+	/bin/sed -i "s/^;listen.allowed_clients/listen.allowed_clients/" ${www_conf}
+else
+	/bin/sed -i "s,^listen =.*,listen = /var/run/php${PHP_VERSION}-fpm.sock,g" ${www_conf}
+	/bin/sed -i "s/^;listen.mode/listen.mode/" ${www_conf}
+fi
+
 if ( [ "`/bin/echo ${port} | /bin/grep -o "^[0-9]*$"`" != "" ] )
 then
 	/bin/sed -i "s/#XXXXPHPPORTXXXX//g" ${HOME}/webserver/configuration/application/nginx/site-available.conf.${APPLICATION}
