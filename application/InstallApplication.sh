182e274da4ff824bc99514304e22ef6dca29c605
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-15 16:57:38 +0100

    Update InstallApplication.sh

--- application/InstallApplication.sh
+++ application/InstallApplication.sh
@@ -42,7 +42,7 @@ else
 fi
 
 #First make sure that the php modules required for the current application are installed to satisfy any checks that are issued during installation
-${HOME}/installation/InstallPHPApplication.sh ${BUILDOS}
+${HOME}/installation/InstallPHPLibraries.sh ${BUILDOS}
 ${HOME}/application/configuration/OverrideApplicationLanguageConfiguration.sh
 ${HOME}/utilities/processing/RunServiceCommand.sh php${PHP_VERSION}-fpm restart 2>/dev/null
 
