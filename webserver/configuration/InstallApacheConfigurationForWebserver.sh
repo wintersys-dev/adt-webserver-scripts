5d11e11e18dd10f005385315c0d85de496d07bc4
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-15 17:05:55 +0100

    Update InstallApacheConfigurationForWebserver.sh

--- webserver/configuration/InstallApacheConfigurationForWebserver.sh
+++ webserver/configuration/InstallApacheConfigurationForWebserver.sh
@@ -73,6 +73,25 @@ then
 	/bin/sed -i "s/#XXXXMODSECURITYXXXX//g" ${HOME}/webserver/configuration/application/apache/site-available.conf.${APPLICATION}
 fi
 
+if ( [ ! -d /var/lib/php/session ] )
+then
+	/bin/mkdir -p /var/lib/php/sessions
+	/bin/chown -R www-data:www-data /var/lib/php
+fi
+
+php_ini="/etc/php/${PHP_VERSION}/fpm/php.ini"
+www_conf="/etc/php/${PHP_VERSION}/fpm/pool.d/www.conf"
+/bin/sed -i "s/^;env/env/g" ${www_conf}
+
+if ( [ "`/bin/echo ${port} | /bin/grep -o "^[0-9]*$"`" != "" ] )
+then
+	/bin/sed -i "s/^listen =.*/listen = 127.0.0.1:${port}/g" ${www_conf}
+	/bin/sed -i "s/^;listen.allowed_clients/listen.allowed_clients/" ${www_conf}
+else
+	/bin/sed -i "s,^listen =.*,listen = /var/run/php${PHP_VERSION}-fpm.sock,g" ${www_conf}
+	/bin/sed -i "s/^;listen.mode/listen.mode/" ${www_conf}
+fi
+
 if ( [ "`/bin/echo ${port} | /bin/grep -o "^[0-9]*$"`" != "" ] )
 then
 	/bin/sed -i "s/#XXXXFASTCGIPORTXXXX//g" ${HOME}/webserver/configuration/application/apache/site-available.conf.${APPLICATION}
