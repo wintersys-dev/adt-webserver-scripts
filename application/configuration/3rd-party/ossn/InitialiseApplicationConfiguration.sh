1704689f68d3a4b6648a93edf441f7da41718f6e
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-16 12:09:56 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/3rd-party/ossn/InitialiseApplicationConfiguration.sh
+++ application/configuration/3rd-party/ossn/InitialiseApplicationConfiguration.sh
@@ -82,6 +82,9 @@ then
         /bin/chown www-data:www-data /var/www/html/ossn.config.site.php.default
 fi
 
+dbprefix="ossn_"
+/bin/echo "${dbprefix}" > /var/www/html/dbp.dat
+/bin/chown www-data:www-data /var/www/html/dbp.dat
 
 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
 then
