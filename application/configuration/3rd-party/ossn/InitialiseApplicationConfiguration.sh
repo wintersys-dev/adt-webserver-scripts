af413b2208701a49b15b610ef7f08be1929717a1
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-16 08:48:34 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/3rd-party/ossn/InitialiseApplicationConfiguration.sh
+++ application/configuration/3rd-party/ossn/InitialiseApplicationConfiguration.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 ###########################################################################################################
-# Description:This script will generate a /var/www/html/ossn.config.db.php and /var/www/html/ossn.site.db.php 
-# using the values that you have set in
+# Description:This script will generate a /var/www/html/configuration.php using the values that you have set in
 #
-#        ${BUILD_HOME}/application/3rd-party/descriptors/ossn.dat
+#        ${BUILD_HOME}/application/descriptors/joomla.dat
 #
-# CHANGE THIS PART****************************
-# If a virgin copy of ossn is being installed, then, /usr/bin/php /var/www/html/installation/joomla.php is used
+# If a virgin copy of joomla is being installed, then, /usr/bin/php /var/www/html/installation/joomla.php is used
 # when making a non-interactive installation this means that the installer doesn't have to do anything once they 
 # have started the build they next thing they will see is a fully configured virgin joomla application. 
 # If you are deploying a baseline or a temporal backup then the configuration.php file is manually generated
 # based on the values set in 
 #
 #         ${BUILD_HOME}/application/descriptors/joomla.dat
-# CHANGE THIS PART****************************
 #
 # Author : Peter Winter
 # Date: 17/05/2017
@@ -62,9 +59,9 @@ then
         config_file="/var/www/html/ossn.config.db.php"
 fi
 
-if ( [ -f ${webroot_directory}/configurations/ossn.config.db.php ] )
+if ( [ -f ${webroot_directory}/ossn.config.db.php ] )
 then
-        /bin/rm ${webroot_directory}/configurations/ossn.config.db.php
+        /bin/rm ${webroot_directory}/ossn.config.db.php
 fi
 
 if ( [ -f ${webroot_directory}/configurations/ossn.site.db.example.php ] )
@@ -80,7 +77,95 @@ then
         config_file_site="/var/www/html/ossn.site.db.php"
 fi
 
-if ( [ -f ${webroot_directory}/configurations/ossn.site.db.php ] )
+if ( [ -f ${webroot_directory}/ossn.site.db.php ] )
 then
-        /bin/rm  ${webroot_directory}/configurations/ossn.site.db.php
+        /bin/rm ${webroot_directory}/ossn.site.db.php
+fi
+
+if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
+then
+        if ( [ ! -f ${webroot_directory}/ossn.config.db.php ] || [ ! -f ${webroot_directory}/ossn.site.db.php ] )
+        then
+                while ( [ ! -f ${webroot_directory}/ossn.config.db.php ] || [ ! -f ${webroot_directory}/ossn.site.db.php ] )
+                do
+                        /bin/sleep 1
+                done
+        fi
+        #       /bin/echo "`/bin/grep "dbprefix" ${webroot_directory}/configuration.php | /usr/bin/awk -F"'" '{print $2}'`" > /var/www/html/dbp.dat
+        #       /bin/chown www-data:www-data /var/www/html/dbp.dat
+else
+        if ( [ -f ${config_file} ] )
+        then
+                /bin/rm ${config_file}
+        fi
+
+        if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:DBaaS`" = "1" ] )
+        then
+                HOST="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBIDENTIFIER'`"
+        else
+                HOST="`${HOME}/services/datastore/config/wrapper/ListFromDatastore.sh "config" "databaseip/*"`"
+        fi
+        DB_PORT="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBPORT'`"
+
+        if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:DBaaS`" = "1" ] )
+        then
+                HOST="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBIDENTIFIER'`"
+        else
+                HOST="`${HOME}/services/datastore/config/wrapper/ListFromDatastore.sh "config" "databaseip/*"`"
+        fi
+
+        if ( [ -f ${HOME}/runtime/application.dat ] )
+        then
+                if ( [ ! -d ${HOME}/runtime/filesystem_sync/webroot-sync/outgoing ] )
+                then
+                        /bin/mkdir -p ${HOME}/runtime/filesystem_sync/webroot-sync/outgoing
+                fi
+
+                if ( [ -f ${HOME}/runtime/filesystem_sync/webroot-sync/outgoing/exclusion_list.dat ] )
+                then
+                        /bin/rm ${HOME}/runtime/filesystem_sync/webroot-sync/outgoing/exclusion_list.dat
+                fi
+
+                for directory in `/bin/grep "^DIRECTORIES_TO_CREATE:" ${HOME}/runtime/application.dat | /bin/sed 's/DIRECTORIES_TO_CREATE://g' | /bin/sed 's/:/ /g'`
+                do
+                        directory="/var/www/html/${directory}"
+
+                        if ( [ ! -d ${directory} ] )
+                        then
+                                /bin/mkdir -p ${directory}
+                                /bin/echo "${directory}" >> ${HOME}/runtime/filesystem_sync/webroot-sync/outgoing/exclusion_list.dat
+                        fi
+
+                        while ( [ "${directory}" != "/var/www/html" ] )
+                        do
+                                /bin/chmod 755 ${directory}
+                                /bin/chown www-data:www-data ${directory}
+                                directory=`/usr/bin/dirname "${directory}"`
+                        done
+                done
+        fi
+
+        user="`/bin/grep "^MANDATORY_INDIVIDUAL_SETTING:user=" ${HOME}/runtime/application.dat | /usr/bin/awk -F'=' '{print $NF}' | /bin/sed "s%'%%g"`"
+        password="`/bin/grep "^MANDATORY_INDIVIDUAL_SETTING:password=" ${HOME}/runtime/application.dat | /usr/bin/awk -F'=' '{print $NF}' | /bin/sed "s%'%%g"`"
+        dbname="`/bin/grep "^MANDATORY_INDIVIDUAL_SETTING:db=" ${HOME}/runtime/application.dat | /usr/bin/awk -F'=' '{print $NF}' | /bin/sed "s%'%%g"`"
+
+        if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:Maria`" = "1" ] || [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Maria`" = "1" ] )
+        then
+                type="mysqli"
+        fi
+
+        if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEDBaaSINSTALLATIONTYPE:MySQL`" = "1" ] || [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:MySQL`" = "1" ] )
+        then
+                type="mysqli"
+        fi
+
+        /bin/sed -i "s%<<host>>%${HOST}:${DB_PORT}%" ${config_file}
+        /bin/sed -i "s%<<user>>%${user}%" ${config_file}
+        /bin/sed -i "s%<<password>>%${password}%" ${config_file}
+        /bin/sed -i "s%<<dbname>>%${dbname}%" ${config_file}
+
+        WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
+
+        /bin/sed -i "s%<<siteurl>>%https://${WEBSITE_URL}%" ${config_file_site}
+        /bin/sed -i "s%<<datadir>>%/var/www/html/ossn_data%" ${config_file_site}
 fi
