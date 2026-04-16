6e556c7ad741d5638b52d503a1df56bf1b2eb828
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-16 18:39:23 +0100

    Update InstallVirginApplication.sh

--- application/installation/cms/moodle/InstallVirginApplication.sh
+++ application/installation/cms/moodle/InstallVirginApplication.sh
@@ -27,7 +27,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 ####################################################################################
 ####################################################################################
-set -x
+#set -x
 
 if ( [ ! -d ${HOME}/logs/application_installation ] )
 then
@@ -46,10 +46,12 @@ fi
 
 cd ${HOME}/runtime/downloads_work_area
 
-moodle_git_branch="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "MOODLE:git-branch" "stripped"`"
+moodle_git_branch="`/bin/grep "^MOODLE:git-branch:" ${HOME}/runtime/application.dat | /bin/sed 's/MOODLE:git-branch://g'`"
+
 if ( [ "${moodle_git_branch}" != "" ] )
 then
-        ${HOME}/services/git/GitClone.sh "github" "" "moodle" "moodle" "" "${moodle_git_branch}" "srclib/apr"
+        ${HOME}/services/git/GitClone.sh "github" "" "moodle" "moodle" "" "${moodle_git_branch}" "/var/www/html/moodle"
+        /bin/chown -R www-data:www-data /var/www/html
 else
         SOURCECODE_URL="`/bin/grep "^SOURCECODE_URL" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_URL://g' | /bin/sed 's/:/ /g'`"
         SOURCECODE_MD5="`/bin/grep "^SOURCECODE_MD5" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_MD5://g' | /bin/sed 's/:/ /g'`"
