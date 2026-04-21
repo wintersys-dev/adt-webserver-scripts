8033db4c9d7805453fd8c08f2a21fc9c6a829bd2
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-21 12:37:05 +0100

    Update InstallVirginApplication.sh

--- application/installation/cms/joomla/InstallVirginApplication.sh
+++ application/installation/cms/joomla/InstallVirginApplication.sh
@@ -27,7 +27,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 ######################################################################################
 ######################################################################################
-set -x
+#set -x
 
 if ( [ ! -d ${HOME}/logs/application_installation ] )
 then
@@ -47,12 +47,13 @@ fi
 cd ${HOME}/runtime/downloads_work_area
 
 checksum="0"
-if ( [ "`/bin/grep "^SOURCECODE_URL:github" ${HOME}/runtime/application.dat | /bin/grep 'github.com'`" != "" ] )
+if ( [ "`/bin/grep "^SOURCECODE_URL" ${HOME}/runtime/application.dat | /bin/grep 'github.com'`" != "" ] )
 then
         SOURCECODE_URL="`/bin/grep "^SOURCECODE_URL" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_URL://g' | /bin/sed 's/:/ /g'`"
-elif ( [ "`/bin/grep "^SOURCECODE_URL:github" ${HOME}/runtime/application.dat | /bin/grep 'joomla.org'`" != "" ] )
+elif ( [ "`/bin/grep "^SOURCECODE_URL" ${HOME}/runtime/application.dat | /bin/grep 'joomla.org'`" != "" ] )
 then
         checksum="1"
+        SOURCECODE_URL="`/bin/grep "^SOURCECODE_URL" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_URL://g' | /bin/sed 's/:/ /g'`"
         SOURCECODE_MD5="`/bin/grep "^SOURCECODE_MD5" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_MD5://g' | /bin/sed 's/:/ /g'`"
         SOURCECODE_SHA1="`/bin/grep "^SOURCECODE_SHA1" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_SHA1://g' | /bin/sed 's/:/ /g'`"
 fi
@@ -103,8 +104,7 @@ then
         /bin/rm joomla.${verified_archive_type}
         /bin/chown -R www-data:www-data ${webroot_directory}/*
 fi
-        
+
 cd ${HOME}
 /bin/echo "success"
-fi
 
