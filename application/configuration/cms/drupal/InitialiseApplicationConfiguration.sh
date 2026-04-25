1e763b3aef444a80a958669aec450d7617bc9580
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-25 17:19:54 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
@@ -181,12 +181,7 @@ else
 
                 /bin/sed -i 's/^$databases.*;/\$databases['\''default'\'']['\''default'\''] = ['\''username'\'' => '${username}', '\''password'\'' => '${password}', '\''database'\'' => '${database}', '\''host'\'' => '\'${HOST}\'', '\''port'\'' => '${DB_PORT}', '\''driver'\'' => '${driver}', '\''prefix'\'' => '\'${dbprefix}\'', '\''collation'\'' => '${collation}', '\''isolation_level'\'' => '\''READ COMMITTED'\'', ];/' ${webroot_directory}/sites/default/settings.php
 
-                if ( [ "`/bin/grep "^APPLICATION_TYPE:droopler" ${HOME}/runtime/application.dat`" != "" ] )                
-                then
-                        /usr/sbin/drush site-install droopler -y
-                else
-                        /usr/sbin/drush site:install -y
-                fi
+                /usr/sbin/drush site:install -y
                 /usr/sbin/drush cr
                 /usr/sbin/drush user:create ${website_username} --password="${website_password}"
                 /usr/sbin/drush user:role:add "administrator" "${website_username}"
