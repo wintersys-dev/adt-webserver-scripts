f1a733b9aff03a7ec52431d5704b6e9a6d392409
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-27 21:15:57 +0100

    Update Backup.sh

--- application/backup/Backup.sh
+++ application/backup/Backup.sh
@@ -76,7 +76,7 @@ then
         current_time="`/usr/bin/date +%s`"
         backup_time="`/bin/cat ${HOME}/runtime/datastore_workarea/time_backup_written`"
         
-        if ( [ "`/usr/bin/expr ${current_time} - ${backup_time}`" -lt "600" ] )
+        if ( [ "`/usr/bin/expr ${current_time} - ${backup_time}`" -lt "300" ] )
         then
                 exit
         fi
