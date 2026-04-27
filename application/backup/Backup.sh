42bf606de9e3b9230335f5c3e927642001447d60
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-27 21:58:42 +0100

    Update Backup.sh

--- application/backup/Backup.sh
+++ application/backup/Backup.sh
@@ -183,12 +183,9 @@ fi
 #Write the backup to the datastore
 if ( [ -f ${HOME}/livebackup/applicationsourcecode.tar.gz ] )
 then
-        backup_id="`${HOME}/services/datastore/operations/ListFromDatastore.sh "backup" "${backup_file}.BACKUP" "${period}${provider_id}" | /usr/bin/wc -l`"
-        backup_id="`/usr/bin/expr ${backup_id} + 1`"
-
         if ( [ "`${HOME}/services/datastore/operations/ListFromDatastore.sh "backup" "${backup_file}" "${period}${provider_id}"`" != "" ] )
         then
-                ${HOME}/services/datastore/operations/MoveDatastore.sh "backup" "${backup_file}" "${backup_file}.BACKUP.${backup_id}" "distributed" "${period}${provider_id}"
+                ${HOME}/services/datastore/operations/MoveDatastore.sh "backup" "${backup_file}" "${backup_file}.BACKUP.`/usr/bin/date +%F-%Hhr`" "distributed" "${period}${provider_id}"
         fi
 
         /bin/systemd-inhibit --why="Persisting sourcecode to datastore" ${HOME}/services/datastore/operations/PutToDatastore.sh "backup" "${HOME}/livebackup/applicationsourcecode.tar.gz" "root" "distributed" "no" "${period}${provider_id}"
