0c1fdc42918fd9bb7745d223c2a823bad9387b16
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-28 16:36:26 +0100

    Update and rename 1.sh to InstallVirginApplication.sh

---
+++ application/installation/3rd-party/social-messenger/InstallVirginApplication.sh
@@ -0,0 +1,4 @@
+ php-social-messenger
+
+${HOME}/services/git/GitClone.sh "github" "" "Iqbolshoh" "php-social-messenger" "" "main" "/var/www/html/social-messenger"
+/bin/chown -R www-data:www-data /var/www/html
