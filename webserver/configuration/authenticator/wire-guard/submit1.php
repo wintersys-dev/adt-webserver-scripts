33966ea345ff2d87600eeef2b2626c56450843d5
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-30 11:29:24 +0100

    Change POST parameter from 'ipaddress' to 'authorise'

--- webserver/configuration/authenticator/wire-guard/submit1.php
+++ webserver/configuration/authenticator/wire-guard/submit1.php
@@ -1,7 +1,7 @@
 <?php
-  $ipaddress = $_POST["ipaddress"];
+  $ipaddress = $_POST["authorise"];
   $file="/tmp/ipaddresses.dat";
-  $data = "$ipaddress\n";
+  $data = "$authorise\n";
   file_put_contents($file, $data, FILE_APPEND );
   echo "<h2>Access for your IP Address has been requested it might take up to a minute, any longer and there is a problem.</h2>";
 ?>
