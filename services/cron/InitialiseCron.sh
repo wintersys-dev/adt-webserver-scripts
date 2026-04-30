45b81876841b441ebc13a8909b531abe9803e79c
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-30 17:16:58 +0100

    Update InitialiseCron.sh

--- services/cron/InitialiseCron.sh
+++ services/cron/InitialiseCron.sh
@@ -61,6 +61,9 @@ then
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 50 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateBasicAuthConfig.sh" >> /var/spool/cron/crontabs/root	
 	elif ( [ "${AUTHENTICATOR_TYPE}" = "wire-guard" ] )
 	then
+		machine_id="`/usr/bin/hostname | /bin/sed 's/NO\-//g' | /usr/bin/awk -F'-' '{print $1}'`"
+		delay="`/usr/bin/expr ${machine_id} \* 20`"
+		/bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep ${delay} && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/AcceptIPAddresses.sh" >> /var/spool/cron/crontabs/root
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateAuthenticationEmails.sh" >> /var/spool/cron/crontabs/root
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 10 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateAuthenticationEmails.sh" >> /var/spool/cron/crontabs/root
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 20 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateAuthenticationEmails.sh" >> /var/spool/cron/crontabs/root
