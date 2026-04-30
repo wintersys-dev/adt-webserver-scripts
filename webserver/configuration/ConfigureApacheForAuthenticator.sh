d03103ffbc3910cbed3a7e286e3aee746134d962
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-30 15:58:31 +0100

    Refactor Apache configuration for authenticator types

--- webserver/configuration/ConfigureApacheForAuthenticator.sh
+++ webserver/configuration/ConfigureApacheForAuthenticator.sh
@@ -146,7 +146,6 @@ then
         /bin/mkdir /var/www/firewall
         /bin/chown www-data:www-data /var/www/firewall
 	fi
-#	/bin/sed -i "s%^open_basedir =.*%open_basedir = /var/www/firewall%" ${php_ini}
 elif ( [ "${AUTHENTICATOR_TYPE}" = "basic-auth" ] )
 then
 	/bin/cp ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/index.html /var/www/html/index.html
@@ -159,24 +158,24 @@ then
         /bin/mkdir /var/www/basic-auth
         /bin/chown www-data:www-data /var/www/basic-auth
 	fi
-	#/bin/sed -i "s%^open_basedir =.*%open_basedir = /var/www/basic-auth%" ${php_ini}
 elif ( [ "${AUTHENTICATOR_TYPE}" = "wire-guard" ] )
 then
-		${HOME}/installation/InstallWireguard.sh
-		${HOME}/installation/InstallQREncode.sh	
-        /bin/cp ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/index.html /var/www/html/index.html
-        /bin/cp ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/submit.php /var/www/html/submit.php
-        /bin/cp ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/submit1.php /var/www/html/submit1.php
-        /bin/chown www-data:www-data /var/www/html/*
-        /bin/chmod 644 /var/www/html/*
-        /bin/sed -i "s/XXXXUSEREMAILDOMAINXXXX/${USER_EMAIL_DOMAIN}/g" /var/www/html/index.html
-        /bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /var/www/html/index.html
-        if ( [ ! -d /var/www/wire-guard ] )
-        then
-                /bin/mkdir /var/www/wire-guard
-                /bin/chown www-data:www-data /var/www/wire-guard
-        fi
-        /bin/sed -i "s%^open_basedir =.*%open_basedir = /var/www/wire-guard%" ${php_ini}
+	${HOME}/installation/InstallWireguard.sh
+	${HOME}/installation/InstallQREncode.sh	
+	/bin/cp ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/index.html /var/www/html/index.html
+	/bin/cp ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/submit.php /var/www/html/submit.php
+	/bin/cp ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/submit1.php /var/www/html/submit1.php
+	/bin/chown www-data:www-data /var/www/html/*
+	/bin/chmod 644 /var/www/html/*
+	/bin/sed -i "s/XXXXUSEREMAILDOMAINXXXX/${USER_EMAIL_DOMAIN}/g" /var/www/html/index.html
+	/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /var/www/html/index.html
+        
+	if ( [ ! -d /var/www/wire-guard ] )
+	then
+		/bin/mkdir /var/www/wire-guard
+		/bin/chown www-data:www-data /var/www/wire-guard
+    fi
+	/bin/sed -i "s%^open_basedir =.*%open_basedir = /var/www/wire-guard%" ${php_ini}
 fi
 
 
