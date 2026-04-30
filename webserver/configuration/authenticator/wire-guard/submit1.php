3758babdcac2fe8a8875f038dd6461e519730bf3
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-30 11:32:55 +0100

    Update submit1.php

--- webserver/configuration/authenticator/wire-guard/submit1.php
+++ webserver/configuration/authenticator/wire-guard/submit1.php
@@ -3,5 +3,5 @@
   $file="/tmp/ipaddresses.dat";
   $data = "$email\n";
   file_put_contents($file, $data, FILE_APPEND );
-  echo "<h2>Access for your IP Address has been requested it might take up to a minute, any longer and there is a problem.</h2>";
+  echo "<h2>Wireguard access for your client has been requested</h2>";
 ?>
