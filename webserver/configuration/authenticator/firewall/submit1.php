0b190e9c35f4a4c3650b07a24dce37824c63837f
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-30 15:07:40 +0100

    Update submit1.php

--- webserver/configuration/authenticator/firewall/submit1.php
+++ webserver/configuration/authenticator/firewall/submit1.php
@@ -1,6 +1,6 @@
 <?php
   $ipaddress = $_POST["ipaddress"];
-  $file="/tmp/ipaddresses.dat";
+  $file="/var/www/firewall/ipaddresses.dat";
   $data = "$ipaddress\n";
   file_put_contents($file, $data, FILE_APPEND );
   echo "<h2>Access for your IP Address has been requested it might take up to a minute, any longer and there is a problem.</h2>";
