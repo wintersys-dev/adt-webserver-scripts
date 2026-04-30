71a4ba9d82a82647225d4955c402e31bb30f6e6a
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-30 13:28:36 +0100

    Add support for 'wire-guard' in authenticator type check

--- services/cron/InitialiseCron.sh
+++ services/cron/InitialiseCron.sh
@@ -51,7 +51,7 @@ then
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 40 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateAuthenticationEmails.sh" >> /var/spool/cron/crontabs/root
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 50 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateAuthenticationEmails.sh" >> /var/spool/cron/crontabs/root
 		/bin/echo "*/1 * * * * /usr/bin/find /var/www/html/ip-address* -mmin +5 -type f -exec rm -fv {} \;" >> /var/spool/cron/crontabs/root	
-	elif ( [ "${AUTHENTICATOR_TYPE}" = "basic-auth" ] )
+	elif ( [ "${AUTHENTICATOR_TYPE}" = "basic-auth" ] || [ "${AUTHENTICATOR_TYPE}" = "wire-guard" ] )
 	then
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateBasicAuthConfig.sh" >> /var/spool/cron/crontabs/root
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 10 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateBasicAuthConfig.sh" >> /var/spool/cron/crontabs/root
