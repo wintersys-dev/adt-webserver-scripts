8c88d5ab6e49d27404f847989168f0355624c691
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-04-30 19:47:33 +0100

    Update GenerateAuthenticationEmails.sh

--- webserver/configuration/authenticator/wire-guard/GenerateAuthenticationEmails.sh
+++ webserver/configuration/authenticator/wire-guard/GenerateAuthenticationEmails.sh
@@ -34,6 +34,10 @@ email_list="`/bin/cat ${HOME}/runtime/authenticator/authentication-emails.dat | 
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 WEBSITE_URL_ORIGINAL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURLORIGINAL'`"
 
+#create a new diirectory for QR codes send the email to email address that the QR code is labelled as  and then move the QR code into processed. 
+#When we generate the QR code check if it exists in the processed directory and if it does, copy it from the processed to tbe new
+#Also copy all the processed QR codes to the datastore and sync them down to the processed directory from other machines
+
 for email_address in ${email_list}
 do
         file_name="`/usr/bin/openssl rand -base64 32 | /usr/bin/tr -cd 'a-zA-Z0-9' | /usr/bin/cut -b 1-16 | /usr/bin/tr '[:upper:]' '[:lower:]'`"
