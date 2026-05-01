5430d374156e90009bcf89988689e096a5061891
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-01 11:45:39 +0100

    Update SyncFromDatastore.sh

--- services/datastore/operations/SyncFromDatastore.sh
+++ services/datastore/operations/SyncFromDatastore.sh
@@ -104,6 +104,16 @@ then
                 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
         fi
         active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-basic-auth-credentials"    
+elif ( [ "${bucket_type}" = "wireguard-config" ] )
+then
+        if ( [ "`/usr/bin/hostname | /bin/grep '\-auth-'`" != "" ] )
+        then
+                WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURLORIGINAL'`"
+        else
+                WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
+        fi
+        active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`"
+        active_bucket="${active_bucket}-wireguard-config"
 fi
 
 if ( [ "${place_to_sync}" = "root" ] )
