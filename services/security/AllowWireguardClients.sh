319de4b4db157ceb16b4ec3b35e41d1080d61d95
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-01 16:37:59 +0100

    Update AllowWireguardClients.sh

--- services/security/AllowWireguardClients.sh
+++ services/security/AllowWireguardClients.sh
@@ -7,15 +7,48 @@ then
   /bin/touch ${HOME}/runtime/IP_FORWARDING_ENABLED 
 fi
 
+if ( [ ! -d /etc/wireguard/freshqrcodes ] )
+then
+  /bin/mkdir -p /etc/wireguard/freshqrcodes
+fi
+
+if ( [ ! -d /etc/wireguard/client ] )
+then
+  /bin/mkdir -p /etc/wireguard/client
+fi
+
+if ( [ ! -d /etc/wireguard/server ] )
+then
+  /bin/mkdir -p /etc/wireguard/server
+fi
+
 ${HOME}/services/datastore/operations/SyncFromDatastore.sh "wireguard-config" "wireguard-config/*" "${HOME}/runtime/authenticator"
 /bin/cp ${HOME}/runtime/authenticator/qrcode/*  /etc/wireguard/freshqrcodes
 /bin/cp ${HOME}/runtime/authenticator/client/* /etc/wireguard
 /bin/cp ${HOME}/runtime/authenticator/server/* /etc/wireguard
 
+is_modified="0"
 now="`/usr/bin/date +%s`" 
-modified="`/usr/bin/stat -c %Y /etc/wireguard`"
 
+modified="`/usr/bin/stat -c %Y /etc/wireguard/freshqrcodes`"
 if ( [ "`/usr/bin/expr ${modified} - ${now}`" -lt "90" ] )
+then
+  is_modified="1"
+fi
+
+modified="`/usr/bin/stat -c %Y /etc/wireguard/client`"
+if ( [ "`/usr/bin/expr ${modified} - ${now}`" -lt "90" ] )
+then
+  is_modified="1"
+fi
+
+modified="`/usr/bin/stat -c %Y /etc/wireguard/server`"
+if ( [ "`/usr/bin/expr ${modified} - ${now}`" -lt "90" ] )
+then
+  is_modified="1"
+fi
+
+if ( [ "${is_modified}" = "1" ] )
 then
   ${HOME}/utilities/processing/RunServiceCommand.sh wg-quick@wg0 stop
   ${HOME}/utilities/processing/RunServiceCommand.sh wg-quick@wg0 start
