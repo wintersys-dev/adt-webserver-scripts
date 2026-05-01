c3b131afc836514c3e344c8b6b1afb9f7f3f7a6e
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-01 16:15:30 +0100

    Update AllowWireguardClient.sh

--- services/security/AllowWireguardClient.sh
+++ services/security/AllowWireguardClient.sh
@@ -1,3 +1,7 @@
 
 
 ${HOME}/services/datastore/operations/SyncFromDatastore.sh "wireguard-config" "wireguard-config/*" "${HOME}/runtime/authenticator"
+/bin/cp ${HOME}/runtime/authenticator/qrcode/*  /etc/wireguard/freshqrcodes
+/bin/cp ${HOME}/runtime/authenticator/client/* /etc/wireguard
+/bin/cp ${HOME}/runtime/authenticator/server/* /etc/wireguard
+
