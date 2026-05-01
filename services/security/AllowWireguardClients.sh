980a364bfde190bb325d20e409e436272c2529e0
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-01 16:27:27 +0100

    Enable IP forwarding if not already set

--- services/security/AllowWireguardClients.sh
+++ services/security/AllowWireguardClients.sh
@@ -1,5 +1,12 @@
 
 
+if ( [ ! -f ${HOME}/runtime/IP_FORWARDING_ENABLED ] )
+then
+  /bin/echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+  /usr/sbin/sysctl -p
+  /bin/touch ${HOME}/runtime/IP_FORWARDING_ENABLED 
+fi
+
 ${HOME}/services/datastore/operations/SyncFromDatastore.sh "wireguard-config" "wireguard-config/*" "${HOME}/runtime/authenticator"
 /bin/cp ${HOME}/runtime/authenticator/qrcode/*  /etc/wireguard/freshqrcodes
 /bin/cp ${HOME}/runtime/authenticator/client/* /etc/wireguard
