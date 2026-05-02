6a42e2c04588ea4f4cf63a77056b478db01bb2b6
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-02 23:32:50 +0100

    Update AcceptEmailAddresses.sh

--- webserver/configuration/authenticator/wire-guard/AcceptEmailAddresses.sh
+++ webserver/configuration/authenticator/wire-guard/AcceptEmailAddresses.sh
@@ -110,6 +110,7 @@ then
                                 /bin/echo "[Peer]
                  PublicKey = ${server_public_key}
                  Endpoint = ${server_ip}:${wireguard_port}
+                 PersistentKeepAlive = 25
                  AllowedIPs =  10.0.0.0/16
                  PersistentKeepalive = 25" >> /etc/wireguard/client_${email_address}.conf
                         done
