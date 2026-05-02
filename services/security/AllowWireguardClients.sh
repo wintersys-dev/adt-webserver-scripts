a1b4ebd6829b6b13dd3ff11b15f3d081ab094d54
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-03 00:28:14 +0100

    Comment out QR code and client copy commands
    Commented out the copy commands for QR codes and clients.

--- services/security/AllowWireguardClients.sh
+++ services/security/AllowWireguardClients.sh
@@ -13,8 +13,8 @@ then
 fi
 
 ${HOME}/services/datastore/operations/SyncFromDatastore.sh "wireguard-config" "wireguard-config/*" "${HOME}/runtime/authenticator"
-/bin/cp ${HOME}/runtime/authenticator/qrcode/*  /etc/wireguard/freshqrcodes
-/bin/cp ${HOME}/runtime/authenticator/client/* /etc/wireguard
+#/bin/cp ${HOME}/runtime/authenticator/qrcode/*  /etc/wireguard/freshqrcodes
+#/bin/cp ${HOME}/runtime/authenticator/client/* /etc/wireguard
 /bin/cp ${HOME}/runtime/authenticator/server/* /etc/wireguard
 
 is_modified="0"
