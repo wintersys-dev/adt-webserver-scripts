038cdbaca2025ebc147e094de608d1af132573cc
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-03 19:47:11 +0100

    Update postup.sh

--- webserver/configuration/authenticator/wire-guard/postup.sh
+++ webserver/configuration/authenticator/wire-guard/postup.sh
@@ -1,22 +1,10 @@
-WIREGUARD_INTERFACE=wg0
-WIREGUARD_LAN=10.0.0.0/24
-MASQUERADE_INTERFACE=eth0
-
-/usr/sbin/iptables -t nat -I POSTROUTING -o $MASQUERADE_INTERFACE -j MASQUERADE -s $WIREGUARD_LAN
-
-# Add a WIREGUARD_wg0 chain to the FORWARD chain
-CHAIN_NAME="WIREGUARD_$WIREGUARD_INTERFACE"
-/usr/sbin/iptables -N $CHAIN_NAME
-/usr/sbin/iptables -A FORWARD -j $CHAIN_NAME
-
-# Accept related or established traffic
-/usr/sbin/iptables -A $CHAIN_NAME -o $WIREGUARD_INTERFACE -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-
-# Accept traffic from any Wireguard IP address connected to the Wireguard server
-/usr/sbin/iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -j ACCEPT
-
-# Drop everything else coming through the Wireguard interface
-/usr/sbin/iptables -A $CHAIN_NAME -i $WIREGUARD_INTERFACE -j DROP
-
-# Return to FORWARD chain
-/usr/sbin/iptables -A $CHAIN_NAME -j RETURN
+IN_FACE="eth0"                   # NIC connected to the internet
+WG_FACE="wg0"                    # WG NIC 
+SUB_NET="10.0.0.0/24"            # WG IPv4 sub/net aka CIDR
+WG_PORT="1036"                  # WG udp port
+ 
+/usr/sbin/iptables -t nat -I POSTROUTING 1 -s $SUB_NET -o $IN_FACE -j MASQUERADE
+/usr/sbin/iptables -I INPUT 1 -i $WG_FACE -j ACCEPT
+/usr/sbin/iptables -I FORWARD 1 -i $IN_FACE -o $WG_FACE -j ACCEPT
+/usr/sbin/iptables -I FORWARD 1 -i $WG_FACE -o $IN_FACE -j ACCEPT
+/usr/sbin/iptables -I INPUT 1 -i $IN_FACE -p udp --dport $WG_PORT -j ACCEPT
