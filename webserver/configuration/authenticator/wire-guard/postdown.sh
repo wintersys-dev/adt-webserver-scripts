cfe230fa76a6bc4e670b97baec4ae8f5b5ca5fd3
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-03 18:20:59 +0100

    Create postdown.sh

---
+++ webserver/configuration/authenticator/wire-guard/postdown.sh
@@ -0,0 +1,11 @@
+WIREGUARD_INTERFACE=wg0
+WIREGUARD_LAN=10.0.0.0/24
+MASQUERADE_INTERFACE=eth0
+CHAIN_NAME="WIREGUARD_$WIREGUARD_INTERFACE"
+
+/usr/sbin/iptables -t nat -D POSTROUTING -o $MASQUERADE_INTERFACE -j MASQUERADE -s $WIREGUARD_LAN
+
+# Remove and delete the WIREGUARD_wg0 chain
+/usr/sbin/iptables -D FORWARD -j $CHAIN_NAME
+/usr/sbin/iptables -F $CHAIN_NAME
+/usr/sbin/iptables -X $CHAIN_NAME
