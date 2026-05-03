e0aa2f121d16f0845a2d955a5107b866b557217c
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-03 16:44:21 +0100

    Update AllowWireguardClients.sh

--- services/security/AllowWireguardClients.sh
+++ services/security/AllowWireguardClients.sh
@@ -1,37 +1,33 @@
-
+#set -x
 
 if ( [ ! -f ${HOME}/runtime/IP_FORWARDING_ENABLED ] )
 then
-  /bin/echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
-  /usr/sbin/sysctl -p
-  /bin/touch ${HOME}/runtime/IP_FORWARDING_ENABLED 
-fi
-
-
-
-if ( [ ! -d /etc/wireguard/server ] )
-then
-        /bin/mkdir -p /etc/wireguard/server
+        /bin/echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+        /usr/sbin/sysctl -p
+        /bin/touch ${HOME}/runtime/IP_FORWARDING_ENABLED 
 fi
 
 ${HOME}/services/datastore/operations/SyncFromDatastore.sh "wireguard-config" "wireguard-config/*" "${HOME}/runtime/authenticator"
-#/bin/cp ${HOME}/runtime/authenticator/qrcode/*  /etc/wireguard/freshqrcodes
-#/bin/cp ${HOME}/runtime/authenticator/client/* /etc/wireguard
-/bin/cp ${HOME}/runtime/authenticator/server/* /etc/wireguard
-
-#ip="`${HOME}/utilities/processing/GetPublicIP.sh`"
-#/bin/sed -i "s/XXXXSERVER_PUBLIC_IPXXXX/${ip}/" /etc/wireguard/wg0.conf
-
-modified="`/usr/bin/stat -c %Y /etc/wireguard/server`"
-if ( [ "`/usr/bin/expr ${modified} - ${now}`" -lt "90" ] )
+if ( [ -f ${HOME}/runtime/authenticator/server/wg0.conf ] && [ -f /etc/wireguard/wg0.conf ] )
 then
-  is_modified="1"
+        if ( [ "`/usr/bin/diff ${HOME}/runtime/authenticator/server/wg0.conf /etc/wireguard/wg0.conf`" != "" ] || [ ! -f /etc/wireguard/wg0.conf ] )
+        then
+                /bin/mv ${HOME}/runtime/authenticator/server/wg0.conf /etc/wireguard/wg0.conf
+        fi
+fi
+
+now="`/usr/bin/date +%s`"
+is_modified="0"
+modified="`/usr/bin/stat -c %Y /etc/wireguard`"
+if ( [ "`/usr/bin/expr ${now} - ${modified}`" -lt "90" ] )
+then
+        is_modified="1"
 fi
 
 if ( [ "${is_modified}" = "1" ] )
 then
-  ${HOME}/utilities/processing/RunServiceCommand.sh wg-quick@wg0 stop
-  ${HOME}/utilities/processing/RunServiceCommand.sh wg-quick@wg0 start
+        ${HOME}/utilities/processing/RunServiceCommand.sh wg-quick@wg0 stop
+        ${HOME}/utilities/processing/RunServiceCommand.sh wg-quick@wg0 start
 fi
 
 
