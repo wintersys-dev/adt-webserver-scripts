a3827f6f6b4c97ed35b99524d1169f5430d9bbad
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-03 22:33:39 +0100

    Update postdown.sh

--- webserver/configuration/authenticator/wire-guard/postdown.sh
+++ webserver/configuration/authenticator/wire-guard/postdown.sh
@@ -1,10 +1,10 @@
- IN_FACE="eth0"                   # NIC connected to the internet
- WG_FACE="wg0"                    # WG NIC 
- SUB_NET="10.0.0.0/24"            # WG IPv4 sub/net aka CIDR
- WG_PORT="1036"                  # WG udp port
+interface="eth0"                  
+wg_interface="wg0"                     
+subnet="10.0.0.0/24"            
+wg_port="1036"                  
   
-  /usr/sbin/iptables -t nat -D POSTROUTING -s $SUB_NET -o $IN_FACE -j MASQUERADE
-  /usr/sbin/iptables -D INPUT -i $WG_FACE -j ACCEPT
-  /usr/sbin/iptables -D FORWARD -i $IN_FACE -o $WG_FACE -j ACCEPT
-  /usr/sbin/iptables -D FORWARD -i $WG_FACE -o $IN_FACE -j ACCEPT
-  /usr/sbin/iptables -D INPUT -i $IN_FACE -p udp --dport $WG_PORT -j ACCEPT
+/usr/sbin/iptables -t nat -D POSTROUTING -s ${subnet} -o ${interface} -j MASQUERADE
+/usr/sbin/iptables -D INPUT -i ${wg_interface} -j ACCEPT
+/usr/sbin/iptables -D FORWARD -i ${interface} -o ${wg_interface} -j ACCEPT
+/usr/sbin/iptables -D FORWARD -i ${wg_interface} -o ${interface} -j ACCEPT
+/usr/sbin/iptables -D INPUT -i ${interface} -p udp --dport ${wg_port} -j ACCEPT
