e0ad39f7b6836df1c696d4d45a9b04932e78da4b
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-03 22:36:24 +0100

    Update postup.sh

--- webserver/configuration/authenticator/wire-guard/postup.sh
+++ webserver/configuration/authenticator/wire-guard/postup.sh
@@ -1,10 +1,10 @@
-IN_FACE="eth0"                   # NIC connected to the internet
-WG_FACE="wg0"                    # WG NIC 
-SUB_NET="10.0.0.0/24"            # WG IPv4 sub/net aka CIDR
-WG_PORT="1036"                  # WG udp port
+interface="eth0"                   # NIC connected to the internet
+wg_interface="wg0"                    # WG NIC 
+subnet="10.0.0.0/24"            # WG IPv4 sub/net aka CIDR
+wg_port="XXXXWG_PORTXXXX"                  # WG udp port
  
-/usr/sbin/iptables -t nat -I POSTROUTING 1 -s $SUB_NET -o $IN_FACE -j MASQUERADE
-/usr/sbin/iptables -I INPUT 1 -i $WG_FACE -j ACCEPT
-/usr/sbin/iptables -I FORWARD 1 -i $IN_FACE -o $WG_FACE -j ACCEPT
-/usr/sbin/iptables -I FORWARD 1 -i $WG_FACE -o $IN_FACE -j ACCEPT
-/usr/sbin/iptables -I INPUT 1 -i $IN_FACE -p udp --dport $WG_PORT -j ACCEPT
+/usr/sbin/iptables -t nat -I POSTROUTING 1 -s ${subnet} -o ${interface} -j MASQUERADE
+/usr/sbin/iptables -I INPUT 1 -i ${wg_interface} -j ACCEPT
+/usr/sbin/iptables -I FORWARD 1 -i ${interface} -o ${wg_interface} -j ACCEPT
+/usr/sbin/iptables -I FORWARD 1 -i ${wg_interface} -o ${interface} -j ACCEPT
+/usr/sbin/iptables -I INPUT 1 -i ${interface} -p udp --dport ${wg_port} -j ACCEPT
