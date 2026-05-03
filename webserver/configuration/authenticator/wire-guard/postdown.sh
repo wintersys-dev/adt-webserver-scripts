01597bd91135313a181db8879780cc0c18aa07b3
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-03 19:47:34 +0100

    Update postdown.sh

--- webserver/configuration/authenticator/wire-guard/postdown.sh
+++ webserver/configuration/authenticator/wire-guard/postdown.sh
@@ -1,11 +1,10 @@
-WIREGUARD_INTERFACE=wg0
-WIREGUARD_LAN=10.0.0.0/24
-MASQUERADE_INTERFACE=eth0
-CHAIN_NAME="WIREGUARD_$WIREGUARD_INTERFACE"
-
-/usr/sbin/iptables -t nat -D POSTROUTING -o $MASQUERADE_INTERFACE -j MASQUERADE -s $WIREGUARD_LAN
-
-# Remove and delete the WIREGUARD_wg0 chain
-/usr/sbin/iptables -D FORWARD -j $CHAIN_NAME
-/usr/sbin/iptables -F $CHAIN_NAME
-/usr/sbin/iptables -X $CHAIN_NAME
+ IN_FACE="eth0"                   # NIC connected to the internet
+ WG_FACE="wg0"                    # WG NIC 
+ SUB_NET="10.0.0.0/24"            # WG IPv4 sub/net aka CIDR
+ WG_PORT="1036"                  # WG udp port
+  
+  /usr/sbin/iptables -t nat -D POSTROUTING -s $SUB_NET -o $IN_FACE -j MASQUERADE
+  /usr/sbin/iptables -D INPUT -i $WG_FACE -j ACCEPT
+  /usr/sbin/iptables -D FORWARD -i $IN_FACE -o $WG_FACE -j ACCEPT
+  /usr/sbin/iptables -D FORWARD -i $WG_FACE -o $IN_FACE -j ACCEPT
+  /usr/sbin/iptables -D INPUT -i $IN_FACE -p udp --dport $WG_PORT -j ACCEPT
