81480ab85388feeae33d4b4bf24e2629c9cbe0f3
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-06 13:19:33 +0100

    Update rp.sh

--- rp.sh
+++ rp.sh
@@ -46,6 +46,8 @@ fi
 SERVER_USER="`${HOME}/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL' | /bin/sed 's/www8/www/g'`"
 
+${HOME}/utilities/config/StoreConfigValue.sh "WEBSITEURL" "${WEBSITE_URL}"
+
 #REMOTE_PROXY_WEBSERVER="`${HOME}/utilities/config/ExtractConfigValue.sh 'REMOTEPROXYWEBSERVER'`"
 #${HOME}/utilities/config/StoreConfigValue.sh "WEBSERVERCHOICE" "${REMOTE_PROXY_WEBSERVER}"
 
