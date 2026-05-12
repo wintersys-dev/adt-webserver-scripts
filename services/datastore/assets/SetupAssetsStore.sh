fa631b8de0d05274262a762df4397676370913da
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-12 07:01:08 +0100

    Update SetupAssetsStore.sh

--- services/datastore/assets/SetupAssetsStore.sh
+++ services/datastore/assets/SetupAssetsStore.sh
@@ -23,3 +23,57 @@
 ####################################################################################
 ####################################################################################
 #set -x
+
+if ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:s3fs:repo'`" = "1" ] || [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:s3fs:source'`" = "1" ] )
+then
+  
+
+elif ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:binary'`" = "1" ] || [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:source'`" = "1" ] )
+then
+  if ( [ ! -d /root/.aws ] )
+                        then
+                                /bin/mkdir /root/.aws
+                        fi
+						
+                        /bin/chmod 755 /root/.aws
+                        /bin/echo "[default]" > /root/.aws/credentials
+                        /bin/echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> /root/.aws/credentials
+                        /bin/echo "aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" >> /root/.aws/credentials
+						/bin/chmod 600 /root/.aws/credentials
+						
+						/bin/cp ${HOME}/services/datastore/assets/config/goofys.conf ${HOME}/runtime/goofys.conf
+
+						/bin/sed -i -e "s/XXXXS3UIDXXXX/${s3fs_uid}/g" -e "s/XXXXS3GIDXXXX/${s3fs_gid}/g" -e "s;XXXXENDPOINTXXXX;${endpoint};g" ${HOME}/runtime/goofys.conf
+ 
+						options=" "
+						for option in `/bin/cat ${HOME}/runtime/goofys.conf`
+						do
+							options="${options}${option} "
+						done
+						
+						/usr/bin/goofys ${options} ${asset_bucket} ${asset_directory}  &
+
+                elif ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:geesefs:binary'`" = "1" ] || [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:geesefs:source'`" = "1" ] )
+                then
+                        if ( [ ! -d /root/.aws ] )
+                        then
+                                /bin/mkdir /root/.aws
+                        fi
+                        /bin/chmod 755 /root/.aws
+                        /bin/echo "[default]" > /root/.aws/credentials
+                        /bin/echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> /root/.aws/credentials
+                        /bin/echo "aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" >> /root/.aws/credentials
+
+						/bin/cp ${HOME}/services/datastore/assets/config/geesefs.conf ${HOME}/runtime/geesefs.conf
+                        /bin/sed -i -e "s/XXXXS3UIDXXXX/${s3fs_uid}/g" -e "s/XXXXS3GIDXXXX/${s3fs_gid}/g" -e "s;XXXXENDPOINTXXXX;${endpoint};g" ${HOME}/runtime/geesefs.conf
+
+                        options=" "
+                        for option in `/bin/cat ${HOME}/runtime/geesefs.conf`
+                        do
+                                options="${options}${option} "
+                        done
+
+						/usr/bin/geesefs ${options} ${asset_bucket} ${asset_directory} &
+						
+                elif ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:rclone:repo'`" = "1" ] || [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:rclone:binary'`" = "1" ] || [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:rclone:source'`" = "1" ] || [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:rclone:script'`" = "1" ] )
+               
