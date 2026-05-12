5c9d7060147b81ae34644f1c5a54b24f85c09924
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-12 14:12:02 +0100

    Update Backup.sh

--- application/backup/Backup.sh
+++ application/backup/Backup.sh
@@ -102,7 +102,7 @@ then
         exclude_list="${exclude_list} `/usr/bin/mount | /bin/grep -Eo "/var/www/html.* " | /usr/bin/awk '{print $1}' | /usr/bin/tr '\n' ' ' | /bin/sed 's;/var/www/html/;;g'`"
 elif ( [ "`${HOME}/utilities/config/CheckConfigValue.sh PERSISTASSETSTODATASTORE:1`" = "0" ] )
 then
-        directories_to_omit="`/bin/grep "^WEBROOT_DIRECTORIES_TO_MOUNT:" ${HOME}/runtime/application.dat | /bin/sed 's/WEBROOT_DIRECTORIES_TO_MOUNT://g' | /bin/sed 's/:/ /g'`"
+        directories_to_omit="`/bin/grep "^WEBROOT_ASSET_DIRECTORIES:" ${HOME}/runtime/application.dat | /bin/sed 's/WEBROOT_ASSET_DIRECTORIES://g' | /bin/sed 's/:/ /g'`"
 
         if ( [ "${directories_to_omit}" != "" ] )
         then
