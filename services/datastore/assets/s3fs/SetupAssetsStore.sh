368b69717ae69e59ee9b522c6b3166373041a840
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-12 07:28:55 +0100

    Update SetupAssetsStore.sh

--- services/datastore/assets/s3fs/SetupAssetsStore.sh
+++ services/datastore/assets/s3fs/SetupAssetsStore.sh
@@ -34,5 +34,28 @@ then
         fi
 	done
 fi
-        if ( [ "`/bin/mount  | /bin/grep -P "${asset_directory}(?=\s|$)"`" = "" ] )
-        then	
+
+if ( [ "`/bin/mount  | /bin/grep -P "${asset_directory}(?=\s|$)"`" = "" ] )
+then				
+	/bin/cp ${HOME}/services/datastore/assets/config/s3fs.conf ${HOME}/runtime/s3fs.conf
+	/bin/sed -i -e "s/XXXXS3UIDXXXX/${s3fs_uid}/g" -e "s/XXXXS3GIDXXXX/${s3fs_gid}/g" -e "s;XXXXENDPOINTXXXX;${endpoint};g" ${HOME}/runtime/s3fs.conf
+	password_file="`/bin/grep "passwd_file" ${HOME}/runtime/s3fs.conf | /usr/bin/awk -F'=' '{print $NF}'`"
+	/bin/echo "${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY}" > ${password_file}
+	/bin/chmod 600 ${password_file}
+						
+	options="-o "
+	for option in `/bin/cat ${HOME}/runtime/s3fs.conf`
+	do
+		options="${options}${option},"
+	done
+	options="`/bin/echo ${options} | /bin/sed 's/,$//g'`"
+
+	/usr/bin/s3fs ${options} ${asset_bucket} ${asset_directory}
+
+	if ( [ "`/bin/mount  | /bin/grep -P "${asset_directory}(?=\s|$)"`" != "" ] )
+	then
+		if ( [ ! -f /var/www/s3-${asset_directory}/FIRST_MOUNT_INITIALISED ]
+		then
+			/bin/touch /var/www/s3-${asset_directory}/FIRST_MOUNT_INITIALISED
+			/bin/cp -r `/bin/echo /var/www/html/
+fi
