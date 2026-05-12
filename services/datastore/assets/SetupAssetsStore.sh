300d35d00d948b5c6b286775dd184c49ec3468d6
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-12 06:58:24 +0100

    Update SetupAssetsStore.sh

--- services/datastore/assets/SetupAssetsStore.sh
+++ services/datastore/assets/SetupAssetsStore.sh
@@ -1,1 +1,25 @@
-
+#!/bin/sh
+####################################################################################
+# Description: This script mounts a bucket from a cloud based datastore and uses it
+# as a shared directory.
+# This should only be used if you are deploying from a temporal backup. Baselined
+# and virgin deployments shouldn't use this and should have their assets on the 
+# local filesystem.
+# Author: Peter Winter
+# Date :  9/4/2016
+###################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+####################################################################################
+####################################################################################
+#set -x
