0de83c10d9d135d8369c079acdaff1e98b9b2256
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-12 13:34:23 +0100

    Update SyncAssetsBetweenProviders.sh

--- services/datastore/assets/SyncAssetsBetweenProviders.sh
+++ services/datastore/assets/SyncAssetsBetweenProviders.sh
@@ -1,15 +1,22 @@
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
-application_asset_dirs="`${HOME}/utilities/config/ExtractConfigValues.sh 'DIRECTORIESTOMOUNT' 'stripped' | /bin/sed 's/:/ /g'`"
+#application_asset_dirs="`${HOME}/utilities/config/ExtractConfigValues.sh 'DIRECTORIESTOMOUNT' 'stripped' | /bin/sed 's/:/ /g'`"
 
+application_asset_dirs=""
 application_asset_buckets=""
-for directory in ${application_asset_dirs}
-do
-        asset_bucket="`/bin/echo "${WEBSITE_URL}-assets-${directory}" | /bin/sed -e 's/\./-/g' -e 's;/;-;g' -e 's/--/-/g'`"
-        destinations="`/bin/grep "^\[s3_" /root/.config/rclone/rclone.multi.conf | /bin/sed '/^\[s3_1]/d' | /bin/sed -e 's/\[//' -e 's/\]//'`"
+application_asset_dirs="`/bin/grep "^WEBROOT_DIRECTORIES_TO_MOUNT:" ${HOME}/runtime/application.dat | /bin/sed 's/WEBROOT_DIRECTORIES_TO_MOUNT://g'`"
 
-        for destination in ${destinations}
+
+if ( [ "${application_asset_dirs}" != "" ] )
+then
+        for directory in ${application_asset_dirs}
         do
-                /usr/bin/rclone --config /root/.config/rclone/rclone.multi.conf mkdir ${destination}:${asset_bucket}
-                /usr/bin/rclone --config /root/.config/rclone/rclone.multi.conf sync s3_1:${asset_bucket} ${destination}:${asset_bucket}
+                asset_bucket="`/bin/echo "${WEBSITE_URL}-assets-${directory}" | /bin/sed -e 's/\./-/g' -e 's;/;-;g' -e 's/--/-/g'`"
+                destinations="`/bin/grep "^\[s3_" /root/.config/rclone/rclone.multi.conf | /bin/sed '/^\[s3_1]/d' | /bin/sed -e 's/\[//' -e 's/\]//'`"
+
+                for destination in ${destinations}
+                do
+                        /usr/bin/rclone --config /root/.config/rclone/rclone.multi.conf mkdir ${destination}:${asset_bucket}
+                        /usr/bin/rclone --config /root/.config/rclone/rclone.multi.conf sync s3_1:${asset_bucket} ${destination}:${asset_bucket}
+                done
         done
-done
+fi
