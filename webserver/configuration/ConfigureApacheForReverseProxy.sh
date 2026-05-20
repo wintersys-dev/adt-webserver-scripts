82318a79ed81f5ecf4345293868efa92abc843e1
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-20 20:27:02 +0100

    Update ConfigureApacheForReverseProxy.sh

--- webserver/configuration/ConfigureApacheForReverseProxy.sh
+++ webserver/configuration/ConfigureApacheForReverseProxy.sh
@@ -105,13 +105,11 @@ then
 	S3_HOST_BASE="`${HOME}/utilities/config/ExtractConfigValue.sh 'S3HOSTBASE'`"
 	application_asset_dirs="`/bin/grep "^WEBROOT_ASSET_DIRECTORIES:" ${HOME}/runtime/application.dat | /bin/sed 's/WEBROOT_ASSET_DIRECTORIES://g' | /bin/sed 's/:/ /g'`"
 
-	/bin/echo "RewriteEngine On" > ${HOME}/runtime/redirection.conf
-
 	for application_assets_directory in ${application_asset_dirs}
 	do
         asset_bucket="`/bin/echo "${WEBSITE_URL}-assets-${application_assets_directory}" | /bin/sed -e 's/\./-/g' -e 's;/;-;g' -e 's/--/-/g' -e 's/_/-/g'`"
         full_bucket_url="${asset_bucket}.${S3_HOST_BASE}"
-        /bin/cat ${HOME}/webserver/configuration/reverseproxy/apache/redirection-template.conf >> ${HOME}/runtime/redirection.conf
+        /bin/cp ${HOME}/webserver/configuration/reverseproxy/apache/redirection-template.conf ${HOME}/runtime/redirection.conf
         /bin/sed -i "s/XXXXASSETSXXXX/${application_assets_directory}/" ${HOME}/runtime/redirection.conf
         /bin/sed -i "s/XXXXS3_HOST_URLXXXX/${full_bucket_url}/" ${HOME}/runtime/redirection.conf
 		/bin/sed -i "s/XXXXWEBSITE_URLXXXX/${WEBSITE_URL}/g" ${HOME}/runtime/redirection.conf
