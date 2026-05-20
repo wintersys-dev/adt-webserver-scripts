6fc045c9442c5d7d35a0dd49b7776f3049af2366
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-20 22:10:34 +0100

    Update SetupAssetsRedirection.sh

--- services/datastore/assets/SetupAssetsRedirection.sh
+++ services/datastore/assets/SetupAssetsRedirection.sh
@@ -40,18 +40,18 @@ do
         #s3cmd is the only tool I use (I think) that can set bucket policies so we cludge it to make it so
         if ( [ "${asset_bucket}" != "" ] && [ -f /usr/bin/s3cmd ] && [ "`/usr/bin/hostname | /bin/grep "\-rp-"`" != "" ] )
         then
-                reverse_proxy_ips="`${HOME}/services/datastore/config/wrapper/ListFromDatastore.sh "config" "reverseproxypublicips/*"`"
+               # reverse_proxy_ips="`${HOME}/services/datastore/config/wrapper/ListFromDatastore.sh "config" "reverseproxypublicips/*"`"
                 /bin/cp ${HOME}/services/datastore/assets/policy/policy-template.json ${HOME}/runtime/datastore_workarea/policy-${asset_bucket}.json
                 /bin/sed -i "s/XXXXBUCKET_NAMEXXXX/${asset_bucket}/g" ${HOME}/runtime/datastore_workarea/policy-${asset_bucket}.json
                 /bin/sed -i "s/XXXXWEBSITE_URLXXXX/${WEBSITE_URL}/g" ${HOME}/runtime/datastore_workarea/policy-${asset_bucket}.json
 
 
-                for ip in ${reverse_proxy_ips}
-                do
-                        /bin/sed -i '/XXXXRP_PUBLIC_IPXXXX/a "'${ip}'/32",' ${HOME}/runtime/datastore_workarea/policy-${asset_bucket}.json
-                done
-                /bin/sed -zi 's/\(.*\),/\1/' ${HOME}/runtime/datastore_workarea/policy-${asset_bucket}.json
-                /bin/sed -i 's/XXXXRP_PUBLIC_IPXXXX//g' ${HOME}/runtime/datastore_workarea/policy-${asset_bucket}.json
+                #for ip in ${reverse_proxy_ips}
+                #do
+                #        /bin/sed -i '/XXXXRP_PUBLIC_IPXXXX/a "'${ip}'/32",' ${HOME}/runtime/datastore_workarea/policy-${asset_bucket}.json
+                #done
+                #/bin/sed -zi 's/\(.*\),/\1/' ${HOME}/runtime/datastore_workarea/policy-${asset_bucket}.json
+                #/bin/sed -i 's/XXXXRP_PUBLIC_IPXXXX//g' ${HOME}/runtime/datastore_workarea/policy-${asset_bucket}.json
                 /usr/bin/s3cmd setpolicy ${HOME}/runtime/datastore_workarea/policy-${asset_bucket}.json s3://${asset_bucket}
 
         fi
