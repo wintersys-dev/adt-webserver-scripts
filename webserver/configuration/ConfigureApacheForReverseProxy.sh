0df31d16509fd8255c94b586c04ad863aaf0da3f
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-21 10:03:03 +0100

    Update ConfigureApacheForReverseProxy.sh

--- webserver/configuration/ConfigureApacheForReverseProxy.sh
+++ webserver/configuration/ConfigureApacheForReverseProxy.sh
@@ -114,7 +114,7 @@ then
         /bin/cp ${HOME}/webserver/configuration/reverseproxy/apache/redirection-template.conf ${HOME}/runtime/redirection.conf
         /bin/sed -i "s/XXXXASSETSXXXX/${application_assets_directory}/" ${HOME}/runtime/redirection.conf
         /bin/sed -i "s/XXXXS3_HOST_URLXXXX/${full_bucket_url}/" ${HOME}/runtime/redirection.conf
-		/bin/sed -i "s/XXXXWEBSITE_URLXXXX/${WEBSITE_URL}-${S3_ACCESS_KEY}/g" ${HOME}/runtime/redirection.conf
+		/bin/sed -i "s/XXXXREFERERXXXX/${WEBSITE_URL}-${S3_ACCESS_KEY}/g" ${HOME}/runtime/redirection.conf
         /bin/sed -i -e "/#XXXXS3_REDIRECTIONXXXX/{r ${HOME}/runtime/redirection.conf" -e 'd}' ${HOME}/webserver/configuration/reverseproxy/apache/site-available.conf    
 		/bin/rm ${HOME}/runtime/redirection.conf 
 	done
