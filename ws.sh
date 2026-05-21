2dac16df4fac6be829395343aac1e1dda737d200
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-21 10:06:34 +0100

    Update ws.sh

--- ws.sh
+++ ws.sh
@@ -215,11 +215,11 @@ cd ${HOME}
 #/bin/echo "${0} Determining application type"
 #${HOME}/application/processing/DetermineApplicationType.sh 
 
-if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh PERSISTASSETSTODATASTORE:1`" = "1" ] )
-then
-	/bin/echo "${0} Setup Assets Store"
-	${HOME}/services/datastore/assets/SetupAssetsStore.sh
-fi
+#if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh PERSISTASSETSTODATASTORE:1`" = "1" ] )
+#then
+#	/bin/echo "${0} Setup Assets Store"
+#	${HOME}/services/datastore/assets/SetupAssetsStore.sh
+#fi
 
 /bin/echo "${0} Initialising crontab"
 ${HOME}/services/cron/InitialiseCron.sh
