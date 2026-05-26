93b9d29ad72c4f91a76159d06b8494b67e253f86
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-26 11:02:05 +0100

    Update ConfigureNginxForReverseProxy.sh

--- webserver/configuration/ConfigureNginxForReverseProxy.sh
+++ webserver/configuration/ConfigureNginxForReverseProxy.sh
@@ -58,7 +58,7 @@ fi
 
 /bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" ${HOME}/webserver/configuration/reverseproxy/nginx/site-available.conf
 /bin/sed -i "s;XXXXHOMEXXXX;${HOME};g" ${HOME}/webserver/configuration/reverseproxy/nginx/site-available.conf
-/bin/sed -i "s;XXXXVPC_IP_RANGEXXXX;${VPC_IP_RANGE};g" ${HOME}/webserver/configuration/reverseproxy/nginx/site-available.conf
+#/bin/sed -i "s;XXXXVPC_IP_RANGEXXXX;${VPC_IP_RANGE};g" ${HOME}/webserver/configuration/reverseproxy/nginx/site-available.conf
 /bin/sed -i "s/XXXXBUILD_MACHINE_IPXXXX/${BUILD_MACHINE_IP}/g" ${HOME}/webserver/configuration/reverseproxy/nginx/site-available.conf
 
 
