6fb3f5cd6165a52f94ef46465904b191613c3d6b
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-28 11:39:10 +0100

    Update ConfigureApacheForAuthenticator.sh

--- webserver/configuration/ConfigureApacheForAuthenticator.sh
+++ webserver/configuration/ConfigureApacheForAuthenticator.sh
@@ -161,21 +161,6 @@ then
         /bin/mkdir /var/www/basic-auth
         /bin/chown www-data:www-data /var/www/basic-auth
 	fi
-elif ( [ "${AUTHENTICATOR_TYPE}" = "wire-guard" ] )
-then
-	/bin/cp ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/index.html /var/www/html/index.html
-	/bin/cp ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/submit.php /var/www/html/submit.php
-	/bin/cp ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/submit1.php /var/www/html/submit1.php
-	/bin/chown www-data:www-data /var/www/html/*
-	/bin/chmod 644 /var/www/html/*
-	/bin/sed -i "s/XXXXUSEREMAILDOMAINXXXX/${USER_EMAIL_DOMAIN}/g" /var/www/html/index.html
-	/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /var/www/html/index.html
-        
-	if ( [ ! -d /var/www/wire-guard ] )
-	then
-		/bin/mkdir /var/www/wire-guard
-		/bin/chown www-data:www-data /var/www/wire-guard
-    fi
 fi
 
 
