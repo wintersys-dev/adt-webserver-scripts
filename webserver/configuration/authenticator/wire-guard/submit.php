5ec46de2e900354669e38cf9dd1422632494b5d7
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-28 11:30:51 +0100

    Delete webserver/configuration/authenticator/wire-guard/submit.php

--- webserver/configuration/authenticator/wire-guard/submit.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $email = $_POST["email"];
-  $file="/var/www/wire-guard/authentication-emails.dat";
-  $data = "$email\n";
-  file_put_contents($file, $data, FILE_APPEND );
-  echo "<h2>Email Address Submitted, if needed you should shortly receive an authentication email at $email.</h2>";
-  echo "<h2>The Link in the email will be valid for 5 minutes.</h2>";
-
-?>
