1cd49c42261d6927ea5162625b131a862f284033
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-29 07:39:08 +0100

    Update AcceptEmailAddresses.sh

--- webserver/configuration/authenticator/wire-guard/AcceptEmailAddresses.sh
+++ webserver/configuration/authenticator/wire-guard/AcceptEmailAddresses.sh
@@ -1,1 +1,11 @@
+if ( [ ! -d ${HOME}/runtime/authenticator ] )
+then
+        /bin/mkdir -p ${HOME}/runtime/authenticator/incoming
+fi
+
+if ( [ -f /var/www/wire-guard/authentication-emails.dat ] )
+then
+        /bin/mv /var/www/wire-guard/authentication-emails.dat ${HOME}/runtime/authenticator/incoming/authentication-emails.dat
+fi
+
 
