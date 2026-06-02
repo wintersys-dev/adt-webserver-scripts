8ff58c7e2fbc0fa4321f319f001b6de5eddfe191
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-02 11:20:54 +0100

    Update InitialiseCron.sh

--- services/cron/InitialiseCron.sh
+++ services/cron/InitialiseCron.sh
@@ -59,7 +59,14 @@ then
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 30 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateBasicAuthConfig.sh" >> /var/spool/cron/crontabs/root
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 40 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateBasicAuthConfig.sh" >> /var/spool/cron/crontabs/root
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 50 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateBasicAuthConfig.sh" >> /var/spool/cron/crontabs/root	
+	elif ( [ "${AUTHENTICATOR_TYPE}" = "wire-guard" ] )
+    then
+    	if ( [ "`/usr/bin/hostname | /bin/grep '^NO-1'`" != "" ] )
+        then                        
+			/bin/echo "*/1 * * * * export HOME="${HOME}" && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/AcceptEmailAddresses.sh" >> /var/spool/cron/crontabs/root
+        fi	
 	fi
+	
 	/bin/echo "22 4 * * * export HOME="${HOME}" && ${HOME}/utilities/software/UpdateSoftware.sh" >> /var/spool/cron/crontabs/root
 	/bin/echo "45 4 * * * export HOME="${HOME}" && /bin/rm ${HOME}/runtime/FIREWALL-ACTIVE" >> /var/spool/cron/crontabs/root
 
