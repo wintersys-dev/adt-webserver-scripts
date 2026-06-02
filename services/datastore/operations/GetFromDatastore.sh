0fe886954fe92bf7c1f6071b6df07c24883fec7e
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-02 11:33:11 +0100

    Update GetFromDatastore.sh

--- services/datastore/operations/GetFromDatastore.sh
+++ services/datastore/operations/GetFromDatastore.sh
@@ -80,6 +80,9 @@ elif ( [ "${bucket_type}" = "snap" ] )
 then
         active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`"
         active_bucket="${active_bucket}-${DNS_CHOICE}-snap"
+elif ( [ "${bucket_type}" = "authentication-emails" ] )
+then
+        active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-authentication-emails"  
 fi
 
 datastore_tool=""
