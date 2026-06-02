7ed7f81c6b67a4d8c5875a55538b5f4461099158
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-02 08:31:07 +0100

    Update AcceptEmailAddresses.sh

--- webserver/configuration/authenticator/wire-guard/AcceptEmailAddresses.sh
+++ webserver/configuration/authenticator/wire-guard/AcceptEmailAddresses.sh
@@ -119,5 +119,6 @@ do
                 PersistentKeepalive = 25" >> ${HOME}/runtime/authenticator/wire-guard/client/${email_address}/client.conf
 
                 /usr/bin/qrencode -t png -o ${HOME}/runtime/authenticator/wire-guard/client/${email_address}/client.png -r ${HOME}/runtime/authenticator/wire-guard/client/${email_address}/client.conf
+                /bin/touch ${HOME}/runtime/authenticator/wire-guard/client/${email_address}/CANDIDATE_QR_CODE
         fi
 done
