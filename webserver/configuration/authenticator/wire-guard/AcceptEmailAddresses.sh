b9aae3365d030689abb56d264931987dc62f7f48
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-02 11:35:34 +0100

    Update AcceptEmailAddresses.sh

--- webserver/configuration/authenticator/wire-guard/AcceptEmailAddresses.sh
+++ webserver/configuration/authenticator/wire-guard/AcceptEmailAddresses.sh
@@ -1,4 +1,4 @@
 
 
-
+${HOME}/services/datastore/operations/MountDatastore.sh "authentication-emails" "distributed"
 ${HOME}/services/datastore/operations/PutToDatastore.sh "authentication-emails" /var/www/wire-guard/authentication-emails.dat "" "distributed" "no"
