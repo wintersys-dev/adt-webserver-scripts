9e2687c46eab00188c0d011cc82376fa189f5ed9
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-03 17:02:14 +0100

    Update ConfigureNginxForAuthenticator.sh

--- webserver/configuration/ConfigureNginxForAuthenticator.sh
+++ webserver/configuration/ConfigureNginxForAuthenticator.sh
@@ -182,6 +182,7 @@ then
         	/bin/chmod 644 /var/www/html/*
         	/bin/sed -i "s/XXXXUSEREMAILDOMAINXXXX/${USER_EMAIL_DOMAIN}/g" /var/www/html/index.html
         	/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /var/www/html/index.html
+			/bin/sed -i 's/#XXXXWEBROOTXXX//g' /etc/nginx/sites-available/${WEBSITE_NAME}
 
         	if ( [ ! -d /var/www/wire-guard ] )
         	then
