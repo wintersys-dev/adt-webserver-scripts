1edebc77841a01c40de18ce37cfa4a557303106f
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-04 15:20:53 +0100

    Update GenerateClientInterfaceAndPeer.sh

--- webserver/configuration/reverseproxy/wire-guard/GenerateClientInterfaceAndPeer.sh
+++ webserver/configuration/reverseproxy/wire-guard/GenerateClientInterfaceAndPeer.sh
@@ -5,30 +5,40 @@ wireguard_port="`/usr/bin/expr ${SSH_PORT} + 1`"
 
 endpoint="`${HOME}/utilities/processing/GetPublicIP.sh`"
 
-if ( [ -f ${HOME}/runtime/wire-guard/client/${email_address}/client_private.key ] )
+if ( [ ! -f ${HOME}/runtime/wire-guard/client/${email_address}/client.conf ] )
 then
-  client_private_key="`/bin/cat ${HOME}/runtime/wire-guard/client/${email_address}/client_private.key`"
-fi
 
-if ( [ -f ${HOME}/runtime/wire-guard/client/${email_address}/CLIENT_NO ] )
-then
-  client_no="`/bin/cat ${HOME}/runtime/wire-guard/client/${email_address}/CLIENT_NO`"
-fi
-                
-if ( [ -f ${HOME}/runtime/wire-guard/server/server_public.key ] )
-then
-  server_public_key="`/bin/cat ${HOME}/runtime/wire-guard/server/server_public.key`"
-fi
-                
-if ( [ -f ${HOME}/runtime/wire-guard/client/${email_address}/preshared.key ] )
-then
-  preshared_key="`/bin/cat ${HOME}/runtime/wire-guard/client/${email_address}/preshared.key`"
-fi
+  if ( [ -f ${HOME}/runtime/wire-guard/client/${email_address}/client_private.key ] )
+  then
+    client_private_key="`/bin/cat ${HOME}/runtime/wire-guard/client/${email_address}/client_private.key`"
+  fi
 
-# Create client config
-/bin/echo "[Interface]
+  if ( [ -f ${HOME}/runtime/wire-guard/client/${email_address}/CLIENT_NO ] )
+  then
+    client_no="`/bin/cat ${HOME}/runtime/wire-guard/client/${email_address}/CLIENT_NO`"  
+  fi
+                
+  if ( [ -f ${HOME}/runtime/wire-guard/server/server_public.key ] )
+  then
+    server_public_key="`/bin/cat ${HOME}/runtime/wire-guard/server/server_public.key`"
+  fi
+                
+  if ( [ -f ${HOME}/runtime/wire-guard/client/${email_address}/preshared.key ] )
+  then
+    preshared_key="`/bin/cat ${HOME}/runtime/wire-guard/client/${email_address}/preshared.key`"
+  fi
+
+  twenty_four="`/usr/bin/expr ${client_no} / 255`"
+  iteration1="`/usr/bin/expr ${twenty_four} \* 255`"
+  thirty_two="`/usr/bin/expr ${client_no} - ${iteration1}`"
+  sixteen="`/usr/bin/expr ${twenty_four} / 255`"
+  iteration2="`/usr/bin/expr ${sixteen} \* 255`"
+  twenty_four="`/usr/bin/expr ${twenty_four} - ${iteration2}`"
+
+  # Create client config
+  /bin/echo "[Interface]
   PrivateKey = ${client_private_key}
-  Address = 10.0.0.${client_no}/32
+  Address = 10.${sixteen}.${twenty_four}.${thirty_two}/32
   MTU = 1380
   DNS = 1.1.1.1, 1.0.0.1 
 
@@ -38,3 +48,4 @@ fi
   Endpoint = ${endpoint}:${wireguard_port}
   AllowedIPs =  10.0.0.`/usr/bin/hostname | /usr/bin/awk -F'-' '{print $2}'`/32,10.0.0.0/8
   PersistentKeepalive = 25" >> ${HOME}/runtime/wire-guard/client/${email_address}/client.conf
+fi
