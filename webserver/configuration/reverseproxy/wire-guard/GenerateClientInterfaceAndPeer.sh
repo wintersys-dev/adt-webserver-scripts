4c4799c60e68bc97e15455850948b6864ae55c71
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-04 15:16:53 +0100

    Update GenerateClientInterfaceAndPeer.sh

--- webserver/configuration/reverseproxy/wire-guard/GenerateClientInterfaceAndPeer.sh
+++ webserver/configuration/reverseproxy/wire-guard/GenerateClientInterfaceAndPeer.sh
@@ -1,4 +1,10 @@
 
+export HOME="`/bin/cat /home/homedir.dat`"
+SSH_PORT="`${HOME}/utilities/config/ExtractConfigValue.sh 'SSHPORT'`"
+wireguard_port="`/usr/bin/expr ${SSH_PORT} + 1`"
+
+endpoint="`${HOME}/utilities/processing/GetPublicIP.sh`"
+
 if ( [ -f ${HOME}/runtime/wire-guard/client/${email_address}/client_private.key ] )
 then
   client_private_key="`/bin/cat ${HOME}/runtime/wire-guard/client/${email_address}/client_private.key`"
@@ -29,6 +35,6 @@ fi
 [Peer]
   PublicKey = ${server_public_key}
   PresharedKey = ${preshared_key}
-  Endpoint = ${server_ip}:${wireguard_port}
+  Endpoint = ${endpoint}:${wireguard_port}
   AllowedIPs =  10.0.0.`/usr/bin/hostname | /usr/bin/awk -F'-' '{print $2}'`/32,10.0.0.0/8
   PersistentKeepalive = 25" >> ${HOME}/runtime/wire-guard/client/${email_address}/client.conf
