5da41e3f9155fbea82f2b51916a7d92af9072ce5
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-04 11:37:06 +0100

    Update AcceptEmailAddresses.sh

--- webserver/configuration/authenticator/wire-guard/AcceptEmailAddresses.sh
+++ webserver/configuration/authenticator/wire-guard/AcceptEmailAddresses.sh
@@ -4,12 +4,14 @@ then
   if ( [ "`/bin/grep '@' /var/www/wire-guard/authentication-emails.dat`" != "" ] )
   then
     rnd="`/usr/bin/shuf -i 1-100000000000 -n 1`"
-    if ( [ ! -d ${HOME}/runtime/authenticator ] )
+    
+    if ( [ ! -d ${HOME}/runtime/wire-guard/emails ] )
     then
-      /bin/mkdir -p ${HOME}/runtime/authenticator
+      /bin/mkdir -p ${HOME}/runtime/wire-guard/emails
     fi
-    /bin/mv /var/www/wire-guard/authentication-emails.dat ${HOME}/runtime/authenticator/authentication-emails.dat.${rnd}
+    
+    /bin/mv /var/www/wire-guard/authentication-emails.dat ${HOME}/runtime/wire-guard/emails/authentication-emails.dat.${rnd}
     ${HOME}/services/datastore/operations/MountDatastore.sh "wire-guard-emails" "distributed"
-    ${HOME}/services/datastore/operations/PutToDatastore.sh "wire-guard-emails" ${HOME}/runtime/authenticator/authentication-emails.dat.${rnd} "" "distributed" "no"
+    ${HOME}/services/datastore/operations/PutToDatastore.sh "wire-guard-emails" ${HOME}/runtime/wire-guard/emails/authentication-emails.dat.${rnd} "" "distributed" "no"
   fi
 fi
