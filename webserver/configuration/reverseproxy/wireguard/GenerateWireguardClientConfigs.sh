980a8198a69ecd123a54ce5654ce3d17a37ea1ef
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-04 12:21:24 +0100

    Update GenerateWireguardClientConfigs.sh

--- webserver/configuration/reverseproxy/wireguard/GenerateWireguardClientConfigs.sh
+++ webserver/configuration/reverseproxy/wireguard/GenerateWireguardClientConfigs.sh
@@ -5,6 +5,12 @@ then
   for email_address in `/bin/cat ${HOME}/runtime/wire-guard/emails/processing/to_process_authentication_emails.dat`
   do
 
+  #Generate the client configs for this machine
+
+  #Create CANDIDATE file to associate with the new client configs
+
+  #Sync the client configs to the datastore and process them on the authenticator
+
   done
 
   /bin/cat ${HOME}/runtime/wire-guard/emails/processing/to_process_authentication_emails.dat >> ${HOME}/runtime/wire-guard/emails/processing/processed_authentication_emails.dat
