319855c214006572c375080cd2d9b88264564d25
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-04 15:09:42 +0100

    Update GenerateClientInterfaceAndPeer.sh

--- webserver/configuration/reverseproxy/wire-guard/GenerateClientInterfaceAndPeer.sh
+++ webserver/configuration/reverseproxy/wire-guard/GenerateClientInterfaceAndPeer.sh
@@ -1,13 +1,13 @@
 # Create client config
 /bin/echo "[Interface]
-                PrivateKey = ${client_private_key}
-                Address = 10.0.0.${client_no}/32
-                MTU = 1380
-                DNS = 1.1.1.1, 1.0.0.1 
+  PrivateKey = ${client_private_key}
+  Address = 10.0.0.${client_no}/32
+  MTU = 1380
+  DNS = 1.1.1.1, 1.0.0.1 
 
-                [Peer]
-                PublicKey = ${server_public_key}
-                PresharedKey = ${preshared_key}
-                Endpoint = ${server_ip}:${wireguard_port}
-                AllowedIPs =  10.0.0.`/usr/bin/hostname | /bin/sed -e 's/NO-//g' -e 's/-.*//g'`/32,10.0.0.0/8
-                PersistentKeepalive = 25" >> ${HOME}/runtime/authenticator/wire-guard/client/${email_address}/client.conf
+[Peer]
+  PublicKey = ${server_public_key}
+  PresharedKey = ${preshared_key}
+  Endpoint = ${server_ip}:${wireguard_port}
+  AllowedIPs =  10.0.0.`/usr/bin/hostname | /usr/bin/awk -F'-' '{print $2}'`/32,10.0.0.0/8
+  PersistentKeepalive = 25" >> ${HOME}/runtime/authenticator/wire-guard/client/${email_address}/client.conf
