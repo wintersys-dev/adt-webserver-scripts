004840f72b295e2cee59e022408c57993071705c
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-04 12:23:47 +0100

    Update GenerateWireguardServerConfig.sh

--- webserver/configuration/reverseproxy/wireguard/GenerateWireguardServerConfig.sh
+++ webserver/configuration/reverseproxy/wireguard/GenerateWireguardServerConfig.sh
@@ -1,1 +1,5 @@
 
+#Don't need to put server file onto the authenticator machine just install it and update it on the current reverse proxy
+#configure wg1 for authenticator 1 ?
+#wg2 for authenticator 2?
+#wg3 for authenticator 3?
