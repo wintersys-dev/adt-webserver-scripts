0d274cea81f65e60cd384fcf7c4ad52f514cce91
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-04 11:45:29 +0100

    Update ObtainWireguardEmails.sh

--- webserver/configuration/reverseproxy/wireguard/ObtainWireguardEmails.sh
+++ webserver/configuration/reverseproxy/wireguard/ObtainWireguardEmails.sh
@@ -1,1 +1,23 @@
+if ( [ ! -d ${HOME}/runtime/wire-guard/emails/incoming ] )
+then
+  /bin/mkdir -p ${HOME}/runtime/wire-guard/emails/incoming
+fi
 
+${HOME}/services/datastore/operations/SyncFromDatastore.sh "wire-guard-emails" "${HOME}/runtime/wire-guard/emails/incoming"
+
+
+/bin/cat ${HOME}/runtime/wire-guard/emails/authentication-emails* > ${HOME}/runtime/wire-guard/emails/incoming/all_authentication-emails.dat
+
+if ( [ ! -d ${HOME}/runtime/wire-guard/emails/processing ] )
+then
+  /bin/mkdir -p ${HOME}/runtime/wire-guard/emails/processing
+fi
+
+if ( [ -f ${HOME}/runtime/wire-guard/emails/processing/processed_authentication_emails.dat ] )
+then
+    /bin/grep -vf ${HOME}/runtime/wire-guard/emails/incoming/all_authentication-emails.dat ${HOME}/runtime/wire-guard/emails/processing/processed_authentication_emails.dat > ${HOME}/runtime/wire-guard/emails/processing/to_process_authentication_emails.dat 
+else
+    /bin/cp ${HOME}/runtime/wire-guard/emails/incoming/all_authentication-emails.dat ${HOME}/runtime/wire-guard/emails/processing/to_process_authentication_emails.dat
+fi
+
+/bin/mv ${HOME}/runtime/wire-guard/emails/incoming/all_authentication-emails.dat ${HOME}/runtime/wire-guard/emails/processing/to_process_authentication_emails.dat
