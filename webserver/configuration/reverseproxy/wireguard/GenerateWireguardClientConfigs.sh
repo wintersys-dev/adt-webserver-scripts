6cceac1b13d53852ca29c5a65bd8102c4367998e
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-04 12:15:32 +0100

    Update GenerateWireguardClientConfigs.sh

--- webserver/configuration/reverseproxy/wireguard/GenerateWireguardClientConfigs.sh
+++ webserver/configuration/reverseproxy/wireguard/GenerateWireguardClientConfigs.sh
@@ -1,1 +1,9 @@
 
+
+if ( [ -f ${HOME}/runtime/wire-guard/emails/processing/to_process_authentication_emails.dat ] )
+then
+  for email_address in `/bin/cat ${HOME}/runtime/wire-guard/emails/processing/to_process_authentication_emails.dat`
+  do
+
+  done
+fi
