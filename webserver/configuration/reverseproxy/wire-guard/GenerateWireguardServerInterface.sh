78e70b3c0104b89811bbd76f319870ad0827901a
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-04 14:48:19 +0100

    Update GenerateWireguardServerInterface.sh

--- webserver/configuration/reverseproxy/wire-guard/GenerateWireguardServerInterface.sh
+++ webserver/configuration/reverseproxy/wire-guard/GenerateWireguardServerInterface.sh
@@ -21,17 +21,22 @@ then
         /bin/sed -i "s/XXXXWG_PORTXXXX/${wireguard_port}/g" /etc/wireguard/postdown.sh
 fi
 
+if ( [ ! -d ${HOME}/runtime/wire-guard/server ] )
+then
+        /bin/mkdir -p ${HOME}/runtime/wire-guard/server
+fi
+
 if ( [ ! -f /etc/wireguard/wg0.conf ] )
 then
-        if ( [ ! -f ${HOME}/runtime//wire-guard/${authenticator_ip}/server_private.key ] )
+        if ( [ ! -f ${HOME}/runtime/wire-guard/server/server_private.key ] )
         then
                 umask 077
-                /usr/bin/wg genkey > ${HOME}/runtime/authenticator/wire-guard/${authenticator_ip}/server_private.key
-                /bin/chmod 600 ${HOME}/runtime/authenticator/wire-guard/${authenticator_ip}/server_private.key
-                /bin/cat ${HOME}/runtime/authenticator/wire-guard/${authenticator_ip}/server_private.key | /usr/bin/wg pubkey > ${HOME}/runtime/authenticator/wire-guard/${authenticator_ip}/server_public.key
+                /usr/bin/wg genkey > ${HOME}/runtime/wire-guard/server/server_private.key
+                /bin/chmod 600 ${HOME}/runtime/wire-guard/server/server_private.key
+                /bin/cat ${HOME}/runtime//wire-guard/server/server_private.key | /usr/bin/wg pubkey > ${HOME}/runtime//wire-guard/server/server_public.key
 
-                server_private_key="`/bin/cat ${HOME}/runtime/authenticator/wire-guard/${authenticator_ip}/server_private.key`"
-                server_public_key="`/bin/cat ${HOME}/runtime/authenticator/wire-guard/${authenticator_ip}/server_public.key`"
+                server_private_key="`/bin/cat ${HOME}/runtime//wire-guard/server/server_private.key`"
+                server_public_key="`/bin/cat ${HOME}/runtime//wire-guard/server/server_public.key`"
 
                 /bin/echo "[Interface]
                 PrivateKey = ${server_private_key}
