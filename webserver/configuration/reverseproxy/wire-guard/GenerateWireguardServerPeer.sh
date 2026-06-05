4e64b06c47ae9d45d3cc0c2949168d71ce9f7a1f
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-05 16:23:09 +0100

    Update GenerateWireguardServerPeer.sh

--- webserver/configuration/reverseproxy/wire-guard/GenerateWireguardServerPeer.sh
+++ webserver/configuration/reverseproxy/wire-guard/GenerateWireguardServerPeer.sh
@@ -1,4 +1,9 @@
 
+if ( [ ! -d ${HOME}/runtime/wire-guard/emails/processing ] )
+then
+	/bin/mkdir -p ${HOME}/runtime/wire-guard/emails/processing
+fi
+
 if ( [ -f ${HOME}/runtime/wire-guard/emails/processing/to_process_authentication_emails.dat ] )
 then
 	for email_address in `/bin/cat ${HOME}/runtime/wire-guard/emails/processing/to_process_authentication_emails.dat`
