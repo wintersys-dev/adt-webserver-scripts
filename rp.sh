c414945b77d0bc37ca989d0a0b79e1653b3e8978
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-05 11:56:51 +0100

    Update rp.sh

--- rp.sh
+++ rp.sh
@@ -173,7 +173,7 @@ then
 fi
 
 /bin/echo "${0} `/bin/date`: Setting up the Firewall" 
-${HOME}/services/security/SetupFirewall.sh
+${HOME}/services/security/firewall/SetupFirewall.sh
 
 cd ${HOME}
 
