bf3cc2ac06b562a77d9451656a2404916c298926
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-08 10:49:12 +0100

    Update ObtainEmails.sh

--- webserver/configuration/reverseproxy/wire-guard/ObtainEmails.sh
+++ webserver/configuration/reverseproxy/wire-guard/ObtainEmails.sh
@@ -1,28 +1,25 @@
 if ( [ ! -d ${HOME}/runtime/wire-guard/emails/incoming ] )
 then
-  /bin/mkdir -p ${HOME}/runtime/wire-guard/emails/incoming
+        /bin/mkdir -p ${HOME}/runtime/wire-guard/emails/incoming
 fi
 
 ${HOME}/services/datastore/operations/SyncFromDatastore.sh "wire-guard-emails" "${HOME}/runtime/wire-guard/emails/incoming"
-
-
 /bin/cat ${HOME}/runtime/wire-guard/emails/incoming/authentication-emails* > ${HOME}/runtime/wire-guard/emails/incoming/all_authentication-emails.dat
-
 /usr/bin/sort -u ${HOME}/runtime/wire-guard/emails/incoming/all_authentication-emails.dat | /bin/sed '/^$/d' >  ${HOME}/runtime/wire-guard/emails/incoming/all_authentication-emails.dat.$$
 /bin/mv ${HOME}/runtime/wire-guard/emails/incoming/all_authentication-emails.dat.$$ ${HOME}/runtime/wire-guard/emails/incoming/all_authentication-emails.dat
 
 if ( [ ! -d ${HOME}/runtime/wire-guard/emails/processing ] )
 then
-  /bin/mkdir -p ${HOME}/runtime/wire-guard/emails/processing
+        /bin/mkdir -p ${HOME}/runtime/wire-guard/emails/processing
 fi
 
 /bin/touch ${HOME}/runtime/wire-guard/emails/incoming/all_authentication-emails.dat
 
 if ( [ -f ${HOME}/runtime/wire-guard/emails/processing/processed_authentication_emails.dat ] )
 then
-    /bin/grep -vf ${HOME}/runtime/wire-guard/emails/incoming/all_authentication-emails.dat ${HOME}/runtime/wire-guard/emails/processing/processed_authentication_emails.dat > ${HOME}/runtime/wire-guard/emails/processing/to_process_authentication_emails.dat 
+        /bin/grep -vf ${HOME}/runtime/wire-guard/emails/processing/processed_authentication_emails.dat ${HOME}/runtime/wire-guard/emails/incoming/all_authentication-emails.dat > ${HOME}/runtime/wire-guard/emails/processing/to_process_authentication_emails.dat 
 else
-    /bin/cp ${HOME}/runtime/wire-guard/emails/incoming/all_authentication-emails.dat ${HOME}/runtime/wire-guard/emails/processing/to_process_authentication_emails.dat
+        /bin/cp ${HOME}/runtime/wire-guard/emails/incoming/all_authentication-emails.dat ${HOME}/runtime/wire-guard/emails/processing/to_process_authentication_emails.dat
 fi
 
 
