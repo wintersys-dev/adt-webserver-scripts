7e95caf65be702e9f40585a5f1089a985b0b8450
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-08 10:49:43 +0100

    Update rp.sh

--- rp.sh
+++ rp.sh
@@ -172,7 +172,7 @@ then
 	${HOME}/installation/InstallWireguard.sh
 	${HOME}/webserver/configuration/reverseproxy/wire-guard/GenerateServerInterface.sh
 	${HOME}/services/datastore/operations/DeleteFromDatastore.sh "wire-guard"  "delete-all" "local"
-	${HOME}/services/datastore/operations/DeleteFromDatastore.sh "wire-guard-emails"  "delete-all" "local"
+	#${HOME}/services/datastore/operations/DeleteFromDatastore.sh "wire-guard-emails"  "delete-all" "local"
 fi
 
 /bin/echo "${0} `/bin/date`: Setting up the Firewall" 
