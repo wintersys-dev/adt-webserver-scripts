41416c85565460798b78a88963f928a0c0fdacb5
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-09 09:45:01 +0100

    Update GenerateAuthenticatorEmails.sh

--- webserver/configuration/authenticator/wire-guard/GenerateAuthenticatorEmails.sh
+++ webserver/configuration/authenticator/wire-guard/GenerateAuthenticatorEmails.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+set -x
+
 if ( [ ! -d ${HOME}/runtime/wire-guard/configs ] )
 then
         /bin/mkdir -p ${HOME}/runtime/wire-guard/configs
 fi
+
+WEBSITE_URL_ORIGINAL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURLORIGINAL'`"
 
 ${HOME}/services/datastore/operations/SyncFromDatastore.sh "wire-guard" "${HOME}/runtime/wire-guard/configs"
 reverse_proxy_ips="`/bin/ls ${HOME}/runtime/wire-guard/configs`"
@@ -13,18 +17,19 @@ do
         email_addresses="`/bin/ls ${HOME}/runtime/wire-guard/configs/${ip}`"
         for email_address in ${email_addresses}
         do
-                if ( [ ! -f  ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf ] )
+                if ( [ ! -f  ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf ] && [ ! -f ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/qrcode.png ] )
                 then
                         /bin/cp ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client_interface.conf ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf 
                         /bin/echo "" >> ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf
                         /bin/cat ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client_peer.conf >> ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf 
-                        /usr/bin/qrencode -t png -o ${HOME}/runtime/wire-guard/client/${ip}/${email_address}/qrcode.png -r ${HOME}/runtime/wire-guard/client/${ip}/${email_address}/client.conf
-                        /bin/touch ${HOME}/runtime/wire-guard/client/${ip}/${email_address}/CANDIDATE_QR_CODE
+                        /usr/bin/qrencode -t png -o ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/qrcode.png -r ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf
+                        /bin/touch ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/CANDIDATE_QR_CODE
                 fi
         done
 done
 
 email_addresses="`/usr/bin/find ${HOME}/runtime/wire-guard/configs -name "CANDIDATE_QR_CODE"  -print | /usr/bin/awk -F'/' '{print $8}'`"
+email_addresses="`/bin/echo ${email_addresses} | /usr/bin/xargs -n1 | /usr/bin/sort -u | /usr/bin/xargs`"
 
 for ip in ${reverse_proxy_ips}
 do
@@ -32,25 +37,25 @@ do
         do
                 file_name="`/usr/bin/openssl rand -base64 32 | /usr/bin/tr -cd 'a-zA-Z0-9' | /usr/bin/cut -b 1-16 | /usr/bin/tr '[:upper:]' '[:lower:]'`"
                 full_file_name="/var/www/html/qrcode-${file_name}-${ip}-${email_address}.png"
-                /bin/cp ${HOME}/runtime/wire-guard/client/${ip}/${email_address}/qrcode.png ${full_file_name}
+                /bin/cp ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/qrcode.png ${full_file_name}
                 full_file_name_html="/var/www/html/client-${file_name}-${ip}-${email_address}.html"
                 /bin/cp ${HOME}/webserver/configuration/authenticator/wire-guard/client_peer_template.html ${full_file_name_html}
-               # /bin/sed -i -e "/XXXXCLIENT_PEERXXXX/{r /etc/wireguard/client_${email_address}.conf" -e 'd}' ${full_file_name_html}
+                /bin/sed -i -e "/XXXXCLIENT_PEERXXXX/{r ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf" -e 'd}' ${full_file_name_html}
 
                 if ( [ ! -f /var/www/html/txtstyle.css ] )
                 then
                         /bin/echo "html, body {font-family:Helvetica, Arial, sans-serif}" > /var/www/html/txtstyle.css
                 fi
 
-                /bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL_ORIGINAL}/g" ${full_file_name}
+                #/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL_ORIGINAL}/g" ${full_file_name}
                 /bin/chmod 600 ${full_file_name}
                 /bin/chmod 600 ${full_file_name_html}
                 /bin/chown www-data:www-data /var/www/html/*
                 qrcode_url="https://${WEBSITE_URL}/qrcode-${file_name}-${ip}-${email_address}.png"
                 client_url="https://${WEBSITE_URL}/client-${file_name}-${ip}-${email_address}.html"
                 message="<!DOCTYPE html> <html> <body> <h1>Wireguard authorisation for ${WEBSITE_URL_ORIGINAL}</h1> <p>Click the below link in order to authorise your wireguard access for ${WEBSITE_URL_ORIGINAL} </p> <a href='"${qrcode_url}"'>View Your Wireguard QR Code</a> <br> <a href='"${client_url}"'>View Your Wireguard QR Client File</a>  </br> </body> </html>"
-                ${HOME}/services/email/SendEmail.sh "Wireguard authorisation for ${WEBSITE_URL_ORIGINAL}" "${message}" MANDATORY ${email_address} "HTML" "AUTHENTICATION"
-                /bin/rm ${HOME}/runtime/authenticator/wire-guard/client/${email_address}/CANDIDATE_QR_CODE
+                ${HOME}/services/email/SendEmail.sh "Wireguard authorisation for ${WEBSITE_URL_ORIGINAL}" "${message}" "MANDATORY" "${email_address}" "HTML" "AUTHENTICATION"
+                /bin/rm ${HOME}/runtime/authenticator/wire-guard/configs/${ip}/${email_address}/CANDIDATE_QR_CODE
         done
 done
 
