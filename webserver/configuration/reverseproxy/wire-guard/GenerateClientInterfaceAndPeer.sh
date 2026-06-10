a268a8450672151391afc7b2783580082cfe0bb8
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-10 16:10:06 +0100

    Update GenerateClientInterfaceAndPeer.sh

--- webserver/configuration/reverseproxy/wire-guard/GenerateClientInterfaceAndPeer.sh
+++ webserver/configuration/reverseproxy/wire-guard/GenerateClientInterfaceAndPeer.sh
@@ -94,14 +94,8 @@ then
                         PublicKey = ${server_public_key}
                         PresharedKey = ${preshared_key}
                         Endpoint = ${endpoint}:${wireguard_port}
-                        AllowedIPs =  10.0.0.0/8
+                        AllowedIPs =  10.`/usr/bin/hostname | /usr/bin/awk -F'-' '{print $2}'`.0.${client_no}/32,10.`/usr/bin/hostname | /usr/bin/awk -F'-' '{print $2}'`.0.0/16
                         PersistentKeepalive = 25" > ${HOME}/runtime/wire-guard/client/${endpoint}/${email_address}/client_peer.conf
-                 #       /bin/echo "[Peer]
-                  #      PublicKey = ${server_public_key}
-                   #     PresharedKey = ${preshared_key}
-                    #    Endpoint = ${endpoint}:${wireguard_port}
-                     #   AllowedIPs =  10.`/usr/bin/hostname | /usr/bin/awk -F'-' '{print $2}'`.0.${client_no}/32,10.`/usr/bin/hostname | /usr/bin/awk -F'-' '{print $2}'`.0.0/16
-                      #  PersistentKeepalive = 25" > ${HOME}/runtime/wire-guard/client/${endpoint}/${email_address}/client_peer.conf
                         /bin/sed -i "/^${email_address}$/d" ${HOME}/runtime/wire-guard/emails/processing/to_process_authentication_emails.dat.client
                         #                       /usr/bin/qrencode -t png -o ${HOME}/runtime/wire-guard/client/${email_address}/qrcode.png -r ${HOME}/runtime/wire-guard/client/${email_address}/client.conf
                 fi
