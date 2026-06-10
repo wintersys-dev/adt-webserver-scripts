279c00e19010ae290fd734dae03dc52437599a76
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-10 12:19:48 +0100

    Update GenerateAuthenticatorEmails.sh

--- webserver/configuration/authenticator/wire-guard/GenerateAuthenticatorEmails.sh
+++ webserver/configuration/authenticator/wire-guard/GenerateAuthenticatorEmails.sh
@@ -24,23 +24,61 @@ WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 /usr/bin/find /var/www/html -mmin +30 -name "*client*" -type f -exec rm -fv {} \;
 
 ${HOME}/services/datastore/operations/SyncFromDatastore.sh "wire-guard" "${HOME}/runtime/wire-guard/configs"
-reverse_proxy_ips="`/bin/ls ${HOME}/runtime/wire-guard/configs`"
 
-for ip in ${reverse_proxy_ips}
+for email_address in ${email_addresses}
 do
-        email_addresses="`/bin/ls ${HOME}/runtime/wire-guard/configs/${ip}`"
-        for email_address in ${email_addresses}
+        config_dirs="`/usr/bin/find ${HOME}/runtime/wire-guard/configs -name "${email_address}" -print`"
+
+        for config_dir in ${config_dirs}
         do
-                if ( [ ! -f  ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf ] && [ ! -f ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/qrcode.png ] )
+                if ( [ -f ${config_dir}/client.conf ] )
                 then
-                        /bin/cp ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client_interface.conf ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf 
-                        /bin/echo "" >> ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf
-                        /bin/cat ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client_peer.conf >> ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf 
-                        /usr/bin/qrencode -t png -o ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/qrcode.png -r ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf
-                        /bin/touch ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/CANDIDATE_QR_CODE
+                        /bin/rm ${config_dir}/client.conf
                 fi
         done
 done
+
+for email_address in ${email_addresses}
+do
+        for config_dir in ${config_dirs}
+        do
+                if ( [ ! -f ${config_dir}/client.conf ] )
+                then
+                        /bin/cp ${config_dir}/client_interface.conf ${config_dir}/client.conf
+                fi
+
+                config_dirs1="`/usr/bin/find ${HOME}/runtime/wire-guard/configs -name "${email_address}" -print`"
+
+                for config_dir1 in ${config_dirs1}
+                do
+                        if ( [ -f ${config_dir1}/client_peer.conf ] )
+                        then
+                                /bin/cat ${config_dir1}/client_peer.conf >> ${config_dir}/client.conf
+                                /bin/echo ${config_dir}/client.conf
+                        fi
+                done
+                /usr/bin/qrencode -t png -o ${config_dir}/qrcode.png -r ${config_dir}/client.conf
+                /bin/touch ${config_dir}/CANDIDATE_QR_CODE
+        done
+done
+
+#reverse_proxy_ips="`/bin/ls ${HOME}/runtime/wire-guard/configs`"
+
+#for ip in ${reverse_proxy_ips}
+#do
+#        email_addresses="`/bin/ls ${HOME}/runtime/wire-guard/configs/${ip}`"
+#        for email_address in ${email_addresses}
+#        do
+#                if ( [ ! -f  ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf ] && [ ! -f ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/qrcode.png ] )
+#                then
+#                        /bin/cp ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client_interface.conf ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf 
+#                        /bin/echo "" >> ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf
+#                        /bin/cat ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client_peer.conf >> ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf 
+#                        /usr/bin/qrencode -t png -o ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/qrcode.png -r ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/client.conf
+#                        /bin/touch ${HOME}/runtime/wire-guard/configs/${ip}/${email_address}/CANDIDATE_QR_CODE
+#                fi
+#        done
+#done
 
 email_addresses="`/usr/bin/find ${HOME}/runtime/wire-guard/configs -name "CANDIDATE_QR_CODE"  -print | /usr/bin/awk -F'/' '{print $8}'`"
 email_addresses="`/bin/echo ${email_addresses} | /usr/bin/xargs -n1 | /usr/bin/sort -u | /usr/bin/xargs`"
