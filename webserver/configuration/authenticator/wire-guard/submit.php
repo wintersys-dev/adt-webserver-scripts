8788cbe4d75c1c1f352bad93d7229224012cdad4
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-10 20:49:07 +0100

    Update submit.php

--- webserver/configuration/authenticator/wire-guard/submit.php
+++ webserver/configuration/authenticator/wire-guard/submit.php
@@ -4,7 +4,7 @@
   $data = "$email\n";
   file_put_contents($file, $data, FILE_APPEND );
   echo "<h2>Email Address Submitted, if needed you should shortly receive an authentication email at $email.</h2>";
-  echo "<h2>The Link in the email will be valid for 5 minutes.</h2>";
+  echo "<h2>The Link in the email will be valid for 30 minutes.</h2>";
 
 ?>
 
