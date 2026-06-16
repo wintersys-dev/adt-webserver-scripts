695739efe87dea3b24c24272647b0381c3dc5002
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-16 09:16:41 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
@@ -323,7 +323,7 @@ then
         done
 fi
 
-/bin/mkdir -p `/bin/grep "^CONFIG_PHP_INI:" ./2 | /bin/sed 's/:/ /g' | grep -o '[^[:space:]]*session.save_path[^[:space:]]*' | /usr/bin/awk -F'=' '{print $NF}'`
+/bin/mkdir -p `/bin/grep "^CONFIG_PHP_INI:" ./2 | /bin/sed 's/:/ /g' | /bin/grep -o '[^[:space:]]*session.save_path[^[:space:]]*' | /usr/bin/awk -F'=' '{print $NF}'`
 
 /usr/bin/php -ln ${config_file}
 
