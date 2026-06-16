c69082f375e44b937556b1f702d0e94218574189
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-16 16:27:44 +0100

    Create GenerateAuthenticatorEmails.sh

---
+++ webserver/configuration/authenticator/wire-guard/GenerateAuthenticatorEmails.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#set -x
+
+if ( [ ! -d ${HOME}/runtime/wire-guard/configs ] )
+then
+        /bin/mkdir -p ${HOME}/runtime/wire-guard/configs
+fi
+
+WEBSITE_URL_ORIGINAL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURLORIGINAL'`"
+WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
+NO_REVERSE_PROXIES="`${HOME}/utilities/config/ExtractConfigValue.sh 'NOREVERSEPROXY'`"
+
+
+/usr/bin/find /var/www/html -mmin +30 -name "*qrcode*" -type f -exec rm -fv {} \;
+/usr/bin/find /var/www/html -mmin +30 -name "*client*" -type f -exec rm -fv {} \;
+
+${HOME}/services/datastore/operations/SyncFromDatastore.sh "wire-guard" "${HOME}/runtime/wire-guard/configs"
