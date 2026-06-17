cf4746c3811cc060338bff96374d46cb836d8a4f
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-17 09:48:20 +0100

    Update AllowAuthenticatorIPAddress.sh

--- services/security/whitelist/AllowAuthenticatorIPAddress.sh
+++ services/security/whitelist/AllowAuthenticatorIPAddress.sh
@@ -70,12 +70,8 @@ do
                 then
                         /bin/mkdir -p ${HOME}/runtime/authenticator
                 fi
-                if ( [ ! -f ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat ] )
-                then
-                        /bin/echo "Require ip ${VPC_IP_RANGE}" > ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
-                        /bin/echo "Require ip 127.0.0.1" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
-                fi
-                /bin/echo "Require ip ${ip_address}" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
+
+                ${HOME}/webserver/configuration/reverseproxy/whitelist/AllowIPAddress.sh "${ip_address}"
                 ${HOME}/webserver/ReloadWebserver.sh
                 /bin/echo "${ip_address}" >> ${HOME}/runtime/authenticator/ipaddresses.dat
         fi
