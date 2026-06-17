c35731bbf66127c3c960fc969e1270b3528d7a17
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-17 07:03:24 +0100

    Update ConfigureLighttpdForAuthenticator.sh

--- webserver/configuration/ConfigureLighttpdForAuthenticator.sh
+++ webserver/configuration/ConfigureLighttpdForAuthenticator.sh
@@ -115,7 +115,7 @@ then
         /bin/chown -R www-data:www-data /var/cache/lighttpd
 fi
 
-if ( [ "${AUTHENTICATOR_TYPE}" = "firewall" ] )
+if ( [ "${AUTHENTICATOR_TYPE}" = "firewall" ] || [ "${AUTHENTICATOR_TYPE}" = "whitelist" ] )
 then
 	/bin/cp ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/index.html /var/www/html/index.html
 	/bin/cp ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/submit.php /var/www/html/submit.php
@@ -124,10 +124,10 @@ then
 	/bin/chmod 644 /var/www/html/*
 	/bin/sed -i "s/XXXXUSEREMAILDOMAINXXXX/${USER_EMAIL_DOMAIN}/g" /var/www/html/index.html
 	/bin/sed -i "s/XXXXWEBSITEURLXXXX/${WEBSITE_URL}/g" /var/www/html/index.html
-	if ( [ ! -d /var/www/firewall ] )
+	if ( [ ! -d /var/www/${AUTHENTICATOR_TYPE} ] )
 	then
-        /bin/mkdir /var/www/firewall
-        /bin/chown www-data:www-data /var/www/firewall
+        /bin/mkdir /var/www/${AUTHENTICATOR_TYPE}
+        /bin/chown www-data:www-data /var/www/${AUTHENTICATOR_TYPE}
 	fi
 elif ( [ "${AUTHENTICATOR_TYPE}" = "basic-auth" ] )
 then
@@ -136,10 +136,10 @@ then
 	/bin/chown www-data:www-data /var/www/html/*
 	/bin/chmod 644 /var/www/html/*
 	/bin/sed -i "s/XXXXUSEREMAILDOMAINXXXX/${USER_EMAIL_DOMAIN}/g" /var/www/html/index.html
-	if ( [ ! -d /var/www/basic-auth ] )
+	if ( [ ! -d /var/www/${AUTHENTICATOR_TYPE} ] )
 	then
-        /bin/mkdir /var/www/basic-auth
-        /bin/chown www-data:www-data /var/www/basic-auth
+        /bin/mkdir /var/www/${AUTHENTICATOR_TYPE}
+        /bin/chown www-data:www-data /var/www/${AUTHENTICATOR_TYPE}
 	fi
 fi
 
