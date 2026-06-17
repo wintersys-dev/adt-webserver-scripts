04998c85f9d330233571fa4908f25c873942bc34
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-17 10:10:55 +0100

    Update AllowIPAddress.sh

--- webserver/configuration/reverseproxy/whitelist/AllowIPAddress.sh
+++ webserver/configuration/reverseproxy/whitelist/AllowIPAddress.sh
@@ -11,22 +11,17 @@ fi
 
 if ( [ "${WEBSERVER_CHOICE}" = "apache" ] )
 then
-  if ( [ ! -f ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat ] )
-  then
-    /bin/echo "Require ip ${VPC_IP_RANGE}" > ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
-    /bin/echo "Require ip 127.0.0.1" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
-  fi
   /bin/echo "Require ip ${ip_address}" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
 fi
 
 if ( [ "${WEBSERVER_CHOICE}" = "nginx" ] )
 then
-  if ( [ ! -f ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat ] )
-  then
-    /bin/echo "allow ${VPC_IP_RANGE};" > ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
-    /bin/echo "allow 127.0.0.1;" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
-    /bin/echo "deny all;" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
-  fi
+#  if ( [ ! -f ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat ] )
+#  then
+#    /bin/echo "allow ${VPC_IP_RANGE};" > ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
+#    /bin/echo "allow 127.0.0.1;" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
+#    /bin/echo "deny all;" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
+ # fi
   /bin/sed -i "1s/^/allow ${ip_address};/" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
 fi
 
