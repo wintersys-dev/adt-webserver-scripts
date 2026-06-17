d112f11a35ff1cfd3427df1583309d73f8810799
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-17 11:41:36 +0100

    Update ConfigureNginxForReverseProxy.sh

--- webserver/configuration/ConfigureNginxForReverseProxy.sh
+++ webserver/configuration/ConfigureNginxForReverseProxy.sh
@@ -76,9 +76,20 @@ then
 	else
     	/bin/sed -i "s;XXXXVPC_IP_RANGEXXXX;${VPC_IP_RANGE};g" ${HOME}/webserver/configuration/reverseproxy/nginx/site-available.conf
     fi
-
 fi
 
+if ( [ "${NO_AUTHENTICATORS}" != "0" ] && [ "${AUTHENTICATOR_TYPE}" = "whitelist" ] && [ "${NO_REVERSE_PROXY}" != "0" ] )
+then
+        if ( [ ! -f ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat ] )
+        then
+                /bin/echo "allow ${VPC_IP_RANGE};" > ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
+                /bin/echo "allow ip 127.0.0.1;" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
+			    /bin/echo "deny all;" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
+        fi
+        /bin/sed -i "s;#XXXXWHITE-LISTXXXX;include ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat;g" ${HOME}/webserver/configuration/reverseproxy/apache/site-available.conf
+else
+        /bin/sed -i "s/#XXXXOPEN-PROXYXXXX/            Require all granted/g" ${HOME}/webserver/configuration/reverseproxy/apache/site-available.conf
+fi
 
 if ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'NGINX:source'`" = "1" ] )
 then
