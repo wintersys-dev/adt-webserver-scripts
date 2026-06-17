af9a81e2ab39b9888c0a2d33a9d58e797076262d
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-17 07:55:17 +0100

    Update InitialiseCron.sh

--- services/cron/InitialiseCron.sh
+++ services/cron/InitialiseCron.sh
@@ -50,7 +50,7 @@ then
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 30 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateAuthenticationEmails.sh" >> /var/spool/cron/crontabs/root
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 40 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateAuthenticationEmails.sh" >> /var/spool/cron/crontabs/root
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 50 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateAuthenticationEmails.sh" >> /var/spool/cron/crontabs/root
-		/bin/echo "*/1 * * * * /usr/bin/find /var/www/html/ip-address* -mmin +5 -type f -exec rm -fv {} \;" >> /var/spool/cron/crontabs/root	
+		/bin/echo "*/1 * * * * /usr/bin/find /var/www/html/ip-address* -mmin +30 -type f -exec rm -fv {} \;" >> /var/spool/cron/crontabs/root
 	elif ( [ "${AUTHENTICATOR_TYPE}" = "basic-auth" ] )
 	then
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateBasicAuthConfig.sh" >> /var/spool/cron/crontabs/root
@@ -75,7 +75,10 @@ then
 		/bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 55 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/AcceptEmailAddresses.sh" >> /var/spool/cron/crontabs/root
 		
         /bin/echo "*/1 * * * * export HOME="${HOME}" && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateAuthenticatorEmails.sh" >> /var/spool/cron/crontabs/root
-       # /bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 5 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateAuthenticatorEmails.sh" >> /var/spool/cron/crontabs/root
+      	/bin/echo "*/1 * * * * /usr/bin/find /var/www/html/qrcode* -mmin +30 -type f -exec rm -fv {} \;" >> /var/spool/cron/crontabs/root
+		/bin/echo "*/1 * * * * /usr/bin/find /var/www/html/client* -mmin +30 -type f -exec rm -fv {} \;" >> /var/spool/cron/crontabs/root
+
+	   # /bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 5 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateAuthenticatorEmails.sh" >> /var/spool/cron/crontabs/root
        # /bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 10 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateAuthenticatorEmails.sh" >> /var/spool/cron/crontabs/root
        # /bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 15 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateAuthenticatorEmails.sh" >> /var/spool/cron/crontabs/root
        # /bin/echo "*/1 * * * * export HOME="${HOME}" && /bin/sleep 20 && ${HOME}/webserver/configuration/authenticator/${AUTHENTICATOR_TYPE}/GenerateAuthenticatorEmails.sh" >> /var/spool/cron/crontabs/root
