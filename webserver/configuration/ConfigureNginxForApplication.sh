56cf2fba2e65858e1eb6f6a35a534126a35e3269
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-17 14:17:38 +0100

    Update ConfigureNginxForApplication.sh

--- webserver/configuration/ConfigureNginxForApplication.sh
+++ webserver/configuration/ConfigureNginxForApplication.sh
@@ -29,7 +29,7 @@ APPLICATION="`${HOME}/utilities/config/ExtractConfigValue.sh 'APPLICATION' | /us
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 NO_REVERSE_PROXY="`${HOME}/utilities/config/ExtractConfigValue.sh 'NOREVERSEPROXY'`"
 MOD_SECURITY="`${HOME}/utilities/config/ExtractConfigValue.sh 'MODSECURITY'`"
-port="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PHP" "stripped" | /usr/bin/awk -F'|' '{print $2}' | /bin/sed '/^$/d'`"
+port="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PHP" "stripped" | /usr/bin/awk -F'|' '{print $2}' | /bin/sed '/^$/d' | /bin/sed 's/ //g'`"
 webroot_directory="`/bin/grep "^WEBROOT_DIRECTORY:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
 
 
