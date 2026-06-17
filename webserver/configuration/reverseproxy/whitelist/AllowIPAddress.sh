54c6862b7aa93a5b82a7216856b4c767e095eb5a
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-17 11:06:48 +0100

    Update AllowIPAddress.sh

--- webserver/configuration/reverseproxy/whitelist/AllowIPAddress.sh
+++ webserver/configuration/reverseproxy/whitelist/AllowIPAddress.sh
@@ -1,4 +1,3 @@
-
 ip_address="${1}"
 
 VPC_IP_RANGE="`${HOME}/utilities/config/ExtractConfigValue.sh 'VPCIPRANGE'`"
@@ -6,22 +5,27 @@ WEBSERVER_CHOICE="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSERVERCHOI
 
 if ( [ ! -d ${HOME}/runtime/authenticator ] )
 then
-  /bin/mkdir -p ${HOME}/runtime/authenticator
+        /bin/mkdir -p ${HOME}/runtime/authenticator
 fi
 
 if ( [ "${WEBSERVER_CHOICE}" = "APACHE" ] )
 then
-  /bin/echo "Require ip ${ip_address}" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
+        if ( [ ! -f ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat ] )
+        then
+                /bin/echo "Require ip ${VPC_IP_RANGE}" > ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
+                /bin/echo "Require ip 127.0.0.1" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
+        fi
+        /bin/echo "Require ip ${ip_address}" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
 fi
 
 if ( [ "${WEBSERVER_CHOICE}" = "NGINX" ] )
 then
-#  if ( [ ! -f ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat ] )
-#  then
-#    /bin/echo "allow ${VPC_IP_RANGE};" > ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
-#    /bin/echo "allow 127.0.0.1;" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
-#    /bin/echo "deny all;" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
- # fi
-  /bin/sed -i "1s/^/allow ${ip_address};/" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
+        if ( [ ! -f ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat ] )
+        then
+                /bin/echo "allow ${VPC_IP_RANGE};" > ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
+                /bin/echo "allow 127.0.0.1;" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
+                /bin/echo "deny all;" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
+        fi
+        /bin/sed -i "1s/^/allow ${ip_address};/" >> ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
 fi
 
