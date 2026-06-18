473cda57bc24de5e956a5f6b38d595a9b1b87ddc
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-18 08:56:57 +0100

    Add authenticator number and sleep logic to script

--- webserver/configuration/authenticator/wire-guard/GenerateAuthenticatorEmails.sh
+++ webserver/configuration/authenticator/wire-guard/GenerateAuthenticatorEmails.sh
@@ -11,8 +11,9 @@ WEBSITE_URL_ORIGINAL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEUR
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
 NO_REVERSE_PROXIES="`${HOME}/utilities/config/ExtractConfigValue.sh 'NOREVERSEPROXY'`"
 
-#/usr/bin/find /var/www/html -mmin +30 -name "*qrcode*" -type f -exec rm -fv {} \;
-#/usr/bin/find /var/www/html -mmin +30 -name "*client*" -type f -exec rm -fv {} \;
+authenticator_no="`/usr/bin/hostname | /usr/bin/awk -F'-' '{print $2}'`"
+sleep="`/usr/bin/expr ${authenticator_no} \* 10`"
+/usr/bin/sleep ${sleep}
 
 ${HOME}/services/datastore/operations/SyncFromDatastore.sh "wire-guard" "${HOME}/runtime/wire-guard/configs"
 
