1be6c43ba07ac3fe7432835f053c20f5ff10d5aa
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-18 13:34:28 +0100

    Refactor Lighttpd configuration for whitelist handling

--- webserver/configuration/ConfigureLighttpdForReverseProxy.sh
+++ webserver/configuration/ConfigureLighttpdForReverseProxy.sh
@@ -74,10 +74,11 @@ fi
 
 if ( [ "${NO_AUTHENTICATORS}" != "0" ] && [ "${AUTHENTICATOR_TYPE}" = "whitelist" ] && [ "${NO_REVERSE_PROXY}" != "0" ] )
 then
-        /bin/sed -i -e "/#XXXXWHITE-LISTXXXX/{r ${HOME}/webserver/configuration/reverseproxy/whitelist/allowed-ips.tmpl" -e 'd}' ${HOME}/webserver/configuration/reverseproxy/lighttpd/lighttpd.conf
-        vpc="`/bin/echo ${VPC_IP_RANGE} | /usr/bin/cut -d. -f-3`\\."
-		/bin/sed -i "s;XXXXIP_ADDRESSESXXXX;${vpc}|127.0.0.1;g" ${HOME}/webserver/configuration/reverseproxy/lighttpd/lighttpd.conf
-		/bin/cp ${HOME}webserver/configuration/reverseproxy/whitelist/403-error.html /var/www/html/${APPLICATION}/403-error.html
+	/bin/sed -i 's/##XXXXWHITE-LISTXXXX//g' ${HOME}/webserver/configuration/reverseproxy/lighttpd/lighttpd.conf
+    /bin/sed -i -e "/#XXXXWHITE-LISTXXXX/{r ${HOME}/webserver/configuration/reverseproxy/whitelist/allowed-ips.tmpl" -e 'd}' ${HOME}/webserver/configuration/reverseproxy/lighttpd/lighttpd.conf
+    vpc="`/bin/echo ${VPC_IP_RANGE} | /usr/bin/cut -d. -f-3`\\."
+	/bin/sed -i "s;XXXXIP_ADDRESSESXXXX;${vpc}|127.0.0.1;g" ${HOME}/webserver/configuration/reverseproxy/lighttpd/lighttpd.conf
+	/bin/cp ${HOME}webserver/configuration/reverseproxy/whitelist/403-error.html /var/www/html/${APPLICATION}/403-error.html
 fi
 
 if ( [ -f ${HOME}/webserver/configuration/reverseproxy/lighttpd/mimetypes.conf ] )
