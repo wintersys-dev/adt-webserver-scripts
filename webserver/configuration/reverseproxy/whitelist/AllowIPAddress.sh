a068a11a87dcf298d7cc69913765fb80668b18b5
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-18 16:39:40 +0100

    Update AllowIPAddress.sh

--- webserver/configuration/reverseproxy/whitelist/AllowIPAddress.sh
+++ webserver/configuration/reverseproxy/whitelist/AllowIPAddress.sh
@@ -37,7 +37,7 @@ if ( [ "${WEBSERVER_CHOICE}" = "LIGHTTPD" ] )
 then        
         if ( [ -f ${HOME}/runtime/REVERSEPROXY_READY ] )
         then
-                if ( [ -f ${HOME}/runtime/authenticator/incoming_ipaddresses.dat ] && [ "`/bin/cat ${HOME}/runtime/authenticator/incoming_ipaddresses.dat`" != "" ] )
+                if ( [ -f /etc/lighttpd/lighttpd.conf ] )
                 then
                         /bin/cat ${HOME}/runtime/authenticator/incoming_ipaddresses.dat > ${HOME}/runtime/authenticator/all_ips_whitelist.dat.$$
                         /bin/cat ${HOME}/runtime/authenticator/processed_ipaddresses.dat >> ${HOME}/runtime/authenticator/all_ips_whitelist.dat.$$
@@ -49,14 +49,21 @@ then
                         ip_addresses="${ip_addresses}|${vpc}|127.0.0.1"
                         /bin/cp ${HOME}/webserver/configuration/reverseproxy/whitelist/allowed-ips.tmpl ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
                         /bin/sed -i "s;XXXXIP_ADDRESSESXXXX;${ip_addresses};" ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
-                        /usr/bin/tac /etc/lighttpd/lighttpd.conf | /usr/bin/awk '!p && /##WHITE-LIST-MARKER/{print "##XXXXWHITE-LISTXXXX"; p=1} 1' | /usr/bin/tac > /etc/lighttpd/lighttpd.conf.$$
-                        /bin/sed -i '/#WHITE-LIST-MARKER/d' /etc/lighttpd/lighttpd.conf.$$
-                        /bin/sed -i -e "/##XXXXWHITE-LISTXXXX/{r ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat" -e 'd}' /etc/lighttpd/lighttpd.conf.$$
-                        /bin/mv /etc/lighttpd/lighttpd.conf.$$ /etc/lighttpd/lighttpd.conf
-                
-                        if ( [ "`/bin/grep ${ip_address} /etc/lighttpd/lighttpd.conf`" != "" ] )
+
+                        value1="`/bin/cat /etc/lighttpd/lighttpd.conf | /usr/bin/sort -u | /usr/bin/wc | /bin/sed 's/ //g'`"
+                        value2="`/bin/cat ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat /etc/lighttpd/lighttpd.conf | /usr/bin/sort -u | /usr/bin/wc | /bin/sed 's/ //g'`"
+
+                        if ( [ "${value1}" -ne "${value2}" ] )
                         then
-                                /bin/echo "${ip_address}" >> ${HOME}/runtime/authenticator/processed_ipaddresses.dat
+                                /usr/bin/tac /etc/lighttpd/lighttpd.conf | /usr/bin/awk '!p && /##WHITE-LIST-MARKER/{print "##XXXXWHITE-LISTXXXX"; p=1} 1' | /usr/bin/tac > /etc/lighttpd/lighttpd.conf.$$
+                                /bin/sed -i '/#WHITE-LIST-MARKER/d' /etc/lighttpd/lighttpd.conf.$$
+                                /bin/sed -i -e "/##XXXXWHITE-LISTXXXX/{r ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat" -e 'd}' /etc/lighttpd/lighttpd.conf.$$
+                                /bin/mv /etc/lighttpd/lighttpd.conf.$$ /etc/lighttpd/lighttpd.conf
+
+                                if ( [ "`/bin/grep ${ip_address} /etc/lighttpd/lighttpd.conf`" != "" ] )
+                                then
+                                        /bin/echo "${ip_address}" >> ${HOME}/runtime/authenticator/processed_ipaddresses.dat
+                                fi
                         fi
                 fi
         else
