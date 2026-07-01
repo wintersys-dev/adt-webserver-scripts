f7ccd1eb875cac2047403cc4632e173d8178e1f8
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-01 14:04:44 +0100

    Comment out EnforcePermissions.sh execution

--- rp.sh
+++ rp.sh
@@ -235,5 +235,5 @@ ${HOME}/utilities/housekeeping/CleanupAfterBuild.sh
 /bin/echo "${0} Restarting Webserver"
 ${HOME}/webserver/RestartWebserver.sh
 
-${HOME}/utilities/security/EnforcePermissions.sh &
+#${HOME}/utilities/security/EnforcePermissions.sh &
 
