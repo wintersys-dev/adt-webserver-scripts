9bbed53f7685bc6516b7bdba0e77ed4616189c60
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-01 10:18:44 +0100

    Update auth.sh

--- auth.sh
+++ auth.sh
@@ -263,8 +263,8 @@ ${HOME}/utilities/housekeeping/CleanupAfterBuild.sh
 /bin/touch ${HOME}/runtime/DONT_MESS_WITH_THESE_FILES-SYSTEM_BREAK
 /usr/bin/touch ${HOME}/runtime/AUTHENTICATOR_READY
 
-${HOME}/utilities/security/EnforcePermissions.sh
-
 /bin/echo "${0} Restarting Authenticator machine Webserver"
-${HOME}/webserver/RestartWebserver.sh
+${HOME}/webserver/RestartWebserver.sh 
+
+${HOME}/utilities/security/EnforcePermissions.sh &
 
