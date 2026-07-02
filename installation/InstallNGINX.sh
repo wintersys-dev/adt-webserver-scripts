da991a002c4c3168b579948590005d14f39ae302
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-02 14:53:59 +0100

    Update InstallNGINX.sh

--- installation/InstallNGINX.sh
+++ installation/InstallNGINX.sh
@@ -36,6 +36,8 @@ MOD_SECURITY="`${HOME}/utilities/config/ExtractConfigValue.sh 'MODSECURITY'`"
 NO_REVERSE_PROXIES="`${HOME}/utilities/config/ExtractConfigValue.sh 'NOREVERSEPROXIES'`"
 
 manager=""
+options=""
+tail_options=""
 if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
 then
 	manager="/usr/bin/apt"
