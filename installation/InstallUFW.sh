61b5bb532707390796684b43d87a309f507f6ddc
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-02 15:59:14 +0100

    Update InstallUFW.sh

--- installation/InstallUFW.sh
+++ installation/InstallUFW.sh
@@ -59,12 +59,12 @@ do
 	then
 		if ( [ "${BUILDOS}" = "ubuntu" ] )
 		then
-			eval ${install_command} ufw	
+			eval ${install_command} ufw	${tail_options}
 		fi
 
 		if ( [ "${BUILDOS}" = "debian" ] )
 		then
-			eval ${install_command} ufw	
+			eval ${install_command} ufw ${tail_options}
 		fi
 	fi
 	count="`/usr/bin/expr ${count} + 1`"
