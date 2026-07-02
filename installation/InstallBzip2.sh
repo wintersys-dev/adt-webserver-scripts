a3e43ee514abad927dc619ff013778a19841a603
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-02 15:46:29 +0100

    Update InstallBzip2.sh

--- installation/InstallBzip2.sh
+++ installation/InstallBzip2.sh
@@ -59,12 +59,12 @@ do
 	then
 		if ( [ "${BUILDOS}" = "ubuntu" ] )
 		then
-			eval ${install_command} bzip2
+			eval ${install_command} bzip2 ${tail_options}
 		fi
 
 		if ( [ "${BUILDOS}" = "debian" ] )
 		then
-			eval ${install_command} bzip2
+			eval ${install_command} bzip2 ${tail_options}
 		fi
 	fi
 	count="`/usr/bin/expr ${count} + 1`"
