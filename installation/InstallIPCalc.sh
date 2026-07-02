25e498ee7f4ec78e6fb296b7c84d3ec158efc432
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-02 15:49:38 +0100

    Update InstallIPCalc.sh

--- installation/InstallIPCalc.sh
+++ installation/InstallIPCalc.sh
@@ -58,12 +58,12 @@ do
 	then
 		if ( [ "${BUILDOS}" = "ubuntu" ] )
 		then
-			eval ${install_command} ipcalc	
+			eval ${install_command} ipcalc ${tail_options}
 		fi
 
 		if ( [ "${BUILDOS}" = "debian" ] )
 		then
-			eval ${install_command} ipcalc	
+			eval ${install_command} ipcalc ${tail_options}
 		fi
 	fi
 	count="`/usr/bin/expr ${count} + 1`"
