1a32ad60c1ef2dbaf8785bebbde71f2e83272021
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-02 15:48:17 +0100

    Update InstallGawk.sh

--- installation/InstallGawk.sh
+++ installation/InstallGawk.sh
@@ -58,12 +58,12 @@ do
 	then
 		if ( [ "${BUILDOS}" = "ubuntu" ] )
 		then
-			eval ${install_command} gawk
+			eval ${install_command} gawk ${tail_options}
 		fi
 
 		if ( [ "${BUILDOS}" = "debian" ] )
 		then
-			eval ${install_command} gawk
+			eval ${install_command} gawk ${tail_options}
 		fi
 	fi
 	count="`/usr/bin/expr ${count} + 1`"
