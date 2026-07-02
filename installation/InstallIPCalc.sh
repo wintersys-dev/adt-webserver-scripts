ea39a1c52294c5010ef3d0568636ce85b2a6b635
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-02 14:52:30 +0100

    Add package manager options for apt installation

--- installation/InstallIPCalc.sh
+++ installation/InstallIPCalc.sh
@@ -32,6 +32,8 @@ else
 fi
 
 manager=""
+options=""
+tail_options=""
 if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
 then
 	manager="/usr/bin/apt"
