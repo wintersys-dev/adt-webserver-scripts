5f8a8a1c03a9f2d591d3ec9a0c01857419bcd4f6
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-02 08:31:23 +0100

    Refactor package manager handling in InstallRClone.sh

--- installation/InstallRClone.sh
+++ installation/InstallRClone.sh
@@ -32,24 +32,26 @@ else
 	BUILDOS="${buildos}"
 fi
 
-apt=""
+manager=""
 if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
 then
-	apt="/usr/bin/apt"
+	manager="/usr/bin/apt"
+	options="-o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y"
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt-get" ] )
 then
-	apt="/usr/bin/apt-get"
+	manager="/usr/bin/apt-get"
+	options="-o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y"
 fi
 
 export DEBIAN_FRONTEND=noninteractive
-install_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y install "
+install_command="${manager} ${options}"
 
 cwd="`/usr/bin/pwd`"
 
 count="0"
 while ( [ ! -f /usr/bin/rclone ] && [ "${count}" -lt "5" ] )
 do
-	if ( [ "${apt}" != "" ] )
+	if ( [ "${manager}" != "" ] )
 	then
 		if ( [ "${BUILDOS}" = "ubuntu" ] )
 		then
