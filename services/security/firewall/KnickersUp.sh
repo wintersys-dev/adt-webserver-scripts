8c2e49e65ec801168ea6b8e6a165916a32dc8276
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-03 08:17:57 +0100

    Remove -E flag from sudo commands in KnickersUp.sh

--- services/security/firewall/KnickersUp.sh
+++ services/security/firewall/KnickersUp.sh
@@ -35,9 +35,9 @@ then
 
 	if ( [ "${firewall}" = "ufw" ] )
 	then
-		/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw default deny incoming
+		/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S /usr/sbin/ufw default deny incoming
 		/bin/sleep 10
-		/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw default allow outgoing
+		/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S /usr/sbin/ufw default allow outgoing
 		/bin/touch ${HOME}/runtime/KNICKERS_ARE_UP
 	elif ( [ "${firewall}" = "iptables" ] )
 	then
