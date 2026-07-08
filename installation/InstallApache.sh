927422c22546d69cf75a641ab6df09f226ef4e57
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-08 22:39:58 +0100

    Change package manager to use nala_wrapper.sh

--- installation/InstallApache.sh
+++ installation/InstallApache.sh
@@ -49,7 +49,7 @@ then
 	options="-o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y"
 elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "nala" ] )
 then
-	manager="/usr/bin/nala"
+	manager="${HOME}/installation/nala_wrapper.sh"
 	tail_options="-y"
 fi
 
