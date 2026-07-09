0e5ae54464355cfe4392dd92498c904a05709379
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-09 12:16:29 +0100

    Update EmergencyRestoration.sh

--- application/backup/EmergencyRestoration.sh
+++ application/backup/EmergencyRestoration.sh
@@ -11,6 +11,10 @@ fi
 
 /bin/mv /var/www/html ${HOME}/runtime/restoration_archives
 
+DB_N="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBNAME' | /bin/sed 's/_ARCHIVE.*//g'`"
+DB_N1="`/bin/echo .${archive_id} | /bin/sed -e 's/\./_/g' -e 's/-/_/g'`"
+/bin/grep -rlZ ${DB_N} ${HOME}/runtime | /usr/bin/xargs -0 /bin/sed -i "s/${DB_N}/${DB_N}${DB_N1}/g"
+/bin/grep -rlZ ${DB_N} /var/www/html | /usr/bin/xargs -0 /bin/sed -i "s/${DB_N}/${DB_N}${DB_N1}/g"
 ${HOME}/application/InstallApplication.sh ${archive_id}
 ${HOME}/application/configuration/InitialiseApplicationConfiguration.sh
 
