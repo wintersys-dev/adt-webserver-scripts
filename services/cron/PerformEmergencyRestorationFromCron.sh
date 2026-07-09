2fd19f95bc0c6e388fb3c0687305dfc98d222944
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-09 13:29:48 +0100

    Create PerformEmergencyRestorationFromCron.sh

---
+++ services/cron/PerformEmergencyRestorationFromCron.sh
@@ -0,0 +1,27 @@
+if ( [ -f ${HOME}/runtime/EMERGENCY_RESTORATION_IN_PROGRESS ] )
+then
+        exit
+fi
+
+marker="`${HOME}/services/datastore/operations/ListFromDatastore.sh "config" "ACTIVATE_RESTORATION" | /bin/grep -o "ACTIVATE_RESTORATION.*"
+
+if ( [ "${marker}" != "" ] )
+then
+        /bin/touch ${HOME}/runtime/EMERGENCY_RESTORATION_IN_PROGRESS
+        ${HOME}/application/backup/EmergencyRestoration.sh
+else
+        exit
+fi
+
+/bin/sleep 120
+
+if ( [ -f ${HOME}/application/backup/EmergencyRestoration.sh ] )
+then
+        /bin/rm ${HOME}/application/backup/EmergencyRestoration.sh
+fi
+
+
+if ( [ "${marker}" != "" ] )
+then
+        ${HOME}/services/datastore/operations/DeleteFromDatastore.sh "config" "${marker}" "local"
+fi
