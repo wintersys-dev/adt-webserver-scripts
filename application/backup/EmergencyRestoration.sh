d64b3e74d3eaf71a82dc2dc3bc870c4a2f0c28aa
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-11 10:19:12 +0100

    Fix case sensitivity in DB name extraction
    Updated the sed command to handle '_archive' instead of '_ARCHIVE' for consistency.

--- application/backup/EmergencyRestoration.sh
+++ application/backup/EmergencyRestoration.sh
@@ -12,7 +12,7 @@ fi
 
 /bin/mv /var/www/html ${HOME}/runtime/restoration_archives/${archive_id}
 
-DB_N="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBNAME' | /bin/sed 's/_ARCHIVE.*//g'`"
+DB_N="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBNAME' | /bin/sed 's/_archive.*//g'`"
 DB_N1="`/bin/echo .${archive_id} | /bin/sed -e 's/\./_/g' -e 's/-/_/g'`"
 DB_N2="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBNAME'`"
 /bin/grep -rlZ ${DB_N2} ${HOME}/runtime | /usr/bin/xargs -0 /bin/sed -i "s/${DB_N2}/${DB_N}${DB_N1}/g"
