7c350e91c9b8ac3e964089a517d98f2793c7fbea
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-16 18:27:09 +0100

    Add support for aptitude package manager

--- installation/InstallApplicationLanguageLibraries.sh
+++ installation/InstallApplicationLanguageLibraries.sh
@@ -59,7 +59,11 @@ elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" 
 then
 	manager="${HOME}/installation/nala_wrapper.sh"
 	tail_options="-y"
-fi           
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "aptitude" ] )
+then
+        manager="${HOME}/installation/aptitude_wrapper.sh"
+        options="-y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold'"
+fi       
 
 export DEBIAN_FRONTEND=noninteractive
 add_repository_command="/usr/bin/add-apt-repository -y "
