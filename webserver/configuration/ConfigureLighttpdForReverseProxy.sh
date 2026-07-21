8b9d3779564b4963b03e534fde6a57995dd0535e
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-21 09:07:53 +0100

    Update ConfigureLighttpdForReverseProxy.sh

--- webserver/configuration/ConfigureLighttpdForReverseProxy.sh
+++ webserver/configuration/ConfigureLighttpdForReverseProxy.sh
@@ -28,6 +28,7 @@ BUILD_MACHINE_IP="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDMACHINEI
 AUTHENTICATOR_TYPE="`${HOME}/utilities/config/ExtractConfigValue.sh 'AUTHENTICATORTYPE'`"
 NO_REVERSE_PROXIES="`${HOME}/utilities/config/ExtractConfigValue.sh 'NOREVERSEPROXIES'`"
 NO_AUTHENTICATORS="`${HOME}/utilities/config/ExtractConfigValue.sh 'NOAUTHENTICATORS'`"
+AUTH_SERVER_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'AUTHSERVERURL'`"
 port="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PHP" "stripped" | /usr/bin/awk -F'|' '{print $2}' | /bin/sed '/^$/d' | /bin/sed 's/ //g'`"
 
 if ( [ -f /etc/php/${PHP_VERSION}/fpm/php.ini ] )
