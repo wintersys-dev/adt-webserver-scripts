cbf6432060da542f128c79449d82cec942b431b5
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-21 09:06:46 +0100

    Create /var/www/html directory and status-403.html
    Ensure the /var/www/html directory exists and create a status-403.html file.

--- webserver/configuration/ConfigureLighttpdForReverseProxy.sh
+++ webserver/configuration/ConfigureLighttpdForReverseProxy.sh
@@ -74,6 +74,11 @@ fi
 
 if ( [ "${NO_AUTHENTICATORS}" != "0" ] && [ "${AUTHENTICATOR_TYPE}" = "whitelist" ] && [ "${NO_REVERSE_PROXIES}" != "0" ] )
 then
+	if ( [ ! -d /var/www/html ] )
+	then
+		/bin/mkdir -p /var/www/html
+	fi
+	/bin/echo "" > /var/www/html/status-403.html
 	/bin/sed -i 's/#XXXXWHITE-LISTXXXX//g' ${HOME}/webserver/configuration/reverseproxy/lighttpd/lighttpd.conf
    # /bin/sed -i -e "/#XXXXWHITE-LISTXXXX/{r ${HOME}/webserver/configuration/reverseproxy/whitelist/allowed-ips.tmpl" -e 'd}' ${HOME}/webserver/configuration/reverseproxy/lighttpd/lighttpd.conf
    	/bin/cp ${HOME}/webserver/configuration/reverseproxy/whitelist/allowed-ips.tmpl ${HOME}/runtime/authenticator/webserver_ip_whitelist.dat
