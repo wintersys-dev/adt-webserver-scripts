aab9d481deccb06268e55d67361e2dfa4a202cfa
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-23 15:28:54 +0100

    Update GenerateAuthenticatorEmails.sh

--- webserver/configuration/authenticator/wire-guard/GenerateAuthenticatorEmails.sh
+++ webserver/configuration/authenticator/wire-guard/GenerateAuthenticatorEmails.sh
@@ -43,31 +43,31 @@ sleep="`/usr/bin/expr ${authenticator_no} \* 10`"
 ${HOME}/services/datastore/operations/SyncFromDatastore.sh "wire-guard" "${HOME}/runtime/wire-guard/configs"
 ${HOME}/services/datastore/operations/SyncFromDatastore.sh "wire-guard-emailed-links" "/var/www/html"
 
-if ( [ -f ${HOME}/runtime/wire-guard/time_email_sent ] )
-then
-        /bin/rm ${HOME}/runtime/wire-guard/time_email_sent
-fi
+#if ( [ -f ${HOME}/runtime/wire-guard/time_email_sent ] )
+#then
+#        /bin/rm ${HOME}/runtime/wire-guard/time_email_sent
+#fi
+##
+#
+#if ( [ "`${HOME}/services/datastore/operations/ListFromDatastore.sh "wire-guard-emailed-links" "time_email_sent"`" != "" ] )
+#then 
+#        ${HOME}/services/datastore/operations/GetFromDatastore.sh "wire-guard-emailed-links" "time_email_sent" "${HOME}/runtime/wire-guard" 
+#fi
+#
+#if ( [ -f ${HOME}/runtime/wire-guard/time_email_sent ] )
+#then
+#        current_time="`/usr/bin/date +%s`"
+#        backup_time="`/bin/cat ${HOME}/runtime/wire-guard/time_email_sent`"
+#
+#        if ( [ "`/usr/bin/expr ${current_time} - ${backup_time}`" -lt "60" ] )
+ #       then
+ #               exit
+ #       fi
+#fi
 
-
-if ( [ "`${HOME}/services/datastore/operations/ListFromDatastore.sh "wire-guard-emailed-links" "time_email_sent"`" != "" ] )
-then 
-        ${HOME}/services/datastore/operations/GetFromDatastore.sh "wire-guard-emailed-links" "time_email_sent" "${HOME}/runtime/wire-guard" 
-fi
-
-if ( [ -f ${HOME}/runtime/wire-guard/time_email_sent ] )
-then
-        current_time="`/usr/bin/date +%s`"
-        backup_time="`/bin/cat ${HOME}/runtime/wire-guard/time_email_sent`"
-
-        if ( [ "`/usr/bin/expr ${current_time} - ${backup_time}`" -lt "60" ] )
-        then
-                exit
-        fi
-fi
-
-time_email_sent="`/usr/bin/date +%s`"
-/bin/echo "${time_email_sent}" > ${HOME}/runtime/wire-guard/time_email_sent
-${HOME}/services/datastore/operations/PutToDatastore.sh "wire-guard-emailed-links" "${HOME}/runtime/wire-guard/time_email_sent" "root" "distributed" "no"
+#time_email_sent="`/usr/bin/date +%s`"
+#/bin/echo "${time_email_sent}" > ${HOME}/runtime/wire-guard/time_email_sent
+#${HOME}/services/datastore/operations/PutToDatastore.sh "wire-guard-emailed-links" "${HOME}/runtime/wire-guard/time_email_sent" "root" "distributed" "no"
 
 email_addresses="`/usr/bin/find ${HOME}/runtime/wire-guard/configs -name "CLIENT_INTERFACE_GENERATED" -print | /usr/bin/awk -F'/' '{print $8}' | /usr/bin/xargs -n1 | /usr/bin/sort -u | /usr/bin/xargs`"
 
