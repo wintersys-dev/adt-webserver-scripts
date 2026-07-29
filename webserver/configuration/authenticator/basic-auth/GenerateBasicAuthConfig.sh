bdc95698c50d4f97701be3005f0d4bc076c67fcc
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-29 10:59:08 +0100

    Update GenerateBasicAuthConfig.sh

--- webserver/configuration/authenticator/basic-auth/GenerateBasicAuthConfig.sh
+++ webserver/configuration/authenticator/basic-auth/GenerateBasicAuthConfig.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
-
+#################################################################################
+# Author : Peter Winter
+# Date   : 13/07/2016
+# Description : This will generate the basic auth configurations used by the reverse
+# proxy machines to grant or deny access based on the users email address and 
+# the password that is emailed to them
+#################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+################################################################################
 #set -x
 
 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURLORIGINAL'`"
