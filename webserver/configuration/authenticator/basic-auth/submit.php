436bf609d578db6c9149575da064d38970967f16
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-29 10:59:47 +0100

    Update password storage logic in submit.php

--- webserver/configuration/authenticator/basic-auth/submit.php
+++ webserver/configuration/authenticator/basic-auth/submit.php
@@ -4,7 +4,6 @@
         $file="/var/www/basic-auth/basic-auth.dat";
         $data = "$email:$previous_password\n";
         file_put_contents($file, $data, FILE_APPEND );
-     //   file_put_contents($file, $email, FILE_APPEND );
         echo "<h2>Your basic auth password has been set.</h2>";
         echo "<h2>Please enter this email address and the password from the email I sent you when visiting our main website.</h2>";
 ?>
