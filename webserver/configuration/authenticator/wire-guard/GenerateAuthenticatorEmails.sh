4d182d438918188f2261a265dfc6ca4614b63629
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-29 11:09:11 +0100

    Update GenerateAuthenticatorEmails.sh

--- webserver/configuration/authenticator/wire-guard/GenerateAuthenticatorEmails.sh
+++ webserver/configuration/authenticator/wire-guard/GenerateAuthenticatorEmails.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
-
-set -x
+###########################################################################################################
+# Description: This will accept candidate email addresses from the HTML form running on an authenticator
+# server and will store the email address(es) in the datastore where the reverse proxies can access them
+# to see who they need to open up to. 
+# Author : Peter Winter
+# Date: 17/05/2017
+######################################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################################
+#######################################################################################################
+#set -x
 
 if ( [ ! -d ${HOME}/runtime/wire-guard/configs ] )
 then
