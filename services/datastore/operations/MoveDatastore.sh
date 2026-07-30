2c1ce1ff1727d6ac57768db0a5e12ac6b1d80a68
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-30 08:28:01 +0100

    Fix bucket naming conventions in MoveDatastore.sh

--- services/datastore/operations/MoveDatastore.sh
+++ services/datastore/operations/MoveDatastore.sh
@@ -92,7 +92,7 @@ then
                 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
         fi
         active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`"
-        active_bucket="${active_bucket}-firewallemailedlinks"
+        active_bucket="${active_bucket}-firewall-emailed-links"
 elif ( [ "${bucket_type}" = "whitelist-auth-laptop-ips" ] )
 then
         if ( [ "`/usr/bin/hostname | /bin/grep '\-auth-'`" != "" ] )
@@ -130,7 +130,7 @@ then
                 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
         fi
         active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`"
-        active_bucket="${active_bucket}-wireguardemails"
+        active_bucket="${active_bucket}-wireguard-emails"
 elif ( [ "${bucket_type}" = "wire-guard-emailed-links" ] )
 then
         if ( [ "`/usr/bin/hostname | /bin/grep '\-auth-'`" != "" ] )
@@ -140,7 +140,7 @@ then
                 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
         fi
         active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`"
-        active_bucket="${active_bucket}-wireguardemailedlinks"
+        active_bucket="${active_bucket}-wireguard-emailed-links"
 fi
 
 S3_ACCESS_KEY="`${HOME}/utilities/config/ExtractConfigValue.sh 'S3ACCESSKEY'`"
