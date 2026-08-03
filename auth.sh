fb1226e5a6bd86737e6398b00d92a285a639ca2c
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-03 08:16:02 +0100

    Update auth.sh

--- auth.sh
+++ auth.sh
@@ -114,6 +114,9 @@ exec 1>>${HOME}/logs/${out_file}
 err_file="initialbuild/webserver-build-err-`/bin/date | /bin/sed 's/ //g'`"
 exec 2>>${HOME}/logs/${err_file}
 
+
+/usr/sbin/sshd -T >  ${HOME}/logs/initialbuild/sshd-initial-settings.log
+
 /bin/echo "${0} `/bin/date`: Building a new authorisation server" 
 
 AUTHENTICATOR_TYPE="`${HOME}/utilities/config/ExtractConfigValue.sh 'AUTHENTICATORTYPE'`"
