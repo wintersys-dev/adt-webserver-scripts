b043c5fed52758484bcd9067acede3e6c54410ee
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-03 11:49:01 +0100

    Update SetupFirewall.sh

--- services/security/firewall/SetupFirewall.sh
+++ services/security/firewall/SetupFirewall.sh
@@ -198,8 +198,11 @@ if ( [ "`/bin/grep ${BUILD_MACHINE_IP} /etc/ssh/sshd_config`" = "" ] )
 then
 	if ( [ "${SERVER_USER}" != "" ] && [ "${BUILD_MACHINE_IP}" != "" ] )
 	then
-		/bin/echo "AllowUsers ${SERVER_USER}@${BUILD_MACHINE_IP}" >> /etc/ssh/sshd_config.d/99-hardening.conf
-		updated_ssh="1"
+		if ( [ "`/bin/grep "AllowUsers" /etc/ssh/sshd_config.d/99-hardening.conf | /bin/grep "${SERVER_USER}" | /bin/grep "${BUILD_MACHINE_IP}"`" = "" ] )
+		then
+			/bin/echo "AllowUsers ${SERVER_USER}@${BUILD_MACHINE_IP}" >> /etc/ssh/sshd_config.d/99-hardening.conf
+			updated_ssh="1"
+		fi
 	fi
 fi
 
