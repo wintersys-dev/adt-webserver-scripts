8fb58bdb537fa0b886083b8b4c3a12b81340719c
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-04 14:21:41 +0100

    Update InstallMariaDBClient.sh

--- installation/InstallMariaDBClient.sh
+++ installation/InstallMariaDBClient.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-######################################################################################################
-# Description: This script will install the maria db client
-# Author: Peter Winter
-# Date: 17/01/2017
-#######################################################################################################
+###################################################################################
+# Description: This  will install mariadb client. You will need this to be installed
+# if you want to access a managed database without having a self managed database 
+# installed on a VPS machine
+# Date: 18/11/2016
+# Author : Peter Winter
+###################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
 # The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
@@ -16,8 +18,8 @@
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
-#######################################################################################################
-#######################################################################################################
+####################################################################################
+####################################################################################
 #set -x
 
 if ( [ "${1}" != "" ] )
@@ -53,28 +55,74 @@ then
         options="-y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold'"
 fi
 
-export DEBIAN_FRONTEND=noninteractive 
-install_command="${manager} ${options} install " 
+export DEBIAN_FRONTEND=noninteractive
+install_command="${manager} ${options} install "
+purge_command="${manager} ${options}  purge " 
+auto_remove_command="${manager} ${options} autoremove " 
+auto_clean_command="${manager} ${options}  autoclean " 
 
 count="0"
-while ( [ ! -f /usr/bin/mariadb ] && [ "${count}" -lt "5" ] )
+while ( [ ! -f /usr/bin/mariadbd ] && [ "${count}" -lt "5" ] )
 do
 	if ( [ "${manager}" != "" ] )
 	then
-		if ( [ "${BUILDOS}" = "ubuntu" ] )
-		then
-			#mariadb_version="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "MARIADB" | /usr/bin/awk -F':' '{print $NF}'`"	
-			#/usr/bin/curl -LsS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version="mariadb-${mariadb_version}"	
-			eval ${install_command} mariadb-client ${tail_options}	
-		fi
+        if ( [ "${BUILDOS}" = "ubuntu" ] )
+        then
+        	if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "MARIADB" | /usr/bin/awk -F':' '{print $NF}'`" != "cloud-init" ] )
+        	then
+    			mariadb_version="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "MARIADB" | /usr/bin/awk -F':' '{print $NF}'`"
+    			if ( [ "${mariadb_version}" = "default" ] )
+        		then
+					${install_command} mariadb-client ${tail_options}
+				else
+    				if ( [ "${BUILDOS_VERSION}" = "24.04" ] )
+    				then
+    					os_type="ubuntu" 
+						os_version="noble"
+					fi
+					if ( [ "${BUILDOS_VERSION}" = "26.04" ] )
+					then
+						os_type="ubuntu" 
+						os_version="resolute"
+					fi
+					#At the time of writing this script doesn't support Ubuntu 26.04 so the default for the OS will have to be used
+					#until such time as the script supports it and then you should be all set to use non default versions on 26.04
+					/usr/bin/curl -LsS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version="mariadb-${mariadb_version}" --os-type="${os_type}" --os-version="${os_version}" --arch='amd64' --skip-maxscale
+					${install_command} mariadb-client ${tail_options}
+				fi  
+            fi
+        fi
 
-		if ( [ "${BUILDOS}" = "debian" ] )
-		then
-			#mariadb_version="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "MARIADB" | /usr/bin/awk -F':' '{print $NF}'`"	
-			#/usr/bin/curl -LsS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version="mariadb-${mariadb_version}"	
-			eval ${install_command} mariadb-client ${tail_options}	
-		fi				
+        if ( [ "${BUILDOS}" = "debian" ] )
+        then
+    		if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "MARIADB" | /usr/bin/awk -F':' '{print $NF}'`" != "cloud-init" ] )
+        	then
+            	mariadb_version="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "MARIADB" | /usr/bin/awk -F':' '{print $NF}'`"
+				if ( [ "${mariadb_version}" = "default" ] )
+				then
+					${install_command} mariadb-client ${tail_options}
+				else
+					if ( [ "${BUILDOS_VERSION}" = "26.04" ] )
+					then
+						os_type="debian"
+						os_version="trixie"
+					fi
+					/usr/bin/curl -LsS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version="mariadb-${mariadb_version}" --os-type="${os)type}" --os-version="${os_version}" --arch='amd64' --skip-maxscale
+					${install_command} mariadb-client ${tail_options}
+				fi
+            fi
+        fi
 	fi
+	
+	if ( [ ! -d /var/log/mysql ] )
+	then
+		/bin/mkdir /var/log/mysql
+		/bin/chown mysql:mysql /var/log/mysql                         
+	fi
+	
+	${HOME}/utilities/processing/RunServiceCommand.sh mariadb enable
+	${HOME}/utilities/processing/RunServiceCommand.sh mariadb restart
+	
 	count="`/usr/bin/expr ${count} + 1`"
 done
 
