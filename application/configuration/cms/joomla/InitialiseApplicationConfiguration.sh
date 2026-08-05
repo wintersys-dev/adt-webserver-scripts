ff8fe2d30d2542637eb664030e431f248fa825ac
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-05 07:14:59 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
@@ -31,13 +31,19 @@
 #######################################################################################################
 set -x 
 
-if ( [ ! -d ${HOME}/logs/application_configuration ] )
+if ( [ ! -d ${HOME}/logs/joomla_configuration ] )
 then
-        /bin/mkdir -p ${HOME}/logs/application_configuration
+        /bin/mkdir -p ${HOME}/logs/joomla_configuration
 fi
 
-exec 1>>${HOME}/logs/application_configuration/joomla_out.log
-exec 2>>${HOME}/logs/application_configuration/joomla_err.log
+log_file="joomla_out_`/bin/date | /bin/sed 's/ //g'`"
+err_file="joomla_err_`/bin/date | /bin/sed 's/ //g'`"
+
+/bin/echo "Log file is at: ${HOME}/logs/joomla_configuration/${log_file}"
+/bin/echo "Error file is at: ${HOME}/logs/joomla_configuration/${err_file}"
+
+exec 1>>${HOME}/logs/joomla_configuration/${log_file}
+exec 2>>${HOME}/logs/joomla_configuration/${err_file}
 
 webroot_directory="`/bin/grep "^WEBROOT_DIRECTORY:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
 
