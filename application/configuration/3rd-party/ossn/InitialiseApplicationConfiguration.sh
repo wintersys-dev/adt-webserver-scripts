a983ae481214319369a5eaef816dfbd97554303a
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-05 07:18:08 +0100

    Change log directory and file names for OSSN
    Updated logging directory and log file names for OSSN configuration.

--- application/configuration/3rd-party/ossn/InitialiseApplicationConfiguration.sh
+++ application/configuration/3rd-party/ossn/InitialiseApplicationConfiguration.sh
@@ -37,13 +37,19 @@
 #######################################################################################################
 set -x 
 
-if ( [ ! -d ${HOME}/logs/application_configuration ] )
+if ( [ ! -d ${HOME}/logs/ossn_configuration ] )
 then
-        /bin/mkdir -p ${HOME}/logs/application_configuration
+        /bin/mkdir -p ${HOME}/logs/ossn_configuration
 fi
 
-exec 1>>${HOME}/logs/application_configuration/ossn_out.log
-exec 2>>${HOME}/logs/application_configuration/ossn_err.log
+log_file="ossn_out_`/bin/date | /bin/sed 's/ //g'`"
+err_file="ossn_err_`/bin/date | /bin/sed 's/ //g'`"
+
+/bin/echo "Log file is at: ${HOME}/logs/ossn_configuration/${log_file}"
+/bin/echo "Error file is at: ${HOME}/logs/ossn_configuration/${err_file}"
+
+exec 1>>${HOME}/logs/ossn_configuration/${log_file}
+exec 2>>${HOME}/logs/ossn_configuration/${err_file}
 
 webroot_directory="`/bin/grep "^WEBROOT_DIRECTORY:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
 
