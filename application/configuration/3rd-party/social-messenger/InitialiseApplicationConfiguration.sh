9e7869227191c5bab561b94cc79e225282111572
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-05 07:19:08 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/3rd-party/social-messenger/InitialiseApplicationConfiguration.sh
+++ application/configuration/3rd-party/social-messenger/InitialiseApplicationConfiguration.sh
@@ -37,10 +37,19 @@
 #######################################################################################################
 set -x 
 
-if ( [ ! -d ${HOME}/logs/application_configuration ] )
+if ( [ ! -d ${HOME}/logs/social_messenger_configuration ] )
 then
-        /bin/mkdir -p ${HOME}/logs/application_configuration
+        /bin/mkdir -p ${HOME}/logs/social_messenger_configuration
 fi
+
+log_file="social_messenger_out_`/bin/date | /bin/sed 's/ //g'`"
+err_file="social_messenger_err_`/bin/date | /bin/sed 's/ //g'`"
+
+/bin/echo "Log file is at: ${HOME}/logs/social_messenger_configuration/${log_file}"
+/bin/echo "Error file is at: ${HOME}/logs/social_messenger_configuration/${err_file}"
+
+exec 1>>${HOME}/logs/social_messenger_configuration/${log_file}
+exec 2>>${HOME}/logs/social_messenger_configuration/${err_file}
 
 
 webroot_directory="`/bin/grep "^WEBROOT_DIRECTORY:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
