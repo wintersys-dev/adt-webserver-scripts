cd83492a87768b298e0a85072587c5a0361785b3
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-05 07:33:35 +0100

    Update ConfigureNginxForApplication.sh

--- webserver/configuration/ConfigureNginxForApplication.sh
+++ webserver/configuration/ConfigureNginxForApplication.sh
@@ -21,6 +21,20 @@
 ##################################################################################
 #set -x
 
+if ( [ ! -d ${HOME}/logs/configure_nginx ] )
+then
+        /bin/mkdir -p ${HOME}/logs/configure_nginx
+fi
+
+log_file="nginx_out_`/bin/date | /bin/sed 's/ //g'`"
+err_file="nginx_err_`/bin/date | /bin/sed 's/ //g'`"
+
+/bin/echo "Log file is at: ${HOME}/logs/configure_nginx/${log_file}"
+/bin/echo "Error file is at: ${HOME}/logs/configure_nginx/${err_file}"
+
+exec 1>>${HOME}/logs/configure_nginx/${log_file}
+exec 2>>${HOME}/logs/configure_nginx/${err_file}
+
 HOME="`/bin/cat /home/homedir.dat`"
 DNS_CHOICE="`${HOME}/utilities/config/ExtractConfigValue.sh 'DNSCHOICE'`"
 PHP_VERSION="`${HOME}/utilities/config/ExtractConfigValue.sh 'PHPVERSION'`"
