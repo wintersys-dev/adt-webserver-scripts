cd275fee593c1bc7b6a4b02cb827a2d5ef11c243
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-05 07:22:13 +0100

    Change log paths for Joomla installation script
    Updated log directory and file paths for Joomla installation.

--- application/installation/cms/joomla/InstallVirginApplication.sh
+++ application/installation/cms/joomla/InstallVirginApplication.sh
@@ -29,13 +29,19 @@
 ######################################################################################
 #set -x
 
-if ( [ ! -d ${HOME}/logs/application_installation ] )
+if ( [ ! -d ${HOME}/logs/joomla_installation ] )
 then
-        /bin/mkdir -p ${HOME}/logs/application_installation
+        /bin/mkdir -p ${HOME}/logs/joomla_installation
 fi
 
-exec 1>>${HOME}/logs/application_installation/joomla_out.log
-exec 2>>${HOME}/logs/application_installation/joomla_err.log
+log_file="joomla_out_`/bin/date | /bin/sed 's/ //g'`"
+err_file="joomla_err_`/bin/date | /bin/sed 's/ //g'`"
+
+/bin/echo "Log file is at: ${HOME}/logs/joomla_installation/${log_file}"
+/bin/echo "Error file is at: ${HOME}/logs/joomla_installation/${err_file}"
+
+exec 1>>${HOME}/logs/joomla_installation/${log_file}
+exec 2>>${HOME}/logs/joomla_installation/${err_file}
 
 if ( [ ! -d ${HOME}/runtime/downloads_work_area ] )
 then
