38526b2dc264efa2acbfbb8019dcc9d1b7aee29a
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-05 07:24:30 +0100

    Fix log file path typo in installation script

--- application/installation/cms/moodle/InstallVirginApplication.sh
+++ application/installation/cms/moodle/InstallVirginApplication.sh
@@ -37,11 +37,11 @@ fi
 log_file="moodle_out_`/bin/date | /bin/sed 's/ //g'`"
 err_file="moodle_err_`/bin/date | /bin/sed 's/ //g'`"
 
-/bin/echo "Log file is at: ${HOME}/logs/moodlel_installation/${log_file}"
+/bin/echo "Log file is at: ${HOME}/logs/moodle_installation/${log_file}"
 /bin/echo "Error file is at: ${HOME}/logs/moodle_installation/${err_file}"
 
 exec 1>>${HOME}/logs/moodle_installation/${log_file}
-exec 2>>${HOME}/logs/moodlel_installation/${err_file}
+exec 2>>${HOME}/logs/moodle_installation/${err_file}
 
 if ( [ ! -d ${HOME}/runtime/downloads_work_area ] )
 then
