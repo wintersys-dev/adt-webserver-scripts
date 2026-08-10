983f77c130fa8f5ab63da8f256196e0aa80b01ac
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-10 11:56:34 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
@@ -20,21 +20,19 @@
 ######################################################################################################
 set -x
 
-if ( [ -f ${HOME}/runtime/INITIAL_CONFIG_SET_FAILED ] )
-then
-        exit
-fi
-
 if ( [ ! -d ${HOME}/logs/drupal_configuration ] )
 then
         /bin/mkdir -p ${HOME}/logs/drupal_configuration
 fi
 
-log_file="drupal_out_`/bin/date | /bin/sed 's/ //g'`"
-err_file="drupal_err_`/bin/date | /bin/sed 's/ //g'`"
+log_file="drupal_configuration_out"
+err_file="drupal_configuration_err"
 
-/bin/echo "Log file is at: ${HOME}/logs/drupal_configuration/${log_file}"
-/bin/echo "Error file is at: ${HOME}/logs/drupal_configuration/${err_file}"
+if ( [ -f ${HOME}/runtime/INITIAL_CONFIG_SET_FAILED ] )
+then
+        /bin/echo "Log file is at: ${HOME}/logs/drupal_configuration/${log_file}"
+        /bin/echo "Error file is at: ${HOME}/logs/drupal_configuration/${err_file}"
+fi
 
 exec 1>>${HOME}/logs/drupal_configuration/${log_file}
 exec 2>>${HOME}/logs/drupal_configuration/${err_file}
@@ -321,6 +319,10 @@ then
         /bin/chmod 600 ${config_file}
         /bin/chown www-data:www-data ${config_file}
         /bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
+        if ( [ -f ${HOME}/runtime/INITIAL_CONFIG_SET_FAILED ] )
+        then
+                /bin/rm ${HOME}/runtime/INITIAL_CONFIG_SET_FAILED
+        fi
 else
         /bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET_FAILED
 fi
