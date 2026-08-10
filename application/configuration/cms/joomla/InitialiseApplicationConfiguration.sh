d4ccd1bfd202f43c80fff72b3671515e932f8abd
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-10 11:55:11 +0100

    Refactor Joomla configuration initialization script
    Remove initial config check and update log file naming.

--- application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
@@ -31,21 +31,19 @@
 #######################################################################################################
 set -x 
 
-if ( [ -f ${HOME}/runtime/INITIAL_CONFIG_SET_FAILED ] )
-then
-        exit
-fi
-
 if ( [ ! -d ${HOME}/logs/joomla_configuration ] )
 then
         /bin/mkdir -p ${HOME}/logs/joomla_configuration
 fi
 
-log_file="joomla_out_`/bin/date | /bin/sed 's/ //g'`"
-err_file="joomla_err_`/bin/date | /bin/sed 's/ //g'`"
+log_file="joomla_configuration_out"
+err_file="joomla_configuration_err"
 
-/bin/echo "Log file is at: ${HOME}/logs/joomla_configuration/${log_file}"
-/bin/echo "Error file is at: ${HOME}/logs/joomla_configuration/${err_file}"
+if ( [ -f ${HOME}/runtime/INITIAL_CONFIG_SET_FAILED ] )
+then
+        /bin/echo "Log file is at: ${HOME}/logs/joomla_configuration/${log_file}"
+        /bin/echo "Error file is at: ${HOME}/logs/joomla_configuration/${err_file}"
+fi
 
 exec 1>>${HOME}/logs/joomla_configuration/${log_file}
 exec 2>>${HOME}/logs/joomla_configuration/${err_file}
@@ -342,6 +340,10 @@ then
         /bin/chmod 600 ${config_file}
         /bin/chown www-data:www-data ${config_file}
         /bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
+        if ( [ -f ${HOME}/runtime/INITIAL_CONFIG_SET_FAILED ] )
+        then
+                /bin/rm ${HOME}/runtime/INITIAL_CONFIG_SET_FAILED
+        fi
 else
         /bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET_FAILED
 fi
