44d27fe07cce96678a1b72badf8ba528eb8be83f
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-10 21:05:17 +0100

    Update InstallVirginApplication.sh

--- application/installation/cms/moodle/InstallVirginApplication.sh
+++ application/installation/cms/moodle/InstallVirginApplication.sh
@@ -66,8 +66,6 @@ then
         ${HOME}/services/git/GitClone.sh "github" "" "moodle" "moodle" "" "${moodle_git_branch}" "${webroot_directory}"
         /bin/chown -R www-data:www-data /var/www/html     
 
-        ls -l /var/www/html/moodle
-
         if ( [ ! -d ${webroot_directory}/vendor ] )
         then
                 BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
@@ -75,7 +73,6 @@ then
                 cd ${webroot_directory}
                 /usr/bin/sudo -u www-data /usr/local/bin/composer install --no-dev --classmap-authoritative
         fi
-        ls -l /var/www/html/moodle
 else
         SOURCECODE_URL="`/bin/grep "^SOURCECODE_URL" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_URL://g' | /bin/sed 's/:/ /g'`"
         SOURCECODE_MD5="`/bin/grep "^SOURCECODE_MD5" ${HOME}/runtime/application.dat | /bin/sed 's/SOURCECODE_MD5://g' | /bin/sed 's/:/ /g'`"
