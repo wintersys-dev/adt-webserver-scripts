b7eeb6187e0c4b1f48c4e7fef78ae11f6d4e4422
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-10 10:05:33 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/moodle/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/moodle/InitialiseApplicationConfiguration.sh
@@ -255,10 +255,10 @@ then
         /bin/sed -i '/.*require_once.*/d' ${config_file}
         #Ugly cludge for apache because I couldn't get moodle to display properly without switching off slash arguments
         #if anyone knows how to sort this please let me know and I will make the fix and remove this cludge
-        if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh WEBSERVERCHOICE:APACHE`" = "1" ] )
-        then
-                /bin/echo '$CFG->slasharguments = false;' >> ${config_file}
-        fi
+     #   if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh WEBSERVERCHOICE:APACHE`" = "1" ] )
+     #   then
+     #           /bin/echo '$CFG->slasharguments = false;' >> ${config_file}
+     #   fi
         /bin/echo "require_once('/var/www/html/moodle/lib/setup.php');" >> ${config_file}
 fi
 
