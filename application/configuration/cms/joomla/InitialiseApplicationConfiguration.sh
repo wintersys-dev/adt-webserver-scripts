085c26830c7d910b637bcc1044534db2ca24f4a1
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-10 10:55:32 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
@@ -31,6 +31,11 @@
 #######################################################################################################
 set -x 
 
+if ( [ -f ${HOME}/runtime/INITIAL_CONFIG_SET_FAILED ] )
+then
+        exit
+fi
+
 if ( [ ! -d ${HOME}/logs/joomla_configuration ] )
 then
         /bin/mkdir -p ${HOME}/logs/joomla_configuration
@@ -337,6 +342,8 @@ then
         /bin/chmod 600 ${config_file}
         /bin/chown www-data:www-data ${config_file}
         /bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
+else
+        /bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET_FAILED
 fi
 
 if ( [ ! -f  ${HOME}/runtime/INITIAL_CONFIG_SET ] )
