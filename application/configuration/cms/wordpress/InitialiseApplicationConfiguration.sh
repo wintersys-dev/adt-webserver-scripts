13510f3bc833079c49e38e7afd9867acb1ef518b
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-11 20:48:25 +0100

    Comment out directory creation logic
    Commented out the section responsible for checking and creating directories based on application.dat.

--- application/configuration/cms/wordpress/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/wordpress/InitialiseApplicationConfiguration.sh
@@ -114,36 +114,36 @@ else
                 HOST="`${HOME}/services/datastore/config/wrapper/ListFromDatastore.sh "config" "databaseip/*"`"
         fi
 
-        if ( [ -f ${HOME}/runtime/application.dat ] )
-        then
-                if ( [ ! -d ${HOME}/runtime/filesystem_sync/webroot-sync/outgoing ] )
-                then
-                        /bin/mkdir -p ${HOME}/runtime/filesystem_sync/webroot-sync/outgoing
-                fi
-
-                if ( [ -f ${HOME}/runtime/filesystem_sync/webroot-sync/outgoing/exclusion_list.dat ] )
-                then
-                        /bin/rm ${HOME}/runtime/filesystem_sync/webroot-sync/outgoing/exclusion_list.dat
-                fi
-
-                for directory in `/bin/grep "^DIRECTORIES_TO_CREATE:" ${HOME}/runtime/application.dat | /bin/sed 's/DIRECTORIES_TO_CREATE://g' | /bin/sed 's/:/ /g'`
-                do
-                        directory="/var/www/html/${directory}"
-
-                        if ( [ ! -d ${directory} ] )
-                        then
-                                /bin/mkdir -p ${directory}
-                                /bin/echo "${directory}" >> ${HOME}/runtime/filesystem_sync/webroot-sync/outgoing/exclusion_list.dat
-                        fi
-
-                        while ( [ "${directory}" != "/var/www/html" ] )
-                        do
-                                /bin/chmod 755 ${directory}
-                                /bin/chown www-data:www-data ${directory}
-                                directory=`/usr/bin/dirname "${directory}"`
-                        done
-                done
-        fi
+    #    if ( [ -f ${HOME}/runtime/application.dat ] )
+    #    then
+    #            if ( [ ! -d ${HOME}/runtime/filesystem_sync/webroot-sync/outgoing ] )
+     #           then
+     #                   /bin/mkdir -p ${HOME}/runtime/filesystem_sync/webroot-sync/outgoing
+      #          fi
+#
+ #               if ( [ -f ${HOME}/runtime/filesystem_sync/webroot-sync/outgoing/exclusion_list.dat ] )
+  #              then
+   #                     /bin/rm ${HOME}/runtime/filesystem_sync/webroot-sync/outgoing/exclusion_list.dat
+    #            fi
+#
+ #               for directory in `/bin/grep "^DIRECTORIES_TO_CREATE:" ${HOME}/runtime/application.dat | /bin/sed 's/DIRECTORIES_TO_CREATE://g' | /bin/sed 's/:/ /g'`
+  #              do
+   #                     directory="/var/www/html/${directory}"
+#
+ #                       if ( [ ! -d ${directory} ] )
+  #                      then
+   #                             /bin/mkdir -p ${directory}
+    #                            /bin/echo "${directory}" >> ${HOME}/runtime/filesystem_sync/webroot-sync/outgoing/exclusion_list.dat
+     #                   fi
+#
+ #                       while ( [ "${directory}" != "/var/www/html" ] )
+  #                      do
+   #                             /bin/chmod 755 ${directory}
+   #                             /bin/chown www-data:www-data ${directory}
+   #                             directory=`/usr/bin/dirname "${directory}"`
+   #                     done
+   #             done
+   #     fi
 
         WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
         website_name="`/bin/grep "^WEBSITE_NAME:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
