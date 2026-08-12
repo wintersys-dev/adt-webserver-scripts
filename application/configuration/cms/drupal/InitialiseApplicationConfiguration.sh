34c9f0f63ecfc9c61e1f9ada0cf41cdac7c99c28
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-12 14:13:12 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
@@ -44,14 +44,20 @@ then
         webroot_directory="/var/www/html/web"
 fi
 
-if ( [ -f ${webroot_directory}/sites/default/default.settings.php ] )
+#if ( [ -f ${webroot_directory}/sites/default/default.settings.php ] )
+#then
+#        if ( [ ! -f ${webroot_directory}/sites/default/settings.php ] )
+#        then
+#                /bin/cp ${webroot_directory}/sites/default/default.settings.php ${webroot_directory}/sites/default/settings.php
+#                /bin/chown www-data:www-data ${webroot_directory}/sites/default/settings.php
+#        fi
+#        /bin/cp ${webroot_directory}/sites/default/default.settings.php /var/www/html/settings.php.default
+#        /bin/chown www-data:www-data /var/www/html/settings.php.default
+#fi
+
+if ( [ -f ${webroot_directory}/sites/default/default.settings.php  ] )
 then
-        if ( [ ! -f ${webroot_directory}/sites/default/settings.php ] )
-        then
-                /bin/cp ${webroot_directory}/sites/default/default.settings.php ${webroot_directory}/sites/default/settings.php
-                /bin/chown www-data:www-data ${webroot_directory}/sites/default/settings.php
-        fi
-        /bin/cp ${webroot_directory}/sites/default/default.settings.php /var/www/html/settings.php.default
+        /bin/cp ${webroot_directory}/sites/default/default.settings.php  /var/www/html/settings.php.default
         /bin/chown www-data:www-data /var/www/html/settings.php.default
 fi
 
