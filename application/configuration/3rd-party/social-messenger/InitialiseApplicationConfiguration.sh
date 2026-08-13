c9fc0d926832f9da567fd19403985a14b0465436
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-13 07:51:51 +0100

    Modify user variable to append '_notls'

--- application/configuration/3rd-party/social-messenger/InitialiseApplicationConfiguration.sh
+++ application/configuration/3rd-party/social-messenger/InitialiseApplicationConfiguration.sh
@@ -154,7 +154,7 @@ else
                 exit
         fi
 
-        user="`/bin/grep "^MANDATORY_INDIVIDUAL_SETTING:user=" ${HOME}/runtime/application.dat | /usr/bin/awk -F'=' '{print $NF}' | /bin/sed "s%'%%g"`"
+        user="`/bin/grep "^MANDATORY_INDIVIDUAL_SETTING:user=" ${HOME}/runtime/application.dat | /usr/bin/awk -F'=' '{print $NF}' | /bin/sed "s%'%%g"`_notls"
         password="`/bin/grep "^MANDATORY_INDIVIDUAL_SETTING:password=" ${HOME}/runtime/application.dat | /usr/bin/awk -F'=' '{print $NF}' | /bin/sed "s%'%%g"`"
         dbname="`/bin/grep "^MANDATORY_INDIVIDUAL_SETTING:db=" ${HOME}/runtime/application.dat | /usr/bin/awk -F'=' '{print $NF}' | /bin/sed "s%'%%g"`"
 
