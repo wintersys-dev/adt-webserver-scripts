fcf0d6c200243308ae900073fcec5d076af4f9f9
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-18 11:10:08 +0100

    Update CheckWebrootImmutabilityStatus.sh

--- utilities/security/CheckWebrootImmutabilityStatus.sh
+++ utilities/security/CheckWebrootImmutabilityStatus.sh
@@ -1,38 +1,22 @@
 if ( [ "`${HOME}/services/datastore/config/wrapper/ListFromDatastore.sh "config" "IMMUTABLE-WEBROOT"`" != "" ] )
 then
-  if ( [ ! -f ${HOME}/runtime/IMMUTABLE-WEBROOT.on ] )
-  then
-    /bin/touch ${HOME}/runtime/IMMUTABLE-WEBROOT.on
-    ${HOME}/utilities/security/EnforcePermissions.sh
-  fi
-  if ( [ -f ${HOME}/runtime/IMMUTABLE-WEBROOT.off ] )
-  then
-    /bin/rm ${HOME}/runtime/IMMUTABLE-WEBROOT.off
-  fi
-  if ( [ -f ${HOME}/runtime/MUTABLE-WEBROOT.on ] )
-  then
-    /bin/rm ${HOME}/runtime/MUTABLE-WEBROOT.on
-  fi
-  if ( [ ! -f ${HOME}/runtime/MUTABLE-WEBROOT.off ] )
-  then
-    /bin/touch ${HOME}/runtime/MUTABLE-WEBROOT.off
-  fi
+        if ( [ ! -f ${HOME}/runtime/IMMUTABLE-WEBROOT-ON ] )
+        then
+                /bin/touch ${HOME}/runtime/IMMUTABLE-WEBROOT-ON
+                ${HOME}/utilities/security/EnforcePermissions.sh
+                if ( [ -f ${HOME}/runtime/IMMUTABLE-WEBROOT-OFF ] )
+                then
+                        /bin/rm ${HOME}/runtime/IMMUTABLE-WEBROOT-OFF
+                fi
+        fi
 else
-  if ( [ ! -f ${HOME}/runtime/MUTABLE-WEBROOT.on ] )
-  then
-    /bin/touch ${HOME}/runtime/MUTABLE-WEBROOT.on
-    ${HOME}/utilities/security/EnforcePermissions.sh
-  fi
-  if ( [ -f ${HOME}/runtime/MUTABLE-WEBROOT.off ] )
-  then
-    /bin/rm ${HOME}/runtime/MUTABLE-WEBROOT.off
-  fi
-  if ( [ -f ${HOME}/runtime/IMMUTABLE-WEBROOT.on ] )
-  then
-    /bin/rm ${HOME}/runtime/IMMUTABLE-WEBROOT.on
-  fi
-  if ( [ ! -f ${HOME}/runtime/IMMUTABLE-WEBROOT.off ] )
-  then
-    /bin/touch ${HOME}/runtime/IMMUTABLE-WEBROOT.off
-  fi
+        if ( [ ! -f ${HOME}/runtime/MUTABLE-WEBROOT-ON ] )
+        then
+                /bin/touch ${HOME}/runtime/MUTABLE-WEBROOT-ON
+                ${HOME}/utilities/security/EnforcePermissions.sh
+                if ( [ -f ${HOME}/runtime/MUTABLE-WEBROOT-OFF ] )
+                then
+                        /bin/rm ${HOME}/runtime/MUTABLE-WEBROOT-OFF
+                fi
+        fi
 fi
