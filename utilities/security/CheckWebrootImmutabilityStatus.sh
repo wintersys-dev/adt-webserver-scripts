6df95c091e94b0223a5911bfa4356f1adbf4bbda
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-18 09:10:28 +0100

    Add script to check webroot immutability status

---
+++ utilities/security/CheckWebrootImmutabilityStatus.sh
@@ -0,0 +1,14 @@
+if ( [ "`${HOME}/services/datastore/config/wrapper/ListFromDatastore.sh "config" "IMMUTABLE-WEBROOT"`" != "" ] )
+then
+  if ( [ -f ${HOME}/runtime/MUTABLE-WEBROOT ] )
+  then
+    /bin/rm ${HOME}/runtime/MUTABLE-WEBROOT 
+  fi
+  /bin/touch ${HOME}/runtime/IMMUTABLE-WEBROOT
+else
+  if ( [ -f ${HOME}/runtime/IMMUTABLE-WEBROOT ] )
+  then
+    /bin/rm ${HOME}/runtime/IMMUTABLE-WEBROOT
+  fi
+  /bin/touch ${HOME}/runtime/MUTABLE-WEBROOT
+fi
