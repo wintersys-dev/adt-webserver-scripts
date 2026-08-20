ce5d8e7905a2955204eda7584ed4ad18af4ca9d3
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-20 19:06:56 +0100

    Update aptitude_wrapper.sh

--- installation/aptitude_wrapper.sh
+++ installation/aptitude_wrapper.sh
@@ -10,4 +10,16 @@ done
 
 /usr/bin/dpkg --configure -a
 command="/usr/bin/aptitude $@"
-eval "${command}"
+
+success="0"
+
+while ( [ "${success}" = "0" ] )
+do
+        eval "${command}"
+        if ( [ "$?" = "0" ] )
+        then    
+                success="1"
+        else
+                /bin/sleep 5
+        fi
+done
