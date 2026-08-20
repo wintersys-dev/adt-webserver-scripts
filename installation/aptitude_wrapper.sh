565044b9a0d0e60dfb04c1282c87ec09bbb89b02
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-20 19:08:54 +0100

    Limit retries for aptitude command to 30 attempts

--- installation/aptitude_wrapper.sh
+++ installation/aptitude_wrapper.sh
@@ -12,14 +12,21 @@ done
 command="/usr/bin/aptitude $@"
 
 success="0"
+count="0"
 
-while ( [ "${success}" = "0" ] )
+while ( [ "${success}" = "0" ] && [ "${count}" -lt "30" ] )
 do
         eval "${command}"
         if ( [ "$?" = "0" ] )
         then    
                 success="1"
         else
+                count="`/usr/bin/expr ${count} + 1`"
                 /bin/sleep 5
         fi
 done
+
+if ( [ "${count}" = "30" ] )
+then
+        /bin/echo "command: ${command} failed"
+fi
