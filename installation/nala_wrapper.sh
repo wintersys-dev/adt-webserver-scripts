95a61fa4e5724e9ea6a1bea4c82f880079c18447
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-23 11:12:23 +0100

    Create nala_wrapper.sh

---
+++ installation/nala_wrapper.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -x
+
+while ( [ "`/usr/bin/fuser /var/lib/dpkg/lock-frontend`" != "" ] )
+do
+        echo "Waiting for another package manager process to complete..."
+        sleep 5
+done
+
+/usr/bin/dpkg --configure -a
+command="/usr/bin/nala $@"
+
+success="0"
+count="0"
+
+while ( [ "${success}" = "0" ] && [ "${count}" -lt "30" ] )
+do
+        eval "${command}"
+        if ( [ "$?" = "0" ] )
+        then
+                success="1"
+        else
+                count="`/usr/bin/expr ${count} + 1`"
+                /bin/sleep 5
+        fi
+done
+
+if ( [ "${count}" = "30" ] )
+then
+        /bin/echo "command: ${command} failed"
+fi
