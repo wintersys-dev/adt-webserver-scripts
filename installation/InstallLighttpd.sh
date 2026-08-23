e23b0f1310467410a634124d9575591707d93a67
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-24 00:32:31 +0100

    Update InstallLighttpd.sh

--- installation/InstallLighttpd.sh
+++ installation/InstallLighttpd.sh
@@ -60,7 +60,7 @@ install_command="${manager} ${options} install"
 ${HOME}/installation/PurgeApache.sh
 
 cloud_init="0"
-if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "LIGHTTPD" | /bin/grep ':cloud-init'`" = "" ] )
+if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "LIGHTTPD" | /bin/grep ':cloud-init'`" != "" ] )
 then    
         cloud_init="1"
 fi
