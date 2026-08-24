b487da5b3d35ac531bdb79ea75153a2bb148fe3b
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-24 23:13:28 +0100

    Update CheckIfApplicationIsInstalled.sh

--- application/monitoring/3rd-party/social-messenger/CheckIfApplicationIsInstalled.sh
+++ application/monitoring/3rd-party/social-messenger/CheckIfApplicationIsInstalled.sh
@@ -42,7 +42,7 @@ fi
 
 if ( [ "${webroot_directory}" = "" ] )
 then
-        webroot_directory="/var/www/html/joomla"
+        webroot_directory="/var/www/html/social-messenger"
 fi
 
 installed="1"
