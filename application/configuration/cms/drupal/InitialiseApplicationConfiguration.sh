3e7ee7c24cfd5988feb0de070a954c2c76f89fa2
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-25 06:39:16 +0100

    Update default webroot directory for Drupal
    Change default webroot directory from '/var/www/html/web' to '/var/www/html/drupal'.

--- application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
@@ -19,11 +19,22 @@ fi
 #exec 1>>${HOME}/logs/drupal_configuration/${log_file}
 #exec 2>>${HOME}/logs/drupal_configuration/${err_file}
 
+#Extract the value of the webroot directory from the application descriptor and if its not set, fall back to a default value
 webroot_directory="`/bin/grep "^WEBROOT_DIRECTORY:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
+
+if ( [ -f /var/www/html/wr.dat ] )
+then
+        archived_webroot_directory="`/bin/cat /var/www/html/wr.dat`"
+fi
+
+if ( [ "${webroot_directory}" != "${archived_webroot_directory}" ] && [ "${archived_webroot_directory}" != "" ] )
+then
+        webroot_directory="${archived_webroot_directory}"
+fi
 
 if ( [ "${webroot_directory}" = "" ] )
 then
-        webroot_directory="/var/www/html/web"
+        webroot_directory="/var/www/html/drupal"
 fi
 
 if ( [ -f ${webroot_directory}/web/sites/default/default.settings.php ] )
