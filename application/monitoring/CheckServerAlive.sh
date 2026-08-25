013d6f31278a2636324f1fb9143b57eab7ba4241
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-25 09:05:26 +0100

    Update CheckServerAlive.sh

--- application/monitoring/CheckServerAlive.sh
+++ application/monitoring/CheckServerAlive.sh
@@ -40,6 +40,16 @@ DB_U="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBUSERNAME'`"
 DB_P="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBPASSWORD'`"
 DB_N="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBNAME'`"
 
+if ( [ "`/bin/echo ${DB_U} | /bin/grep ':::'`" != "" ] )
+then
+        DB_U="`/bin/echo ${DB_U} | /usr/bin/awk -F':::' '{print $2}'`"
+fi
+
+if ( [ "`/bin/echo ${DB_P} | /bin/grep ':::'`" != "" ] )
+then
+        DB_P="`/bin/echo ${DB_P} | /usr/bin/awk -F':::' '{print $2}'`"
+fi
+
 if ( [ "`/bin/grep -- "${DB_N}" ${config_file}`" = "" ] || [ "`/bin/grep -- "${DB_P}" ${config_file}`" = "" ] || [ "`/bin/grep -- "${DB_U}" ${config_file}`" = "" ] )
 then
 	exit
