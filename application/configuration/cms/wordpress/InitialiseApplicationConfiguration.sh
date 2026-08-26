1870f5cbe1d8566663630ae5adcd9462f50967b2
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-26 12:21:03 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/wordpress/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/wordpress/InitialiseApplicationConfiguration.sh
@@ -243,3 +243,23 @@ then
         fi
 fi
 
+for setting in `/bin/grep "^INDIVIDUAL_SETTING:" ${HOME}/runtime/application.dat | /bin/sed 's/^INDIVIDUAL_SETTING://g' | /usr/bin/awk -F'::' '{print $NF}' | /bin/sed 's/^://g'`
+do
+        label="`/bin/echo ${setting} | /usr/bin/awk -F'=' '{print $1}'`"
+        value="`/bin/echo ${setting} | /usr/bin/awk -F'=' '{print $NF}'`"
+        if ( [ "${label}" != "" ] && [ "${value}" != "" ] )
+        then
+                /usr/bin/sudo -u www-data wp config set "${label}" "${value}" --config-file="${config_file}"
+        fi
+done
+
+for setting in `/bin/grep "^INDIVIDUAL_SETTING_UNQUOTED:" ${HOME}/runtime/application.dat | /bin/sed 's/^INDIVIDUAL_SETTING://g' | /usr/bin/awk -F'::' '{print $NF}' | /bin/sed 's/^://g'`
+do
+        label="`/bin/echo ${setting} | /usr/bin/awk -F'=' '{print $1}'`"
+        value="`/bin/echo ${setting} | /usr/bin/awk -F'=' '{print $NF}'`"
+        if ( [ "${label}" != "" ] && [ "${value}" != "" ] )
+        then
+                /usr/bin/sudo -u www-data wp config set "${label}" "${value}" --raw --config-file="${config_file}"
+        fi
+done
+
