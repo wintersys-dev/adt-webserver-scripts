3c22505708b6e5df96b6f2a9eaff2a942caa6e88
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-26 12:13:16 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/wordpress/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/wordpress/InitialiseApplicationConfiguration.sh
@@ -97,3 +97,18 @@ if ( [ "${config_file}" = "" ] )
 then
         config_file="/var/www/outside_webroot/wp-config.php"
 fi
+
+#This tests of the current deployment is intended to be interactive and if it is we block until the user has entered the requisite input data
+#using their browser
+if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
+then
+        if ( [ ! -f ${webroot_directory}/wp-config.php ] )
+        then
+                while ( [ ! -f ${webroot_directory}/wp-config.php ] )
+                do
+                        /bin/sleep 1
+                done
+        fi
+        /bin/echo "`/bin/grep "dbprefix" ${webroot_directory}/wp-config.php | /usr/bin/awk -F"'" '{print $2}'`" > /var/www/html/dbp.dat
+        /bin/chown www-data:www-data /var/www/html/dbp.dat
+else
