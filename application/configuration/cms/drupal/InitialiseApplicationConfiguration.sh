da3efe627529984434278f1df184ffa3b29507db
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-27 15:34:48 +0100

    Remove echo statement from configuration script
    Removed echo statement for better log management.

--- application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
@@ -19,8 +19,6 @@ fi
 exec 1>>${HOME}/logs/drupal_configuration/${log_file}
 exec 2>>${HOME}/logs/drupal_configuration/${err_file}
 
-/bin/echo "================================================"
-
 #Extract the value of the webroot directory from the application descriptor and if its not set, fall back to a default value
 webroot_directory="`/bin/grep "^WEBROOT_DIRECTORY:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
 
