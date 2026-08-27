13cef1ef437842594627188c85f031d2b3dbe6a0
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-27 15:37:29 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
@@ -1,6 +1,36 @@
 #!/bin/sh
-
-set -x
+###########################################################################################################
+# Description:This script will generate a ${webroot_directory}/web/sites/default/settings.php using the values 
+# that you have set in
+#
+#        ${BUILD_HOME}/application/descriptors/drupal.dat
+#
+# If a virgin copy of drupal is being installed, then, /usr/sbin/drush is used when making a non-interactive 
+# installation this means that the installer doesn't have to do anything once they  have started the build the 
+# next thing they will see is a fully configured virgin drupal application. 
+# If you are deploying a baseline or a temporal backup then the settings.php file is manually generated
+# based on the values set in 
+#
+#         ${BUILD_HOME}/application/descriptors/drupal.dat
+#
+# Author : Peter Winter
+# Date: 17/05/2017
+######################################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################################
+#######################################################################################################
+set -x 
 
 if ( [ ! -d ${HOME}/logs/drupal_configuration ] )
 then
