0fdfc5f69e2e08b87d4af91e031818dabd871dbc
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-28 10:01:11 +0100

    Update InstallDrush.sh

--- installation/InstallDrush.sh
+++ installation/InstallDrush.sh
@@ -38,7 +38,8 @@ count="0"
 while ( [ ! -x /usr/sbin/drush ] && [ "${count}" -lt "5" ] )
 do
 	webroot_directory="`/bin/grep "^WEBROOT_DIRECTORY:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
-
+	cd ${webroot_directory}
+	
 	if ( [ "${BUILDOS}" = "ubuntu" ] )
 	then
                 /usr/bin/sudo -u www-data /usr/local/bin/composer require drush/drush --no-interaction 
