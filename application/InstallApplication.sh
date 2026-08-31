897355d09f33535432588e72d1cca779891ffb25
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-31 19:14:11 +0100

    Update InstallApplication.sh

--- application/InstallApplication.sh
+++ application/InstallApplication.sh
@@ -21,19 +21,19 @@
 #######################################################################################################
 set -x
 
-if ( [ ! -d ${HOME}/logs/installation ] )
+if ( [ ! -d ${HOME}/logs/application_installation ] )
 then
-        /bin/mkdir -p ${HOME}/logs/installation
+        /bin/mkdir -p ${HOME}/logs/application_installation
 fi
 
 log_file="installation_out_`/bin/date | /bin/sed 's/ //g'`"
 err_file="installation_err_`/bin/date | /bin/sed 's/ //g'`"
 
-/bin/echo "Log file is at: ${HOME}/logs/installation/${log_file}"
-/bin/echo "Error file is at: ${HOME}/logs/installation/${err_file}"
+/bin/echo "Log file is at: ${HOME}/logs/application_installation/${log_file}"
+/bin/echo "Error file is at: ${HOME}/logs/application_installation/${err_file}"
 
-exec 1>>${HOME}/logs/installation/${log_file}
-exec 2>>${HOME}/logs/installation/${err_file}
+exec 1>>${HOME}/logs/application_installation/${log_file}
+exec 2>>${HOME}/logs/application_installation/${err_file}
 
 archive_id="${1}"
 
