c5941175ecf8cd48016d69303a075345c340c03f
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-31 09:52:07 +0100

    Update InstallVirginApplication.sh

--- application/installation/cms/drupal/InstallVirginApplication.sh
+++ application/installation/cms/drupal/InstallVirginApplication.sh
@@ -41,8 +41,6 @@ then
         /usr/bin/sudo -u www-data /usr/local/bin/composer create-project ${drupal_version} ${webroot_directory} --no-interaction --no-install
         cd ${webroot_directory}
         /usr/bin/sudo -u www-data /usr/local/bin/composer install
-        /usr/bin/sudo -u www-data /usr/local/bin/composer require droptica/droopler --with-all-dependencies
-
 
         ${HOME}/installation/InstallDrush.sh ${BUILDOS}
 
