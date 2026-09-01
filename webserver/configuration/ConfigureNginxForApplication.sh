6bf33bd867b6fac19e80048ffe890399275eb1be
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-01 13:23:17 +0100

    Update ConfigureNginxForApplication.sh

--- webserver/configuration/ConfigureNginxForApplication.sh
+++ webserver/configuration/ConfigureNginxForApplication.sh
@@ -47,6 +47,7 @@ port="`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PHP" "stripped" | /u
 
 #Extract the value of the webroot directory from the application descriptor and if its not set, fall back to a default value
 webroot_directory="`/bin/grep "^WEBROOT_DIRECTORY:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
+webroot_subdirectory="`/bin/grep "^WEBROOT_SUBDIRECTORY:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
 
 if ( [ -f /var/www/html/wr.dat ] )
 then
