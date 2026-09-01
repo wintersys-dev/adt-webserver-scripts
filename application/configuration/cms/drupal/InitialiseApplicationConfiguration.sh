61c6f5ecb0de54efdecc12c3465be18758324b2f
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-01 13:33:13 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ###########################################################################################################
-# Description:This script will generate a ${webroot_directory}/web/sites/default/settings.php using the values 
-# that you have set in
+# Description:This script will generate a ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php 
+# using the values that you have set in
 #
 #        ${BUILD_HOME}/application/descriptors/drupal.dat
 #
