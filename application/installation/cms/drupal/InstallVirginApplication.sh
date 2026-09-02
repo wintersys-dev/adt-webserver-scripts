b162b2cf6671db68cbf309777584e6876aa00629
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-02 20:27:49 +0100

    Update InstallVirginApplication.sh

--- application/installation/cms/drupal/InstallVirginApplication.sh
+++ application/installation/cms/drupal/InstallVirginApplication.sh
@@ -84,18 +84,38 @@ then
         cd ${webroot_directory}
         /usr/bin/sudo -u www-data /usr/local/bin/composer install
 #this is needed for CMS version 1
-        ${HOME}/installation/InstallDrush.sh ${BUILDOS}
+      #  ${HOME}/installation/InstallDrush.sh ${BUILDOS}
 
-  #      if ( [ -f ${webroot_directory}/vendor/bin/drush.php ] )
-  #      then
-  #              /bin/echo "/bin/chmod 755 ${webroot_directory}/vendor/bin/drush.php"> /usr/sbin/drush
-  #              /bin/echo "/bin/chmod 755 ${webroot_directory}/vendor/drush/drush" >> /usr/sbin/drush
-  #              /bin/echo "/usr/bin/php ${webroot_directory}/vendor/bin/drush.php \$@" >> /usr/sbin/drush
-  #              /bin/chmod 750 /usr/sbin/drush
-   #     fi
+        if ( [ -f ${webroot_directory}/vendor/bin/drush.php ] )
+        then
+                /bin/echo "/bin/chmod 755 ${webroot_directory}/vendor/bin/drush.php"> /usr/sbin/drush
+                /bin/echo "/bin/chmod 755 ${webroot_directory}/vendor/drush/drush" >> /usr/sbin/drush
+                /bin/echo "/usr/bin/php ${webroot_directory}/vendor/bin/drush.php \$@" >> /usr/sbin/drush
+                /bin/chmod 750 /usr/sbin/drush
+        fi
 
         cd ${HOME}
         /bin/echo "CMS_DRUPAL" > /var/www/html/dbt.dat
+        /bin/echo "success"
+elif ( [ "`/bin/grep "^APPLICATION_TYPE:opensocial" ${HOME}/runtime/application.dat`" != "" ] )
+then
+        cd ${HOME}
+        BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+        ${HOME}/installation/InstallComposer.sh ${BUILDOS}
+        /bin/rm -r /var/www/*
+        /bin/chown www-data:www-data /var/www
+        opensocial_version="`/bin/grep "^OPENSOCIAL_VERSION:" ${HOME}/runtime/application.dat | /bin/sed 's/^OPENSOCIAL_VERSION://g'`"
+        /usr/bin/sudo -u www-data /usr/local/bin/composer create-project ${opensocial_version} ${webroot_directory} --no-interaction --no-install
+        verify_php_version
+        cd ${webroot_directory}
+        /usr/bin/sudo -u www-data /usr/local/bin/composer config allow-plugins true
+        /usr/bin/sudo -u www-data /usr/local/bin/composer update --with-all-dependencies --no-security-blocking
+        /usr/bin/sudo -u www-data /usr/local/bin/composer install
+
+        ${HOME}/installation/InstallDrush.sh ${BUILDOS}
+
+        cd ${HOME}
+        /bin/echo "OPENSOCIAL" > /var/www/html/dbt.dat
         /bin/echo "success"
 
 fi 
