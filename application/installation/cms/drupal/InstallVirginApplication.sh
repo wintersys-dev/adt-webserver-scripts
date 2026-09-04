d76e47dd48e79f6caacc7aae984a4bbefa8bfdcf
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-04 09:44:01 +0100

    Add installation steps for OpenSocial application

--- application/installation/cms/drupal/InstallVirginApplication.sh
+++ application/installation/cms/drupal/InstallVirginApplication.sh
@@ -98,4 +98,24 @@ then
         cd ${HOME}
         /bin/echo "CMS_DRUPAL" > /var/www/html/dbt.dat
         /bin/echo "success"
+#elif ( [ "`/bin/grep "^APPLICATION_TYPE:opensocial" ${HOME}/runtime/application.dat`" != "" ] )
+#then
+else 
+        cd ${HOME}
+        BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
+        ${HOME}/installation/InstallComposer.sh ${BUILDOS}
+  
+        /bin/rm -r /var/www/*
+        /bin/chown www-data:www-data /var/www
+        ${HOME}/services/git/GitClone.sh "github" "" "goalgorilla" "open_social" "" "releese/13.0.0-stable" "${webroot_directory}"
+        /bin/chown -R www-data:www-data /var/www/html
+        verify_php_version
+        cd ${webroot_directory}
+        /usr/bin/sudo -u www-data /usr/local/bin/composer install
+        ${HOME}/installation/InstallDrush.sh ${BUILDOS}
+
+        cd ${HOME}
+        /bin/echo "CMS_DRUPAL" > /var/www/html/dbt.dat
+        /bin/echo "success"
+
 fi
