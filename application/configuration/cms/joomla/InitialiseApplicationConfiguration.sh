9c5ac5ddc2587ccd9f1b35d0ad9ccfd873cbe8cd
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-06 22:51:50 +0100

    Comment out Joomla CLI commands in script
    Comment out Joomla CLI commands for version and update checks.

--- application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
@@ -98,6 +98,10 @@ fi
 #using their browser
 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
 then
+#php cli/joomla.php --version
+#php cli/joomla.php core:check-updates - checks that joomla can interact with database
+
+
         if ( [ ! -f ${webroot_directory}/configuration.php ] )
         then
            #     while ( [ ! -f ${webroot_directory}/configuration.php ] )
