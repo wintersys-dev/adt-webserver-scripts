10848b0cf94ffc0fb2620580175ac39acaeb25e9
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-06 22:46:14 +0100

    Add comment for wp core installation check
    Added a comment regarding the wp core installation check.

--- application/configuration/cms/wordpress/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/wordpress/InitialiseApplicationConfiguration.sh
@@ -97,6 +97,8 @@ fi
 #using their browser
 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] && [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
 then
+#wp core is-installed
+
         if ( [ ! -f ${webroot_directory}/wp-config.php ] )
         then
                 while ( [ ! -f ${webroot_directory}/wp-config.php ] )
