b61a43be242ac98d8f1cadd1c85bac6b96c277de
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-08 23:44:33 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
@@ -324,7 +324,6 @@ fi
 /bin/echo "<?php require( '${config_file}' ); ?>" > ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php
 /bin/chown www-data:www-data ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php
 /bin/chmod 600 ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php
-#/usr/bin/chattr -i  ${config_file}
 /bin/chown www-data:www-data ${config_file}
 /bin/chmod 600 ${config_file}
 
