76b1d1eb9c7520c80a36285aed67778ea1313772
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-08 11:53:57 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
@@ -202,7 +202,7 @@ if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`
 then
         if ( [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
         then
-                /usr/bin/chattr -i ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php
+                /usr/bin/chattr +i ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php
                 #If this string varies in later releases or is removed then another alternative string will have  to be checked for to signify a completed install                
                 #while ( [ "`/usr/bin/curl --insecure https://localhost:443/index.php 2>/dev/null | /bin/grep "Congratulations and welcome to the Drupal community"`" = "" ] )
                
@@ -319,7 +319,7 @@ fi
 /bin/echo "<?php require( '${config_file}' ); ?>" > ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php
 /bin/chown www-data:www-data ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php
 /bin/chmod 600 ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php
-/usr/bin/chattr +i ${config_file}
+/usr/bin/chattr -i ${config_file}
 /bin/chown www-data:www-data ${config_file}
 /bin/chmod 600 ${config_file}
 
