b367b303c5a7bd679917480ed617eeef92d7769e
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-09 15:13:57 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
@@ -146,15 +146,12 @@ if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`
 then
         if ( [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
         then
-                if ( [ ! -f ${webroot_directory}/configuration.php ] )
-                then
-                        /bin/touch ${webroot_directory}/configuration.php
-                fi
+                /bin/cp /var/www/html/configuration.php.default ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php.orig
 
                 ready="0"
                 while ( [ "${ready}" = "0" ] )
                 do
-                        if ( [ "`/usr/bin/diff ${webroot_directory}/configuration.php ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php.orig`" != "" ] )
+                        if ( [ -f ${webroot_directory}/configuration.php ] && [ "`/usr/bin/diff ${webroot_directory}/configuration.php ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php.orig`" != "" ] )
                         then
                                 while ( [ "${ready}" = "0" ] )
                                 do
@@ -166,6 +163,7 @@ then
                                         /bin/sleep 5
                                 done  
                         fi
+                        /bin/sleep 5
                 done
         else
                 #Obtain the database credentials from the application descriptor because this is not an interactive installation
