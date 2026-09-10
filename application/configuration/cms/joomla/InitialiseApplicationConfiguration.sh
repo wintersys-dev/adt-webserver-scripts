bfd9a989697d84f89b45b476939aadee07f95835
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-10 18:50:41 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
@@ -95,6 +95,16 @@ then
         config_file="/var/www/outside_webroot/configuration.php"
 fi
 
+if ( [ -f ${config_file} ] )
+then
+        /bin/rm ${config_file}
+fi
+
+if ( [ -f ${webroot_directory}/configuration.php ] ) 
+then
+        /bin/rm ${webroot_directory}/configuration.php
+fi
+
 # Make sure that the session save path directory is set and exists as sometimes this causes an issue if its not set correctly
 session_save_path="`/bin/grep "^CONFIG_PHP_INI:" ${HOME}/runtime/application.dat | /bin/sed 's/:/ /g' | /bin/grep -o '[^[:space:]]*session.save_path[^[:space:]]*' | /usr/bin/awk -F'=' '{print $NF}'`"
 
