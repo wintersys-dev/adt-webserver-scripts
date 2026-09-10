b3d95ac4ed05fe403c3ec8c1632eda7c427062ce
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-10 10:06:17 +0100

    Refactor Joomla application configuration script

--- application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/joomla/InitialiseApplicationConfiguration.sh
@@ -168,18 +168,32 @@ if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`
 then
         if ( [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
         then
-                while inotifywait -e create --include '_Joomla' ${webroot_directory}/installation
+               # while inotifywait -e create --include '_Joomla' ${webroot_directory}/installation
+               # /usr/bin/inotifywait ${webroot_directory} -e create | while read -r file
+                
+                /usr/bin/inotifywait -e create --include 'configuration\.php' ${webroot_directory} | while read
                 do
-                        if ( [ -f ${webroot_directory}/installation/_Joomla* ] )
-                        then
-                                /bin/rm ${webroot_directory}/installation/_Joomla*
-                                while ( [ "`/usr/bin/php /var/www/html/joomla/cli/joomla.php core:check-updates | /bin/grep 'Your current Joomla version is'`" = "" ] )
-                                do
-                                        /bin/sleep 1
-                                done
-                                break
-                        fi
-                done        
+                        /bin/rm ${webroot_directory}/installation/_Joomla*
+                done
+
+                while ( [ "`/usr/bin/php ${webroot_directory}/cli/joomla.php core:check-updates | /bin/grep 'Your current Joomla version is'`" = "" ] )
+                do
+                        /bin/sleep 1
+                done
+                        
+
+                #while inotifywait -e create --include '_Joomla' ${webroot_directory}/installation
+                #do
+                #        if ( [ -f ${webroot_directory}/installation/_Joomla* ] )
+                #        then
+                #                /bin/rm ${webroot_directory}/installation/_Joomla*
+                #                while ( [ "`/usr/bin/php /var/www/html/joomla/cli/joomla.php core:check-updates | /bin/grep 'Your current Joomla version is'`" = "" ] )
+                #                do
+                #                        /bin/sleep 1
+                #                done
+                 #               break
+                # #       fi
+                #done        
         else
                 #Obtain the database credentials from the application descriptor because this is not an interactive installation
                 /usr/bin/php ${webroot_directory}/installation/joomla.php install --site-name="${website_name}" --admin-user="${website_user_description}" --admin-email="${webmaster_email}" --admin-username="${website_username}" --admin-password="${website_password}"  --db-type="${driver}" --db-host="${HOST}:${DB_PORT}"  --db-user=${user} --db-pass=${password} --db-name=${db}  --db-prefix=${dbprefix} --db-encryption=1 --no-interaction  
