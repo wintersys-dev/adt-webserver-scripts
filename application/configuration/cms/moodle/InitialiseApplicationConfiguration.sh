f0ce6f4345e0ac4ed99324bb07be1fb9c62895f3
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-11 18:33:42 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/moodle/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/moodle/InitialiseApplicationConfiguration.sh
@@ -214,7 +214,10 @@ then
                         /bin/sed -i "/\$CFG->dboptions/a     'ssl' => 'require'," ${webroot_directory}/config.php    
                 done     
 
-                #need to have a curl command waiting for the end of the installation
+                while ( [ "`/usr/bin/sudo -u www-data /usr/bin/php ${webroot_directory}/public/admin/cli/upgrade.php --non-interactive`" != "No upgrade needed" ] )
+                do
+                        /bin/sleep 1
+                done
         else
                 PHP_VERSION="`${HOME}/utilities/config/ExtractConfigValue.sh 'PHPVERSION'`"
                 /bin/sed -i 's/.*max_input_vars.*/max_input_vars = 6000/' /etc/php/${PHP_VERSION}/cli/php.ini
