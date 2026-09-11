0fdce18832e44fa036bc94977ecdba3ebb56a01f
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-11 22:51:30 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/moodle/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/moodle/InitialiseApplicationConfiguration.sh
@@ -392,8 +392,6 @@ fi
 
 if ( [ "$?" = "0" ] )
 then
-        /bin/chmod 600 ${config_file}
-        /bin/chown root:www-data ${config_file}
         /bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
         #    ${HOME}/utilities/security/EnforcePermissions.sh 
 
