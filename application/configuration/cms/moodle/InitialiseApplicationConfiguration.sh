1810aead5d317c5f61209f6b6672bc27eaf74574
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-11 12:22:34 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/moodle/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/moodle/InitialiseApplicationConfiguration.sh
@@ -195,6 +195,12 @@ if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`
 then
         if ( [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
         then
+                        WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
+                /bin/cp ${webroot_directory}/config-dist.php ${webroot_directory}/config.php
+                /bin/sed -i "/\$CFG->dboptions/a     'ssl' => 'require'," ${webroot_directory}/config.php
+                /bin/sed -i "s,^\$CFG->dataroot  = '/home/example/moodledata';,\$CFG->dataroot  = '/var/www/html/moodle/moodledata';," ${webroot_directory}/config.php
+                /bin/sed -i "s,^\$CFG->wwwroot  = '.*';,\$CFG->wwwroot  = 'https://"${WEBSITE_URL}"';," ${webroot_directory}/config.php
+                /usr/bin/php /var/www/html/moodle/admin/cli/install.php --skip-database --agree-license --non-interactive --adminuser="${website_username}" --adminpass="${website_password}" --adminemail="${webmaster_email}" --dbport="${DB_PORT}" --dbhost="${HOST}" --dbuser="${dbuser}" --dbpass="${dbpass}" --dbname="${dbname}" --dbtype="${dbtype}" --prefix="${dbprefix}" --wwwroot="https://${WEBSITE_URL}" --dataroot="/var/www/html/moodle/moodledata" --fullname="${website_fullname}" --shortname="${website_shortname}" --chmod=2750
                 if ( [ ! -f ${webroot_directory}/config.php ] )
                 then
                         while ( [ ! -f ${webroot_directory}/config.php ] )
