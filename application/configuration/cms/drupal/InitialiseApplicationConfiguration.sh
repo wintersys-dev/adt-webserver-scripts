d79d9a59b44a68de2e2ed2490a050cc2b385de02
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-11 23:42:59 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
@@ -341,6 +341,9 @@ then
                         /usr/sbin/drush cr -y
                 done
         fi
+
+        /bin/grep "prefix" ${webroot_directory}/sites/default/settings.php | /usr/bin/awk -F"'" '{print $4}' > /var/www/html/dbp.dat
+        /bin/chown www-data:www-data /var/www/html/dbp.dat
 else    
       #  if ( [ -f ${webroot_directory}/${webroot_subdirectory}/sites/default/default.settings.php ] )
       #  then
