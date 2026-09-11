66e48ca9d0e294daa1e3456b3a55458f893c3cc3
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-11 18:26:30 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/cms/moodle/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/moodle/InitialiseApplicationConfiguration.sh
@@ -195,9 +195,9 @@ if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`
 then
         if ( [ "`/bin/grep "^INTERACTIVE_APPLICATION_INSTALL" ${HOME}/runtime/application.dat | /bin/sed 's/INTERACTIVE_APPLICATION_INSTALL://g' | /bin/sed 's/:/ /g'`" = "yes" ] )
         then
+                dbuser.orig="${dbuser}"
                 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Postgres`" = "0" ] )
                 then
-                        dbuser.orig="${dbuser}"
                         dbuser="${dbuser}_notls"
                 fi
                 
@@ -217,9 +217,9 @@ then
                 PHP_VERSION="`${HOME}/utilities/config/ExtractConfigValue.sh 'PHPVERSION'`"
                 /bin/sed -i 's/.*max_input_vars.*/max_input_vars = 6000/' /etc/php/${PHP_VERSION}/cli/php.ini
                 WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
+                dbuser.orig="${dbuser}"
                 if ( [ "`${HOME}/utilities/config/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:Postgres`" = "0" ] )
                 then
-                        dbuser.orig="${dbuser}"
                         dbuser="${dbuser}_notls"
                 fi
                 /usr/bin/php /var/www/html/moodle/admin/cli/install.php --agree-license --non-interactive --adminuser="${website_username}" --adminpass="${website_password}" --adminemail="${webmaster_email}" --dbport="${DB_PORT}" --dbhost="${HOST}" --dbuser="${dbuser}" --dbpass="${dbpass}" --dbname="${dbname}" --dbtype="${dbtype}" --prefix="${dbprefix}" --wwwroot="https://${WEBSITE_URL}" --dataroot="/var/www/html/moodledata" --fullname="${website_fullname}" --shortname="${website_shortname}" --chmod=2750 
