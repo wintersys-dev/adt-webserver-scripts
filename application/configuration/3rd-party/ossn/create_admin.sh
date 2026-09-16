2400f84adb8bc470f9ffb4539fd281692e9f8452
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-16 11:49:45 +0100

    Update create_admin.sh

--- application/configuration/3rd-party/ossn/create_admin.sh
+++ application/configuration/3rd-party/ossn/create_admin.sh
@@ -1,19 +1,28 @@
 INSERT INTO `ossn_users` (
-    `username`, 
-    `email`, 
-    `password`, 
-    `first_name`, 
-    `last_name`, 
-    `type`, 
-    `activated`, 
-    `time_created`
-) VALUES (
-    'XXXXWEBMASTER_USERNAMEXXXX', 
-    'XXXXWEBMASTER_EMAILXXXX', 
-    'XXXXWEBMASTER_PASSWORDXXXX', 
-    'System', 
-    'Administrator', 
-    'admin', 
-    1, 
-    UNIX_TIMESTAMP()
-);
+        `username`, 
+        `email`, 
+        `password`, 
+        `salt`,
+        `first_name`, 
+        `last_name`, 
+        `type`, 
+        `last_login`,
+        `last_activity`,
+        `activation`, 
+        `time_created`,
+        `time_updated`
+        ) VALUES (
+                'XXXXWEBMASTER_USERNAMEXXXX', 
+                'XXXXWEBMASTER_EMAILXXXX', 
+                'XXXXWEBMASTER_PASSWORDXXXX', 
+                'XXXXSALTXXXX',
+                'System', 
+                'Administrator', 
+                'admin', 
+                UNIX_TIMESTAMP(),
+                UNIX_TIMESTAMP(),
+                1, 
+                UNIX_TIMESTAMP(),
+                UNIX_TIMESTAMP()
+                );
+~                     
