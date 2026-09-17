51c8160417108347f719664df45df1231dcd9d02
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-17 15:06:24 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/3rd-party/ossn/InitialiseApplicationConfiguration.sh
+++ application/configuration/3rd-party/ossn/InitialiseApplicationConfiguration.sh
@@ -206,7 +206,9 @@ else
                 /bin/sed -i "s/<<owner_email>>/${owner_email}/" ${webroot_directory}/installation/sql/opensource-socialnetwork.sql
                 notification_email="`/bin/grep "^WEBMASTER_EMAIL:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
                 /bin/sed -i "s/<<notification_email>>/${notification_email}/" ${webroot_directory}/installation/sql/opensource-socialnetwork.sql
-                /bin/cp ${HOME}/application/configuration/3rd-party/ossn/non-interactive.php ${webroot_directory}/non-interactive.php
+                /bin/cp ${HOME}/application/configuration/3rd-party/ossn/non-interactive.php ${HOME}/runtime/non-interactive.php
+                /bin/sed "/^\$errors.*/ r ${HOME}/runtime/non-interactive.php" ${webroot_directory}/actions/administrator/settings/save/basic.php
+                /bin/rm ${HOME}/runtime/non-interactive.php
                 ${HOME}/utilities/remote/ConnectToRemoteMySQL.sh < ${webroot_directory}/installation/sql/opensource-socialnetwork.sql
                 /bin/sed -i '0,/requirments/{s//account/}' ${webroot_directory}/installation/libraries/ossn.install.php
                 /bin/cp ${HOME}/application/configuration/3rd-party/ossn/bootstrap_admin_user.php ${webroot_directory}/bootstrap_admin_user.php
