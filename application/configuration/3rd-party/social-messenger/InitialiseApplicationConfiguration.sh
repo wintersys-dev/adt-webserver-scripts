431244f85858e93ce192488b1ba3aaad19f0c549
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-17 21:14:46 +0100

    Update InitialiseApplicationConfiguration.sh

--- application/configuration/3rd-party/social-messenger/InitialiseApplicationConfiguration.sh
+++ application/configuration/3rd-party/social-messenger/InitialiseApplicationConfiguration.sh
@@ -297,11 +297,7 @@ fi
 
 if ( [ "$?" = "0" ] )
 then
-        /bin/chmod 600 ${config_file}
-        /bin/chown root:www-data ${config_file}
         /bin/touch ${HOME}/runtime/INITIAL_CONFIG_SET
-      #  ${HOME}/utilities/security/EnforcePermissions.sh 
-
         if ( [ -f ${HOME}/runtime/INITIAL_CONFIG_SET_FAILED ] )
         then
                 /bin/rm ${HOME}/runtime/INITIAL_CONFIG_SET_FAILED
