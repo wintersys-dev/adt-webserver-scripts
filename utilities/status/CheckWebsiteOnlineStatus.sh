aa2f0314fcb86b1ef8211ed9ea382231792f6e44
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-21 21:09:58 +0100

    Refactor CheckWebsiteOnlineStatus.sh for website variable

--- utilities/status/CheckWebsiteOnlineStatus.sh
+++ utilities/status/CheckWebsiteOnlineStatus.sh
@@ -20,20 +20,22 @@
 ###################################################################################
 #set -x
 
-ip="${1}"
+website="${1}"
 
+checked="0"
 if ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'TEXTBROWSER:lynx'`" = "1" ] )
 then
-        /usr/bin/lynx -dump -accept_all_cookies https://${ip}:443 2>&1 >/dev/null
-        if ( [ "$?" = "0" ] )
-        then
-                /bin/echo "success"
-        else
-                /bin/echo "failure"
-        fi
+        checked="1"
+       # timeout 23 /usr/bin/lynx -dump -accept_all_cookies https://${ip} 2>&1 >/dev/null
+        timeout 23 /usr/bin/lynx -cfg=<(echo "FORCE_SSL_PROMPT:YES") -dump https://${website}
+        status="$?"
 fi
 
 if ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'TEXTBROWSER:w3m'`" = "1" ] )
 then
-        :
+        checked="1"
+        /usr/bin/yes | timeout 23 /usr/bin/w3m -dump -o ssl_verify_server=0  https://${website}
+        status="$?"
 fi
+
+
