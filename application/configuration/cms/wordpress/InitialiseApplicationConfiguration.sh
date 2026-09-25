f235564460cd8283a53b7f41159c40bb23cd45a0
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-25 09:13:35 +0100

    Install and activate 'wps-hide-login' plugin
    Added functionality to install and configure the 'wps-hide-login' plugin for WordPress.

--- application/configuration/cms/wordpress/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/wordpress/InitialiseApplicationConfiguration.sh
@@ -174,6 +174,12 @@ then
                         do
                                 /usr/bin/sudo -u www-data /usr/local/bin/wp plugin install ${plugin} --path="${webroot_directory}"
                         done
+
+                        #The special plugin to hide the admin login area with the value set in the application configuration file
+                        hidden_admin_login="`/bin/grep "^HIDDEN_ADMIN_LOGIN:" ${HOME}/runtime/application.dat | /bin/sed 's/HIDDEN_ADMIN_LOGIN//g' | /bin/sed 's/:/ /g'`"
+
+                        /usr/bin/sudo -u www-data /usr/local/bin/wp plugin install wps-hide-login --path="${webroot_directory}" --activate
+                        /usr/bin/sudo -u www-data /usr/local/bin/wp option update whl_page "${hidden_admin_login}"
                 fi
 
         #If we are looking at our webroot sourcecode we might have forgotten which database type this webroot is associated or was built against so
