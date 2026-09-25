97d659cdca5e358bd1925c40b362f79c52e61a2b
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-25 10:17:29 +0100

    Comment out settings.php removal logic
    Commented out the section that removes the settings.php file if it exists.

--- application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
@@ -388,10 +388,10 @@ fi
 
 #We are in a situation now where whatever type of install we are doing, virgin, baseline or temporal our configuration file is at ${config_file}
 #which is ourside of our webroot. So we want to create a symlink from inside our webroot to the actual configuration file
-if ( [ -f ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php ] )
-then
-        /bin/rm ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php 
-fi
+#if ( [ -f ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php ] )
+#then
+#        /bin/rm ${webroot_directory}/${webroot_subdirectory}/sites/default/settings.php 
+#fi
 
 # The application descriptor lists asset directories and regular directories which are to be linked to from inside the webroot and so this bit of 
 # code sets up that structure
