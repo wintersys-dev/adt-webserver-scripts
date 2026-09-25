8e930eee5cf416a8161743ca39c0d9e8e44b76d0
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-25 10:21:25 +0100

    Update file removal logic in InitialiseApplicationConfiguration.sh

--- application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
+++ application/configuration/cms/drupal/InitialiseApplicationConfiguration.sh
@@ -99,22 +99,12 @@ then
         /bin/rm ${config_file}
 fi
 
-if ( [ -f ${webroot_directory}/sites/default/settings.php ] ) 
+if ( [ -f ${webroot_directory}/${webroot_subdirectory}/sites/default/default.settings.php ] ) 
 then
-        /bin/rm ${webroot_directory}/sites/default/settings.php
+        /bin/rm ${webroot_directory}/${webroot_subdirectory}/sites/default/default.settings.php
 fi
 
 database_profile="`/bin/grep "^DATABASE_PROFILE:" ${HOME}/runtime/application.dat | /usr/bin/awk -F':' '{print $NF}'`"
-
-if ( [ -f ${config_file} ] )
-then
-        /bin/rm ${config_file}
-fi
-
-if ( [ -f ${webroot_directory}/wp-config.php ] ) 
-then
-        /bin/rm ${webroot_directory}/wp-config.php
-fi
 
 #In the case of a subsquent deployment it is expected that the database prefix will have been stored along with the application code
 #in the webroot, but, if it isn virgin installation we will generate the database prefix for ourselves
